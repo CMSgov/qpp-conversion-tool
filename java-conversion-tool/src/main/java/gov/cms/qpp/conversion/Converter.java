2868530f073bd5fbebc509de3d4d4ac4044d9bcb
Dave <dpuglielli@flexion.us>
2017-02-17 06:51:39 -0600

    Command line executable
    Changed to create a self-contained jar to contain its dependencies. Run
    as java -jar java-conversion-tool.jar.

---
+++ java-conversion-tool/src/main/java/gov/cms/qpp/conversion/Converter.java
@@ -0,0 +1,15 @@
+package gov.cms.qpp.conversion;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Converter {
+    static final Logger LOG = LoggerFactory.getLogger(Converter.class);
+
+	public static void main(String[] args) {
+		String msg = "Command line implementation. To be continued...";
+		LOG.info(msg);
+		System.out.println(msg);
+	}
+
+}
