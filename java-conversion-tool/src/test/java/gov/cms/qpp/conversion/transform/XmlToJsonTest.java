600a6b7104700014dd4b687e879c87b477412cac
Dave <dpuglielli@flexion.us>
2017-02-20 07:31:45 -0600

    Begin going after observations

--- java-conversion-tool/src/test/java/gov/cms/qpp/conversion/transform/XmlToJsonTest.java
+++ java-conversion-tool/src/test/java/gov/cms/qpp/conversion/transform/XmlToJsonTest.java
@@ -1,23 +1,62 @@
 package gov.cms.qpp.conversion.transform;
 
+import java.io.ByteArrayOutputStream;
+
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Result;
+import javax.xml.transform.Source;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
-import javax.xml.transform.stream.StreamSource;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathFactory;
 
 import org.junit.Test;
 import org.springframework.core.io.ClassPathResource;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 
 public class XmlToJsonTest {
 
 	@Test
-	public void TransformTest() throws Exception {
-		ClassPathResource xmlResource = new ClassPathResource("measures-data.xml");
-		ClassPathResource xslResource = new ClassPathResource("xml2json.xsl");
+	public void JacksonTest() throws Exception {
+		//ClassPathResource xmlResource = new ClassPathResource("measures-data.xml");
+		ClassPathResource xmlResource = new ClassPathResource("QRDA_III_1.xml");
 		
-		TransformerFactory tFactory = TransformerFactory.newInstance();
-		Transformer transformer = tFactory.newTransformer(new StreamSource(xslResource.getFile()));
-		transformer.transform(new StreamSource(xmlResource.getInputStream()), new StreamResult(System.out));
+        XPath xPath = XPathFactory.newInstance().newXPath();
+
+        InputSource inputSource = new InputSource(xmlResource.getInputStream());
+        
+        // String expression = "/*/measure";
+        String expression = "/*/*[local-name()='component']//*[local-name()='observation' and *[local-name()='templateId']]";
+        XPathExpression exp = xPath.compile(expression);
+        
+ 	    NodeList result = (NodeList) exp.evaluate(inputSource, XPathConstants.NODESET);
+	    // System.out.println(result);
+	    
+        Transformer xform = TransformerFactory.newInstance().newTransformer();
+ 	    xform.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+ 	    XmlMapper xmlMapper = new XmlMapper();
+	    int size = result.getLength();
+	    for (int i = 0; i <size; i++ ) {
+		    ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+		    Source xmlSource = new DOMSource(result.item(i));
+		    Result outputTarget = new StreamResult(outputStream);
+		    xform.transform(xmlSource, outputTarget);
+		    Object entries = xmlMapper.readValue(outputStream.toByteArray(), Object.class);
+		    ObjectMapper jsonMapper = new ObjectMapper();
+		    jsonMapper.enable(SerializationFeature.INDENT_OUTPUT);
+		    String json = jsonMapper.writeValueAsString(entries);
+		    System.out.println(json);
+	    }
+	    
 	}
 
 }
