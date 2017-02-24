cc9a3a9df3f454f93edf3352933f6e8fe0dc2dde
David Puglielli <dpuglielli@flexion.us>
2017-02-24 12:11:12 -0600

    Renamed Decoders to be standardized

--- java-conversion-tool/src/main/java/gov/cms/qpp/conversion/decode/AciProportionDenominatorDecoder.java
+++ java-conversion-tool/src/main/java/gov/cms/qpp/conversion/decode/AciProportionDenominatorDecoder.java
@@ -5,8 +5,8 @@ import org.jdom2.Element;
 import gov.cms.qpp.conversion.model.Node;
 import gov.cms.qpp.conversion.model.XmlDecoder;
 
-@XmlDecoder(templateId="2.16.840.1.113883.10.20.27.3.31")
-public class ACIProportionNumeratorDecoder extends QppXmlDecoder {
+@XmlDecoder(templateId="2.16.840.1.113883.10.20.27.3.32")
+public class AciProportionDenominatorDecoder extends QppXmlDecoder {
 	@Override
 	protected Node internalDecode(Element element, Node thisnode) {
 		return this.decode(element.getChild("entryRelationship"), thisnode);
