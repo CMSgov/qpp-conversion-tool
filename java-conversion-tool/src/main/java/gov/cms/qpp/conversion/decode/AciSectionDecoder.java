cf4804aae74a7483b7570b1ba16b7ff14ff3f8f9
Dave <dpuglielli@flexion.us>
2017-02-24 19:50:59 -0600

    Issue #33
    Create a decoder to parse an xml element with a child element of
    "templateId" and attribute of "root" with value of
    "2.16.840.1.113883.10.20.27.2.5"

---
+++ java-conversion-tool/src/main/java/gov/cms/qpp/conversion/decode/AciSectionDecoder.java
@@ -0,0 +1,21 @@
+package gov.cms.qpp.conversion.decode;
+
+import org.jdom2.Element;
+
+import gov.cms.qpp.conversion.model.Node;
+import gov.cms.qpp.conversion.model.XmlDecoder;
+
+@XmlDecoder(templateId="2.16.840.1.113883.10.20.27.2.5")
+public class AciSectionDecoder extends QppXmlDecoder {
+	@Override
+	protected Node internalDecode(Element element, Node thisnode) {
+		
+		thisnode.putValue("category", "aci");
+	
+		this.decode(element.getChild("entry"), thisnode);
+		
+		return thisnode;
+		
+	}
+		
+}
