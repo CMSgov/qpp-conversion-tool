5550a99af7898945a5748661c2931afd505161c0
David Uselmann <duselmann@compcc.com>
2017-02-25 21:29:53 -0600

    unit test JSON Wrapper class

--- java-conversion-tool/src/main/java/gov/cms/qpp/conversion/encode/JsonWrapper.java
+++ java-conversion-tool/src/main/java/gov/cms/qpp/conversion/encode/JsonWrapper.java
@@ -1,30 +1,74 @@
 package gov.cms.qpp.conversion.encode;
 
 import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 
-public class JsonWrapper {
+/**
+ * Manages building a "simple" object of JSON conversion.
+ * JSON renderers can convert maps and list into JSON Strings.
+ * This class is a wrapper around a list/map impl.
+ * 
+ * @author daviduselmann
+ *
+ * @param <T> Shole be String or Object for maps of children
+ */
+public class JsonWrapper<T> {
 
-	ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+	// package access allows for simpler testing
+	/*package*/ ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
 
-	private Map<String, Object> children = new LinkedHashMap<>();
-	
+	private Map<String, T> object;
+	private List<T> list;
 
-	public JsonWrapper put(String name, String value) {
-		this.children.put(name,value);
-		
-		
+	public JsonWrapper<T> put(String name, T value) {
+		checkState(list);
+		initAsObject();
+		this.object.put(name,value);
 		return this;
+	}
+	public JsonWrapper<T> put(T value) {
+		checkState(object);
+		initAsList();
+		this.list.add(value);
+		return this;
+	}
+
+	private void initAsObject() {
+		if (object == null) {
+			object = new LinkedHashMap<>();
+		}
+	}
+	
+	private void initAsList() {
+		if (list == null) {
+			list = new LinkedList<>();
+		}
+	}
+	
+	private void checkState(Object check) {
+		if (check != null) {
+			throw new IllegalStateException("Current state may not change (from list to object or reverse).");
+		}
+	}
+	
+	public boolean isObject() {
+		return object != null;
+	}
+	
+	public Object getObject() {
+		return isObject()? object :list;
 	}
 	
 	@Override
 	public String toString() {
 		try {
-			return ow.writeValueAsString(children);
+			return ow.writeValueAsString(object);
 		} catch (JsonProcessingException e) {
 			throw new RuntimeException("Issue rendering JSON from JsonWrapper Map", e);
 		}
--- java-conversion-tool/src/test/java/gov/cms/qpp/conversion/encode/JsonWrapperTest.java
+++ java-conversion-tool/src/test/java/gov/cms/qpp/conversion/encode/JsonWrapperTest.java
@@ -7,6 +7,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -15,6 +16,109 @@ import com.fasterxml.jackson.databind.ObjectWriter;
 public class JsonWrapperTest {
 
 	ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+	
+	public JsonWrapper<Object> objectObjWrapper;
+	public JsonWrapper<String> objectStrWrapper;
+	public JsonWrapper<Object> listObjWrapper;
+	public JsonWrapper<String> listStrWrapper;
+	
+	@Before
+	public void before() {
+		objectObjWrapper = new JsonWrapper<>();
+		objectStrWrapper = new JsonWrapper<>();
+		listObjWrapper   = new JsonWrapper<>();
+		listStrWrapper   = new JsonWrapper<>();
+	}
+
+	
+	
+	@Test
+	public void testInitAsList() {
+		assertTrue("Object should be null until the first put", listStrWrapper.getObject() == null);
+		listStrWrapper.put("name");
+		Object list1 = listStrWrapper.getObject();
+		assertNotNull("Init should be as a list", list1);
+		assertTrue("Init should be as a list", list1 instanceof List);
+		listStrWrapper.put("value");
+		Object list2 = listStrWrapper.getObject();
+		assertEquals("The internal instance should not change upon addition put", list1,list2);
+	}
+	
+	
+	@Test
+	public void testInitAsObject() {
+		assertTrue("Object should be null until the first put", objectStrWrapper.getObject() == null);
+		objectStrWrapper.put("name", "value");
+		Object obj1 = objectStrWrapper.getObject();
+		assertNotNull("Init should be as a map", obj1);
+		assertTrue("Init should be as a map", obj1 instanceof Map);
+		objectStrWrapper.put("name", "value");
+		Object obj2 = objectStrWrapper.getObject();
+		assertEquals("The internal instance should not change upon addition put", obj1,obj2);
+	}
+	
+	@Test(expected=IllegalStateException.class)
+	public void testCheckState_objectThenList() {
+		objectStrWrapper.put("name", "value");
+		objectStrWrapper.put("value");
+		fail("should not make it here, prior line should throw exception");
+	}
+	@Test(expected=IllegalStateException.class)
+	public void testCheckState_listThenLObject() {
+		listStrWrapper.put("value");
+		listStrWrapper.put("name", "value");
+		fail("should not make it here, prior line should throw exception");
+	}
+	
+	@Test
+	public void testGetObject_map() {
+		objectStrWrapper.put("name", "value");
+		assertTrue("should be as a map", objectStrWrapper.getObject() instanceof Map);
+		assertEquals("map should contain put value", 
+				"value", ((Map<?,?>)objectStrWrapper.getObject()).get("name"));
+		
+		Object obj = new Object();
+		objectObjWrapper.put("name", obj);
+		assertTrue("should be as a map",  objectObjWrapper.getObject() instanceof Map);
+		assertEquals("map should contain put value",
+				obj, ((Map<?,?>)objectObjWrapper.getObject()).get("name"));
+	}	
+	
+	@Test
+	public void testGetObject_list() {
+		listStrWrapper.put("name");
+		assertTrue("should be as a list", listStrWrapper.getObject() instanceof List);
+		assertTrue("lsit should contian put value",
+				((List<?>)listStrWrapper.getObject()).contains("name"));
+		
+		Object obj = new Object();
+		listObjWrapper.put(obj);
+		assertTrue("should be as a list", listObjWrapper.getObject() instanceof List);
+		assertTrue("lsit should contian put value",
+				((List<?>)listObjWrapper.getObject()).contains(obj));
+	}	
+	
+	@Test
+	public void testIsObject_true() {
+		assertFalse("should not be an object container until first put", objectStrWrapper.isObject());
+		objectStrWrapper.put("name", "value");
+		assertTrue("should be an object container after first put", objectStrWrapper.isObject());
+		
+		assertFalse("should not be an object container until first map put", objectObjWrapper.isObject());
+		objectObjWrapper.put("name", new Object());
+		assertTrue("should be an object container after first map put", objectObjWrapper.isObject());
+	}
+	
+	@Test
+	public void testIsObject_false() {
+		assertFalse("should not be an object container", listStrWrapper.isObject());
+		listStrWrapper.put("name");
+		assertFalse("should not be an object container after first list put", listStrWrapper.isObject());
+		
+		assertFalse("should not be an object container", listObjWrapper.isObject());
+		listObjWrapper.put(new Object());
+		assertFalse("should not be an object container after first list put", listObjWrapper.isObject());
+	}
 	
 	
 	@Test
@@ -108,4 +212,101 @@ public class JsonWrapperTest {
 				expect, json);
 	}
 
+	@Test
+	public void testToString_simpleObject() throws Exception {
+		objectStrWrapper.put("name1", "value1");
+		objectStrWrapper.put("name2", "value2");
+		
+		String json = objectStrWrapper.toString();
+		
+		String expect = "{\n  \"name1\" : \"value1\",\n  \"name2\" : \"value2\"\n}";
+		assertEquals("expect a simple object of JSON",
+				expect, json);
+	}
+
+	@Test
+	public void testToString_objectWithArray() throws Exception {
+		objectObjWrapper.put("name1", "value1");
+		objectObjWrapper.put("name2", new String[] {"A","B","C"});
+		objectObjWrapper.put("name3", "value3");
+		
+		String json = objectObjWrapper.toString();
+		
+		String expect = "{\n  \"name1\" : \"value1\",\n"+
+				"  \"name2\" : [ \"A\", \"B\", \"C\" ],\n" +
+				"  \"name3\" : \"value3\"\n}";
+		assertEquals("expect array to use [] rather than {} block",
+				expect, json);
+	}
+	
+	@Test
+	public void testToString_objectWithList() throws Exception {
+		listStrWrapper.put("A");
+		listStrWrapper.put("B");
+		listStrWrapper.put("C");
+		
+		objectObjWrapper.put("name1", "value1");
+		objectObjWrapper.put("name2", listStrWrapper.getObject());
+		objectObjWrapper.put("name3", "value3");
+		
+		String json = objectObjWrapper.toString();
+		
+		String expect = "{\n  \"name1\" : \"value1\",\n"+
+				"  \"name2\" : [ \"A\", \"B\", \"C\" ],\n" +
+				"  \"name3\" : \"value3\"\n}";
+		assertEquals("expect list to look like array",
+				expect, json);
+	}
+	
+	@Test
+	public void testToString_objectWithChild() throws Exception {
+		objectStrWrapper.put("obj1", "A");
+		objectStrWrapper.put("obj2", "B");
+		
+		objectObjWrapper.put("name1", "value1");
+		objectObjWrapper.put("name2", objectStrWrapper.getObject());
+		objectObjWrapper.put("name3", "value3");
+		
+		String json = objectObjWrapper.toString();
+		
+		String expect = "{\n  \"name1\" : \"value1\",\n"+
+				"  \"name2\" : {\n    \"obj1\" : \"A\",\n    \"obj2\" : \"B\"\n  },\n" +
+				"  \"name3\" : \"value3\"\n}";
+		assertEquals("expect comma after child and no comma after last value pair",
+				expect, json);
+	}
+	
+	
+	@Test
+	public void testToString_objectWithChild_commaAndOrder() throws Exception {
+		objectStrWrapper.put("obj1", "A");
+		objectStrWrapper.put("obj2", "B");
+		
+		objectObjWrapper.put("name1", "value1");
+		objectObjWrapper.put("name3", "value3");
+		objectObjWrapper.put("name2", objectStrWrapper.getObject());
+		
+		String json = objectObjWrapper.toString();
+		
+		String expect = "{\n  \"name1\" : \"value1\",\n"+
+				"  \"name3\" : \"value3\",\n" +
+				"  \"name2\" : {\n    \"obj1\" : \"A\",\n    \"obj2\" : \"B\"\n  }\n}";
+		assertEquals("expect no comma expected after the child and order as inserted",
+				expect, json);
+	}
+	
+	@Test(expected=RuntimeException.class)
+	public void testToString_exception() {
+		objectObjWrapper.put("name", new MockBadJsonTarget());
+		objectObjWrapper.toString();
+		fail("should not get here, expecting runtime parse exception");
+	}
+}
+
+class MockBadJsonTarget {
+	private String var = "";
+	@SuppressWarnings("unused")
+	private String getVar() {
+		return var;
+	}
 }
