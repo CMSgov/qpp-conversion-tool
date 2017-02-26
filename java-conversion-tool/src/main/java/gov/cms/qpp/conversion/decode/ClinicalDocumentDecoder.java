0d8fa82b2e53046196fb7563d6f3b13ada227f38
Dave <dpuglielli@flexion.us>
2017-02-26 14:45:44 -0600

    Using xpath for the clinical document

--- java-conversion-tool/src/main/java/gov/cms/qpp/conversion/decode/ClinicalDocumentDecoder.java
+++ java-conversion-tool/src/main/java/gov/cms/qpp/conversion/decode/ClinicalDocumentDecoder.java
@@ -20,11 +20,38 @@ public class ClinicalDocumentDecoder extends QppXmlDecoder {
 				.compile("./ns:templateId[@root='2.16.840.1.113883.10.20.27.1.2']/@root", 
 						Filters.attribute(), null,  xpathNs);
 		
+		XPathExpression<Attribute> programeNameExpr = XPathFactory.instance()
+				.compile("./ns:informationRecipient/ns:intendedRecipient/ns:id[@root='2.16.840.1.113883.3.249.7']/@extension", 
+						Filters.attribute(), null,  xpathNs);
+		
+		XPathExpression<Attribute> nationalProviderExpr = XPathFactory.instance()
+				.compile("./ns:documentationOf/ns:serviceEvent/ns:performer/ns:assignedEntity/ns:id[@root='2.16.840.1.113883.4.6']/@extension", 
+						Filters.attribute(), null,  xpathNs);
+		
+		XPathExpression<Attribute> taxIdExpr = XPathFactory.instance()
+				.compile("./ns:documentationOf/ns:serviceEvent/ns:performer/ns:assignedEntity/ns:representedOrganization/ns:id[@root='2.16.840.1.113883.4.2']/@extension", 
+						Filters.attribute(), null,  xpathNs);
+		
+		String effTimeStr = "./ns:component/ns:structuredBody/ns:component/ns:section[*[local-name()='templateId' and @root='2.16.840.1.113883.10.20.27.2.6']]/ns:entry/ns:act/ns:effectiveTime";
+		XPathExpression<Attribute> performanceStartExpr = XPathFactory.instance()
+				.compile(effTimeStr + "/ns:low/@value", Filters.attribute(), null,  xpathNs);
+		XPathExpression<Attribute> performanceEndExpr = XPathFactory.instance()
+				.compile(effTimeStr + "/ns:high/@value", Filters.attribute(), null,  xpathNs);
+		
 		XPathExpression<Element> componentExpr = XPathFactory.instance()
 				.compile("./ns:component/ns:structuredBody/ns:component", 
 						Filters.element(), null, xpathNs);
 		
 		Optional.ofNullable(templateIdExpr.evaluateFirst(element)).ifPresent(p -> thisnode.setId(p.getValue()));
+		
+		Optional.ofNullable(programeNameExpr.evaluateFirst(element)).ifPresent(p -> thisnode.putValue("programName", p.getValue().toLowerCase()));
+
+		Optional.ofNullable(nationalProviderExpr.evaluateFirst(element)).ifPresent(p -> thisnode.putValue("nationalProviderIdentifier", p.getValue()));
+		
+		Optional.ofNullable(taxIdExpr.evaluateFirst(element)).ifPresent(p -> thisnode.putValue("taxpayerIdentificationNumber", p.getValue()));
+
+		Optional.ofNullable(performanceStartExpr.evaluateFirst(element)).ifPresent(p -> thisnode.putValue("performanceStart", p.getValue()));
+		Optional.ofNullable(performanceEndExpr.evaluateFirst(element)).ifPresent(p -> thisnode.putValue("performanceEnd", p.getValue()));
 		
 		Optional.ofNullable(componentExpr.evaluate(element)).ifPresent(p -> this.decode(p, thisnode));
 		
--- java-conversion-tool/src/test/java/gov/cms/qpp/conversion/decode/ClinicalDocumentDecoderTest.java
+++ java-conversion-tool/src/test/java/gov/cms/qpp/conversion/decode/ClinicalDocumentDecoderTest.java
@@ -31,6 +31,11 @@ public class ClinicalDocumentDecoderTest {
 		assertThat("returned node should not be null", decoder, is(not(nullValue())));
 		
 		assertThat("template ID is correct", root.getId(), is("2.16.840.1.113883.10.20.27.1.2"));
+		assertThat("programName is correct", root.getValue("programName"), is("mips"));
+		assertThat("nationalProviderIdentifier correct", root.getValue("nationalProviderIdentifier"), is("2567891421"));
+		assertThat("taxpayerIdentificationNumber correct", root.getValue("taxpayerIdentificationNumber"), is("123456789"));
+		assertThat("performanceStart  correct", root.getValue("performanceStart"), is("20170101"));
+		assertThat("performanceEnd correct", root.getValue("performanceEnd"), is("20171231"));
 
 	}
 	
