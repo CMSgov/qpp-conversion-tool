aacedf2ba859e6b49bb1de0a792eaf505bd0fe15
saquino0827 <saquino@flexion.us>
2017-03-30 14:39:25 -0500

    Refactor Decoder
    Java docs added for clarity.
    Unit Test Code Refactored to represent more of a unit test.

--- java-conversion-tool/src/main/java/gov/cms/qpp/conversion/decode/AciSectionDecoder.java
+++ java-conversion-tool/src/main/java/gov/cms/qpp/conversion/decode/AciSectionDecoder.java
@@ -7,17 +7,23 @@ import gov.cms.qpp.conversion.model.Node;
 import gov.cms.qpp.conversion.model.XmlDecoder;
 
 /**
- * Decoder to parse Advancing Care Information Section.
- * @author David Puglielli
+ * Decoder to read XML Data for an ACI Section.
  *
  */
 @XmlDecoder(templateId="2.16.840.1.113883.10.20.27.2.5")
 public class AciSectionDecoder extends QppXmlDecoder {
+
+	/**
+	 * Decodes an ACI Section into the QPP format
+	 *
+	 * @param element XML element that represents the ACI Section
+	 * @param thisnode represents the aci section
+	 * @return
+	 */
 	@Override
 	protected DecodeResult internalDecode(Element element, Node thisnode) {
 		thisnode.putValue("category", "aci");
 		return DecodeResult.TreeContinue;
-		/// TODO here is a case where TreeContinue could be used
 	}
 		
 }
