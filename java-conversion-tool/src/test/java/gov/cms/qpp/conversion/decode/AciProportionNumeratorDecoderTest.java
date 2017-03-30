4f402b9a6c64831198f72aa4f87dac582c0fbed8
halprin <pkendall@flexion.us>
2017-03-30 12:32:12 -0600

    Added a expected result string to one of the tests

--- java-conversion-tool/src/test/java/gov/cms/qpp/conversion/decode/AciProportionNumeratorDecoderTest.java
+++ java-conversion-tool/src/test/java/gov/cms/qpp/conversion/decode/AciProportionNumeratorDecoderTest.java
@@ -1,14 +1,15 @@
 package gov.cms.qpp.conversion.decode;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
-
+import gov.cms.qpp.conversion.model.Node;
 import org.jdom2.Element;
 import org.junit.Test;
 
-import gov.cms.qpp.conversion.model.Node;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
 public class AciProportionNumeratorDecoderTest {
+
+	private static final String NUMERATOR_NODE_NAME = "aciProportionNumerator";
 
 	@Test
 	public void testInternalDecode() {
@@ -19,6 +20,6 @@ public class AciProportionNumeratorDecoderTest {
 		DecodeResult decodeResult = aciProportionNumeratorDecoder.internalDecode(element, node);
 
 		assertThat("Must continue on tree", decodeResult, is(DecodeResult.TreeContinue));
-		assertThat("", node.getValue("name"), is("aciProportionNumerator"));
+		assertThat("The node name must be " + NUMERATOR_NODE_NAME, node.getValue("name"), is(NUMERATOR_NODE_NAME));
 	}
 }
