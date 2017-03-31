5326bd4cc3f3bbd54be1ece70da2e3da0aae2842
DaveLauta <dlauta@flexion.us>
2017-03-31 16:58:49 -0400

    ClinicalDocument intermediate cibroken

--- java-conversion-tool/src/test/java/gov/cms/qpp/conversion/decode/ClinicalDocumentDecoderTest.java
+++ java-conversion-tool/src/test/java/gov/cms/qpp/conversion/decode/ClinicalDocumentDecoderTest.java
@@ -8,9 +8,10 @@ import static org.junit.Assert.assertThat;
 import java.nio.charset.Charset;
 
 import org.apache.commons.io.IOUtils;
+import org.jdom2.Element;
+import org.jdom2.Namespace;
 import org.junit.Test;
 import org.springframework.core.io.ClassPathResource;
-
 import gov.cms.qpp.conversion.decode.placeholder.DefaultDecoder;
 import gov.cms.qpp.conversion.model.Node;
 import gov.cms.qpp.conversion.xml.XmlUtils;
@@ -26,18 +27,18 @@ public class ClinicalDocumentDecoderTest {
 		// remove default nodes (will fail if defaults change)
 		DefaultDecoder.removeDefaultNode(root.getChildNodes());
 
-		assertThat("returned node should not be null", root	, is(not(nullValue())));
-		
+		assertThat("returned node should not be null", root, is(not(nullValue())));
+
 		assertThat("template ID is correct", root.getId(), is("2.16.840.1.113883.10.20.27.1.2"));
 		assertThat("programName is correct", root.getValue("programName"), is("mips"));
 		assertThat("nationalProviderIdentifier correct", root.getValue("nationalProviderIdentifier"), is("2567891421"));
 		assertThat("taxpayerIdentificationNumber correct", root.getValue("taxpayerIdentificationNumber"), is("123456789"));
 
 		assertThat("returned node should not be null", root, is(not(nullValue())));
-		
+
 		// System.out.println(root.toString());
 		assertThat("returned node should child decoder nodes", root.getChildNodes().size(), is(3));
-		
+
 		Node reportParameterSectionNode = root.getChildNodes().get(0);
 
 		assertThat("returned category", reportParameterSectionNode.getValue("source"), is("provider"));
@@ -48,8 +49,8 @@ public class ClinicalDocumentDecoderTest {
 		Node reportingActSectionNodeMeasureNode = reportParameterSectionNode.getChildNodes().get(0);
 		assertThat("returned should value", reportingActSectionNodeMeasureNode.getValue("performanceStart"), is("20170101"));
 		assertThat("returned should value", reportingActSectionNodeMeasureNode.getValue("performanceEnd"), is("20171231"));
-		
-		
+
+
 		Node aciSectionNode = root.getChildNodes().get(1);
 
 		// Should have an ACI section node 
@@ -69,14 +70,99 @@ public class ClinicalDocumentDecoderTest {
 		assertThat("returned node should have one child decoder node", iaSectionNode.getChildNodes().size(), is(1));
 		Node iaMeasureNode = iaSectionNode.getChildNodes().get(0);
 		assertThat("returned should have measureId", iaMeasureNode.getValue("measureId"), is("IA_EPA_1"));
-		
+
 		// Should have a measure performed node 
 		assertThat("returned node should not be null", iaMeasureNode.getChildNodes(), is(not(nullValue())));
 		assertThat("returned node should have one child decoder node", iaMeasureNode.getChildNodes().size(), is(1));
 		Node iaMeasurePerformedNode = iaMeasureNode.getChildNodes().get(0);
 		assertThat("returned measurePerformed", iaMeasurePerformedNode.getValue("measurePerformed"), is("Y"));
+	}
+
+	@Test
+	public void decodeClinicalDocumentInternalDecode() throws Exception {
+		//set-up
+		Namespace rootns = Namespace.getNamespace("urn:hl7-org:v3");
+		Namespace ns = Namespace.getNamespace("xsi", "http://www.w3.org/2001/XMLSchema-instance");
+
+		Element clinicalDocument = new Element("ClinicalDocument", rootns);
+		clinicalDocument.addNamespaceDeclaration(ns);
+
+		// Program name
+		//"./ns:informationRecipient/ns:intendedRecipient/ns:id[@root='2.16.840.1.113883.3.249.7']/@extension"
+		Element informationRecipient = new Element("informationRecipient", rootns);
+		Element intendedRecipient = new Element("intendedRecipient", rootns);
+		Element programName = new Element("id", rootns)
+				.setAttribute("root", "2.16.840.1.113883.3.249.7")
+				.setAttribute("extension", "MIPS");
+		intendedRecipient.addContent(programName);
+		informationRecipient.addContent(intendedRecipient);
+		clinicalDocument.addContent(informationRecipient);
+
+//			Element taxProviderElement = new Element("performer", rootns);
+//			//"./ns:documentationOf/ns:serviceEvent/ns:performer/ns:assignedEntity/ns:representedOrganization/ns:id[@root='2.16.840.1.113883.4.2']/@extension"; //setTaxProviderTaxIdOnNode
+//			Element processComponentElement = new Element("component", rootns);
+//			//"./ns:component/ns:structuredBody/ns:component"; // processComponentElement
+
+		//NationalProviderId
+		//"./ns:documentationOf/ns:serviceEvent/ns:performer/ns:assignedEntity/ns:id[@root='2.16.840.1.113883.4.6']/@extension"; //setNationalProviderIdOnNode
+		Element documentationOf = new Element("documentationOf", rootns);
+		Element serviceEvent = new Element("serviceEvent", rootns);
+		Element performer = new Element("performer", rootns);
+		Element assignedEntity = new Element("assignedEntity", rootns);
+		Element nationalProviderIdentifier = new Element("id", rootns)
+				.setAttribute("root", "2.16.840.1.113883.4.6")
+				.setAttribute("extension", "2567891421");
+		assignedEntity.addContent(nationalProviderIdentifier);
+		performer.addContent(assignedEntity);
+		serviceEvent.addContent(performer);
+		documentationOf.addContent(serviceEvent);
+		clinicalDocument.addContent(documentationOf);
+
+		//TaxProvider
+		//"./ns:documentationOf/ns:serviceEvent/ns:performer/ns:assignedEntity/ns:representedOrganization/ns:id[@root='2.16.840.1.113883.4.2']/@extension";
+
+		Element representedOrganization = new Element("representedOrganization", rootns);
+		Element taxpayerIdentificationNumber = new Element("id", rootns)
+				.setAttribute("root", "2.16.840.1.113883.4.2")
+				.setAttribute("extension", "123456789");
+
+		representedOrganization.addContent(taxpayerIdentificationNumber);
+		clinicalDocument.addContent(representedOrganization);
+
+		// Test
+		Node thisNode = new Node();
+		ClinicalDocumentDecoder objectUnderTest = new ClinicalDocumentDecoder();
+		objectUnderTest.setNamespace(clinicalDocument, objectUnderTest);
+		objectUnderTest.internalDecode(clinicalDocument, thisNode);
+		assertThat("Clinical Document doesn't contains program name", thisNode.getValue("programName"), is("mips"));
+
+		assertThat("Clinical Document doesn't contains national provider", thisNode.getValue("nationalProviderIdentifier"), is("2567891421"));
+
+
+		/**            Element templateIdElement = new Element("templateId", rootns)
+		 .setAttribute("root", "2.16.840.1.113883.10.20.27.3.28");
+		 Element referenceElement = new Element("reference", rootns);
+		 Element externalDocumentElement = new Element("externalDocument", rootns);
+		 Element idElement = new Element("id", rootns).setAttribute("extension", MEASURE_ID);
+
+		 externalDocumentElement.addContent(idElement);
+		 referenceElement.addContent(externalDocumentElement);
+		 element.addContent(templateIdElement);
+		 element.addContent(referenceElement);
+		 element.addNamespaceDeclaration(ns);
+
+		 Node thisNode = new Node();
+
+		 AciProportionMeasureDecoder objectUnderTest = new AciProportionMeasureDecoder();
+		 objectUnderTest.setNamespace(element, objectUnderTest);
+
+		 //execute
+		 objectUnderTest.internalDecode(element, thisNode);
+
+		 //assert
+		 assertThat("measureId should be " + MEASURE_ID, thisNode.getValue("measureId"), is(MEASURE_ID));
+		 */
+
 
 	}
-	
-
 }
