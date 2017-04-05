f33c834be1147ebdbf30ddfe039783c53e8c78cd
Clyde Tedrick <iamclyde@gmail.com>
2017-04-05 13:52:00 -0400

    QPPCT-10: stab at instrumentation jacoco / powermock issues

--- java-conversion-tool/src/test/java/gov/cms/qpp/conversion/ConverterTest.java
+++ java-conversion-tool/src/test/java/gov/cms/qpp/conversion/ConverterTest.java
@@ -22,7 +22,6 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 import java.util.function.Consumer;
