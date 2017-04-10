a410cc70e3aa13913ff74768bd487f74f3da02fd
DaveLauta <dlauta@flexion.us>
2017-04-10 14:26:59 -0400

    New Annotation types used for converting Strings to TemplateId types

---
+++ java-conversion-tool/src/main/java/gov/cms/qpp/conversion/model/EncoderNew.java
@@ -0,0 +1,27 @@
+package gov.cms.qpp.conversion.model;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+/**
+ * This is the annotation to mark class implementations that should be
+ * registered for encoding nodes into some output like JSON.
+
+ */
+@Target(TYPE)
+@Retention(RUNTIME)
+public @interface EncoderNew {
+
+	/**
+	 * The param(s) is/are the string pattern(s) that the defined handler will
+	 * act.
+	 *
+	 * @return
+	 */
+	TemplateId templateId();
+
+	boolean required() default false;
+}
