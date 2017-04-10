f98888b23d6bc96202be0e0eb702f6ba4124fe54
halprin <pkendall@flexion.us>
2017-04-09 22:05:19 -0600

    Added JavaDocs for XmlUtils

--- java-conversion-tool/src/main/java/gov/cms/qpp/conversion/xml/XmlUtils.java
+++ java-conversion-tool/src/main/java/gov/cms/qpp/conversion/xml/XmlUtils.java
@@ -19,10 +19,20 @@ import java.util.stream.Collectors;
  */
 public class XmlUtils {
 
+	/**
+	 * Private constructor so utility class cannot be instantiated
+	 */
 	private XmlUtils() {
 		//private constructor so utility class cannot be instantiated
 	}
-	
+
+	/**
+	 * Parses the string as XML.
+	 *
+	 * @param xml A string of XML.
+	 * @return The root element of the XML tree.
+	 * @throws XmlException When a failure to parse the XML.
+	 */
 	public static Element stringToDOM(String xml) throws XmlException {
 		if (xml == null) {
 			return null;
@@ -31,6 +41,13 @@ public class XmlUtils {
 		return parseXmlStream(new ByteArrayInputStream(xml.getBytes()));
 	}
 
+	/**
+	 * Parses an XML file specified by a path or filename.
+	 *
+	 * @param filename A path or filename of an XML file.
+	 * @return The root element of the XML tree.
+	 * @throws XmlException When a failure to parse the XML or open and read the file.
+	 */
 	public static Element fileToDOM(String filename) throws XmlException {
 		if (filename == null) {
 			return null;
@@ -38,7 +55,14 @@ public class XmlUtils {
 		
 		return fileToDOM(Paths.get(filename));
 	}
-	
+
+	/**
+	 * Parses an XML file specified by the Path.
+	 *
+	 * @param file An XML file.
+	 * @return The root element of the XML tree.
+	 * @throws XmlException When a failure to parse the XML or open and read the file.
+	 */
 	public static Element fileToDOM(Path file) throws XmlException {
 		try (InputStream xmlStream = Files.newInputStream(file)) {
 			return parseXmlStream(xmlStream);
@@ -46,7 +70,14 @@ public class XmlUtils {
 			throw new XmlException("File '" + file + "' Cannot be parsed", e);
 		}
 	}
-	
+
+	/**
+	 * Parses a stream of XML into a tree of XML elements.
+	 *
+	 * @param xmlStream The XML.
+	 * @return The root element of the XML tree.
+	 * @throws XmlException When a failure to parse the XML.
+	 */
 	protected static Element parseXmlStream(InputStream xmlStream) throws XmlException {
 		try {
 			SAXBuilder saxBuilder = new SAXBuilder();
@@ -56,7 +87,13 @@ public class XmlUtils {
 			throw new XmlException("Failed to process XML String into DOM Element", e);
 		}
 	}
-	
+
+	/**
+	 * Concatenates the parameters.
+	 *
+	 * @param parts The strings to concatenate.
+	 * @return A concatenation of the parts.
+	 */
 	public static String buildString(String ... parts) {
 		return Arrays.stream(parts)
 				.collect(Collectors.joining());
