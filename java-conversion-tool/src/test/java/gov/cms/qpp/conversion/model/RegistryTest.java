d9c9905e96d0b88006f15ac1293d7de6a70768d0
halprin <pkendall@flexion.us>
2017-04-10 15:41:02 -0600

    Improved code coverage by deleting unused classes and methods

--- java-conversion-tool/src/test/java/gov/cms/qpp/conversion/model/RegistryTest.java
+++ java-conversion-tool/src/test/java/gov/cms/qpp/conversion/model/RegistryTest.java
@@ -1,10 +1,8 @@
 package gov.cms.qpp.conversion.model;
 
 import gov.cms.qpp.conversion.decode.AggregateCountDecoder;
-import gov.cms.qpp.conversion.decode.DecodeException;
 import gov.cms.qpp.conversion.decode.InputDecoder;
 import gov.cms.qpp.conversion.encode.AggregateCountEncoder;
-
 import org.apache.commons.io.output.NullOutputStream;
 import org.jdom2.Element;
 import org.junit.After;
@@ -136,7 +134,7 @@ class Placeholder implements InputDecoder {
 	}
 
 	@Override
-	public Node decode(Element xmlDoc) throws DecodeException {
+	public Node decode(Element xmlDoc) {
 		return null;
 	}
 }
@@ -150,7 +148,7 @@ class AnotherPlaceholder implements InputDecoder {
 	}
 
 	@Override
-	public Node decode(Element xmlDoc) throws DecodeException {
+	public Node decode(Element xmlDoc) {
 		return null;
 	}
 }
@@ -161,7 +159,7 @@ class PrivateConstructor implements InputDecoder {
 	}
 
 	@Override
-	public Node decode(Element xmlDoc) throws DecodeException {
+	public Node decode(Element xmlDoc) {
 		return null;
 	}
 }
