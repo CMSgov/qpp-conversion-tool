4c529ea3ac04ce40278a3e54bcaa041d51535358
Clyde Tedrick <iamclyde@gmail.com>
2017-04-13 09:48:03 -0400

    QPPCT-48: review comments

--- benchmark/src/main/java/gov/cms/qpp/conversion/ConverterBenchmark.java
+++ benchmark/src/main/java/gov/cms/qpp/conversion/ConverterBenchmark.java
@@ -1,34 +1,3 @@
-/*
- * Copyright (c) 2014, Oracle America, Inc.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- *  * Redistributions of source code must retain the above copyright notice,
- *	this list of conditions and the following disclaimer.
- *
- *  * Redistributions in binary form must reproduce the above copyright
- *	notice, this list of conditions and the following disclaimer in the
- *	documentation and/or other materials provided with the distribution.
- *
- *  * Neither the name of Oracle nor the names of its contributors may be used
- *	to endorse or promote products derived from this software without
- *	specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
- * THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 package gov.cms.qpp.conversion;
 
 import org.openjdk.jmh.annotations.*;
@@ -39,11 +8,17 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.concurrent.TimeUnit;
 
+/**
+ * Performance test harness.
+ */
 @Warmup(iterations = 5, time = 1, timeUnit = TimeUnit.MILLISECONDS)
 @Measurement(iterations = 5, time = 1, timeUnit = TimeUnit.MILLISECONDS)
 @Fork(3)
 public class ConverterBenchmark {
 
+	/**
+	 * State management for tests.
+	 */
 	@State(Scope.Thread)
 	public static class Cleaner {
 		@TearDown(Level.Trial)
@@ -53,9 +28,13 @@ public class ConverterBenchmark {
 		}
 	}
 
-
+	/**
+	 * Benchmark qrda file conversion.
+	 *
+	 * @param cleaner State management for conversion runs, ensures that output files are deleted.
+	 */
 	@Benchmark
-	public void testMethod( Cleaner cleaner) {
+	public void benchmarkMain( Cleaner cleaner) {
 		Converter.main(new String[] {"src/main/resources/qrda-files/valid-QRDA-III.xml"});
 	}
 
