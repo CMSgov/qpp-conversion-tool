bf80ee91b63a968679a4bcd74d2bad77bc3ea564
DaveLauta <dlauta@flexion.us>
2017-04-14 10:18:58 -0400

    QPPCT-16 created test to verify extraneous info in xml has no effect

--- java-conversion-tool/src/test/java/gov/cms/qpp/conversion/decode/AciNumeratorDenominatorDecoderTest.java
+++ java-conversion-tool/src/test/java/gov/cms/qpp/conversion/decode/AciNumeratorDenominatorDecoderTest.java
@@ -1,86 +1,252 @@
 package gov.cms.qpp.conversion.decode;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.not;
-import static org.hamcrest.CoreMatchers.nullValue;
-import static org.junit.Assert.assertThat;
-
+import gov.cms.qpp.conversion.model.Node;
+import gov.cms.qpp.conversion.model.TemplateId;
+import gov.cms.qpp.conversion.xml.XmlException;
+import gov.cms.qpp.conversion.xml.XmlUtils;
 import org.junit.Test;
 
-import gov.cms.qpp.conversion.model.Node;
-import gov.cms.qpp.conversion.xml.XmlUtils;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.assertThat;
 
 public class AciNumeratorDenominatorDecoderTest {
 
-	@Test
-	public void decodeAciNumeratorDenominatorAsNode() throws Exception {
-		String xmlFragment = XmlUtils.buildString(
-				"<root xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns=\"urn:hl7-org:v3\">",
-				"  <observation classCode=\"OBS\" moodCode=\"EVN\">",
-				"    <templateId root=\"2.16.840.1.113883.10.20.27.3.3\"/>",
-				"    <code code=\"MSRAGG\" codeSystem=\"2.16.840.1.113883.5.4\" codeSystemName=\"ActCode\" displayName=\"rate aggregation\"/>",
-				"    <statusCode code=\"completed\"/>", "    <value xsi:type=\"INT\" value=\"600\"/>",
-				"    <methodCode code=\"COUNT\" codeSystem=\"2.16.840.1.113883.5.84\" codeSystemName=\"ObservationMethod\" displayName=\"Count\"/>",
-				"  </observation>",
-				"</root>");
+    @Test
+    public void decodeRateAggregationAsNode() throws Exception {
+        String xmlFragment = XmlUtils.buildString(
+                "<root xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns=\"urn:hl7-org:v3\">",
+                "  <observation classCode=\"OBS\" moodCode=\"EVN\">",
+                "    <templateId root=\"2.16.840.1.113883.10.20.27.3.3\"/>",
+                "    <code code=\"MSRAGG\" codeSystem=\"2.16.840.1.113883.5.4\" codeSystemName=\"ActCode\" displayName=\"rate aggregation\"/>",
+                "    <statusCode code=\"completed\"/>", "    <value xsi:type=\"INT\" value=\"600\"/>",
+                "    <methodCode code=\"COUNT\" codeSystem=\"2.16.840.1.113883.5.84\" codeSystemName=\"ObservationMethod\" displayName=\"Count\"/>",
+                "  </observation>",
+                "</root>");
 
-		Node numDenomNode = new QppXmlDecoder().decode(XmlUtils.stringToDOM(xmlFragment));
+        Node rateAggreagationNode = new QppXmlDecoder().decode(XmlUtils.stringToDOM(xmlFragment));
 
-		// the returned Node object from the snippet should be:
-		// a top level placeholder node with a single child node that has the
-		// "rateAggregationDenominator" key in it
+        // the returned Node object from the snippet should be:
+        // a top level placeholder node with a single child node that has the
+        // "rateAggregationDenominator" key in it
 
-		assertThat("returned node should not be null", numDenomNode, is(not(nullValue())));
+        assertThat("returned node should not be null", rateAggreagationNode, is(not(nullValue())));
 
-		assertThat("returned node should have one child node", numDenomNode.getChildNodes().size(), is(1));
+        assertThat("returned node should have one child node", rateAggreagationNode.getChildNodes().size(), is(1));
 
-		assertThat("aci numerator/denominator value should be 600",
-				numDenomNode.getChildNodes().get(0).getValue("aggregateCount"), is("600"));
-	}
-	
-	@Test
-	public void decodeAciNumeratorDenominatorNullValueAsNode() throws Exception {
-		String xmlFragment = XmlUtils.buildString(
-				"<root xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns=\"urn:hl7-org:v3\" >",
-				"  <observation classCode=\"OBS\" moodCode=\"EVN\">",
-				"    <templateId root=\"2.16.840.1.113883.10.20.27.3.3\"/>",
-				"    <value xsi:type=\"INT\"/>",
-				"  </observation>",
-				"</root>");
+        assertThat("aci rate aggregation value should be 600",
+                rateAggreagationNode.getChildNodes().get(0).getValue("aggregateCount"), is("600"));
+    }
 
-		Node numDenomNode = new QppXmlDecoder().decode(XmlUtils.stringToDOM(xmlFragment));
+    @Test
+    public void decodeAciNumeratorDenominatorNullValueAsNode() throws Exception {
+        String xmlFragment = XmlUtils.buildString(
+                "<root xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns=\"urn:hl7-org:v3\" >",
+                "  <observation classCode=\"OBS\" moodCode=\"EVN\">",
+                "    <templateId root=\"2.16.840.1.113883.10.20.27.3.3\"/>",
+                "    <value xsi:type=\"INT\"/>",
+                "  </observation>",
+                "</root>");
 
-		// the returned Node object from the snippet should be:
-		// a top level placeholder node with a single child node that has the
-		// "rateAggregationDenominator" key in it
+        Node numDenomNode = new QppXmlDecoder().decode(XmlUtils.stringToDOM(xmlFragment));
 
-		assertThat("returned node should not be null", numDenomNode, is(not(nullValue())));
+        // the returned Node object from the snippet should be:
+        // a top level placeholder node with a single child node that has the
+        // "rateAggregationDenominator" key in it
 
-		assertThat("returned node should have one child node", numDenomNode.getChildNodes().size(), is(1));
+        assertThat("returned node should not be null", numDenomNode, is(not(nullValue())));
 
-		assertThat("aci numerator/denominator value should be null", numDenomNode.getChildNodes().get(0).getValue("aggregateCount"), is(nullValue()));
-	}
+        assertThat("returned node should have one child node", numDenomNode.getChildNodes().size(), is(1));
 
-	@Test
-	public void decodeAciNumeratorDenominatorNullElementAsNode() throws Exception {
-		String xmlFragment = XmlUtils.buildString(
-				"<root xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns=\"urn:hl7-org:v3\">",
-				"  <observation classCode=\"OBS\" moodCode=\"EVN\">",
-				"    <templateId root=\"2.16.840.1.113883.10.20.27.3.3\"/>",
-				"  </observation>",
-				"</root>");
+        assertThat("aci numerator/denominator value should be null", numDenomNode.getChildNodes().get(0).getValue("aggregateCount"), is(nullValue()));
+    }
 
-		Node numDenomNode = new QppXmlDecoder().decode(XmlUtils.stringToDOM(xmlFragment));
+    @Test
+    public void decodeAciNumeratorDenominatorNullElementAsNode() throws Exception {
+        String xmlFragment = XmlUtils.buildString(
+                "<root xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns=\"urn:hl7-org:v3\">",
+                "  <observation classCode=\"OBS\" moodCode=\"EVN\">",
+                "    <templateId root=\"2.16.840.1.113883.10.20.27.3.3\"/>",
+                "  </observation>",
+                "</root>");
 
-		// the returned Node object from the snippet should be:
-		// a top level placeholder node with a single child node that has the
-		// "rateAggregationDenominator" key in it
+        Node numDenomNode = new QppXmlDecoder().decode(XmlUtils.stringToDOM(xmlFragment));
 
-		assertThat("returned node should not be null", numDenomNode, is(not(nullValue())));
+        // the returned Node object from the snippet should be:
+        // a top level placeholder node with a single child node that has the
+        // "rateAggregationDenominator" key in it
 
-		assertThat("returned node should have one child node", numDenomNode.getChildNodes().size(), is(1));
+        assertThat("returned node should not be null", numDenomNode, is(not(nullValue())));
 
-		assertThat("aci numerator/denominator value should be null", numDenomNode.getChildNodes().get(0).getValue("aggregateCount"), is(nullValue()));
-	}
+        assertThat("returned node should have one child node", numDenomNode.getChildNodes().size(), is(1));
 
+        assertThat("aci numerator/denominator value should be null", numDenomNode.getChildNodes().get(0).getValue("aggregateCount"), is(nullValue()));
+    }
+
+    @Test
+    public void decodeAciNumeratorDenominatorValidTest() throws XmlException {
+        Node aciMeasureNode = new QppXmlDecoder().decode(XmlUtils.stringToDOM(getValidXmlFragment()));
+        assertThat("Decoded xml fragment should contain some nodes",
+                aciMeasureNode.getChildNodes().size(), is(2));
+
+        List<Node> nodeList = aciMeasureNode.findNode(TemplateId.ACI_NUMERATOR.getTemplateId());
+        assertThat("Decoded xml fragment should contain " + TemplateId.ACI_NUMERATOR.name(),
+                nodeList.size(), is(1));
+
+        nodeList = nodeList.get(0).findNode(TemplateId.ACI_AGGREGATE_COUNT.getTemplateId());
+        assertThat("Decoded xml fragment " + TemplateId.ACI_NUMERATOR.name() +
+                        " should contain " + TemplateId.ACI_AGGREGATE_COUNT.name(),
+                nodeList.size(), is(1));
+
+        nodeList = aciMeasureNode.findNode(TemplateId.ACI_DENOMINATOR.getTemplateId());
+        assertThat("Decoded xml fragment should contain " + TemplateId.ACI_DENOMINATOR.name(),
+                nodeList.size(), is(1));
+
+        nodeList = nodeList.get(0).findNode(TemplateId.ACI_AGGREGATE_COUNT.getTemplateId());
+        assertThat("Decoded xml fragment " + TemplateId.ACI_NUMERATOR.name() +
+                        " should contain " + TemplateId.ACI_AGGREGATE_COUNT.name(),
+                nodeList.size(), is(1));
+        int numberNodes = countNodes(aciMeasureNode);
+        assertThat("Decoded xml fragment " + aciMeasureNode.getType().name() +
+                        " should contain 8 nodes", numberNodes, is(8));
+    }
+
+    @Test
+    public void decodeAciNumeratorDenominatorExtraneousXMLTest() throws XmlException {
+        String xmlFragment = getValidXmlFragment();
+        xmlFragment = xmlFragment.replaceAll("<statusCode ",
+        "\n<Stuff arbitrary=\"123\"><newnode>Some extra stuff</newnode></Stuff>Unexpected stuff appears here\n\n<statusCode ");
+
+        Node aciMeasureNode = new QppXmlDecoder().decode(XmlUtils.stringToDOM(xmlFragment));
+        assertThat("Decoded xml fragment should contain some nodes",
+                aciMeasureNode.getChildNodes().size(), is(2));
+
+        List<Node> nodeList = aciMeasureNode.findNode(TemplateId.ACI_NUMERATOR.getTemplateId());
+        assertThat("Decoded xml fragment should contain " + TemplateId.ACI_NUMERATOR.name(),
+                nodeList.size(), is(1));
+
+        nodeList = nodeList.get(0).findNode(TemplateId.ACI_AGGREGATE_COUNT.getTemplateId());
+        assertThat("Decoded xml fragment " + TemplateId.ACI_NUMERATOR.name() +
+                        " should contain " + TemplateId.ACI_AGGREGATE_COUNT.name(),
+                nodeList.size(), is(1));
+
+        nodeList = aciMeasureNode.findNode(TemplateId.ACI_DENOMINATOR.getTemplateId());
+        assertThat("Decoded xml fragment should contain " + TemplateId.ACI_DENOMINATOR.name(),
+                nodeList.size(), is(1));
+
+        nodeList = nodeList.get(0).findNode(TemplateId.ACI_AGGREGATE_COUNT.getTemplateId());
+        assertThat("Decoded xml fragment " + TemplateId.ACI_NUMERATOR.name() +
+                        " should contain " + TemplateId.ACI_AGGREGATE_COUNT.name(),
+                nodeList.size(), is(1));
+        int numberNodes = countNodes(aciMeasureNode);
+        assertThat("Decoded xml fragment " + aciMeasureNode.getType().name() +
+                " should contain 7 nodes", numberNodes, is(8));
+        System.out.println(aciMeasureNode.toString()+"\n");
+	    System.out.println(xmlFragment);
+    }
+
+    private int countNodes(Node parent){
+        int count = 1;
+        if ( parent == null ) {
+            return count;
+        }
+        List<Node> children = parent.getChildNodes();
+        if ( children == null || children.isEmpty()){
+            return count;
+        }
+        for (Node node : children ){
+            count += countChildren(node);
+        }
+        return count;
+    }
+
+    private int countChildren(Node parent){
+        int count = 1;
+        if ( parent == null ) {
+            return count;
+        }
+        List<Node> children = parent.getChildNodes();
+        if ( children == null || children.isEmpty()){
+            return count;
+        }
+        for (Node node : children ){
+            count += countNodes(node);
+        }
+        return count;
+    }
+
+    private String getValidXmlFragment(){
+        String xmlFragment = XmlUtils.buildString(
+                "<root xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns=\"urn:hl7-org:v3\">",
+                "\n<entry>",
+                "\n<organizer classCode=\"CLUSTER\" moodCode=\"EVN\">",
+                "\n<!-- Implied template Measure Reference templateId -->",
+                "\n<templateId root=\"2.16.840.1.113883.10.20.27.2.5\" extension=\"2016-09-01\"/>",
+                "\n<!-- ACI Numerator Denominator Type Measure Reference and Results templateId -->",
+                "\n<templateId root=\"2.16.840.1.113883.10.20.27.3.28\" extension=\"2016-09-01\"/>",
+                "\n<id root=\"ac575aef-7062-4ea2-b723-df517cfa470a\"/>",
+                "\n<statusCode code=\"completed\"/>",
+                "\n<reference typeCode=\"REFR\">",
+                "\n <!-- Reference to a particular ACI measure's unique identifier. -->",
+                "\n <externalDocument classCode=\"DOC\" moodCode=\"EVN\">",
+                "\n     <!-- This is a temporary root OID that indicates this is an ACI measure identifier -->",
+                "\n     <!-- extension is the unique identifier for an ACI measure. \"ACI-PEA-1\" is for illustration only. -->",
+                "\n     <id root=\"2.16.840.1.113883.3.7031\" extension=\"ACI-PEA-1\"/>",
+                "\n     <!-- ACI measure title -->",
+                "\n     <text>Patient Access</text>",
+                "\n </externalDocument>",
+                "\n</reference>",
+                "\n<component>",
+                "\n <observation classCode=\"OBS\" moodCode=\"EVN\">",
+                "\n     <!-- Performance Rate templateId -->",
+                "\n     <templateId root=\"2.16.840.1.113883.10.20.27.3.30\" extension=\"2016-09-01\"/>",
+                "\n     <code code=\"72510-1\" codeSystem=\"2.16.840.1.113883.6.1\" codeSystemName=\"LOINC\" displayName=\"Performance Rate\"/>",
+                "\n     <statusCode code=\"completed\"/>",
+                "\n     <value xsi:type=\"REAL\" value=\"0.750000\"/>",
+                "\n </observation>",
+                "\n</component>",
+                "\n<component>",
+                "\n <observation classCode=\"OBS\" moodCode=\"EVN\">",
+                "\n     <!-- ACI Numerator Denominator Type Measure Numerator Data templateId -->",
+                "\n     <templateId root=\"2.16.840.1.113883.10.20.27.3.31\" extension=\"2016-09-01\"/>",
+                "\n     <code code=\"ASSERTION\" codeSystem=\"2.16.840.1.113883.5.4\" codeSystemName=\"ActCode\" displayName=\"Assertion\"/>",
+                "\n     <statusCode code=\"completed\"/>",
+                "\n     <value xsi:type=\"CD\" code=\"NUMER\" codeSystem=\"2.16.840.1.113883.5.4\" codeSystemName=\"ActCode\"/>",
+                "\n     <!-- Numerator Count-->",
+                "\n     <entryRelationship typeCode=\"SUBJ\" inversionInd=\"true\">",
+                "\n         <observation classCode=\"OBS\" moodCode=\"EVN\">",
+                "\n             <templateId root=\"2.16.840.1.113883.10.20.27.3.3\"/>",
+                "\n             <code code=\"MSRAGG\" codeSystem=\"2.16.840.1.113883.5.4\" codeSystemName=\"ActCode\" displayName=\"rate aggregation\"/>",
+                "\n             <statusCode code=\"completed\"/>",
+                "\n             <value xsi:type=\"INT\" value=\"600\"/>",
+                "\n             <methodCode code=\"COUNT\" codeSystem=\"2.16.840.1.113883.5.84\" codeSystemName=\"ObservationMethod\" displayName=\"Count\"/>",
+                "\n         </observation>",
+                "\n     </entryRelationship>",
+                "\n </observation>",
+                "\n</component>",
+                "\n<component>",
+                "\n <observation classCode=\"OBS\" moodCode=\"EVN\">",
+                "\n     <!-- ACI Numerator Denominator Type Measure Denominator Data templateId -->",
+                "\n     <templateId root=\"2.16.840.1.113883.10.20.27.3.32\" extension=\"2016-09-01\"/>",
+                "\n     <code code=\"ASSERTION\" codeSystem=\"2.16.840.1.113883.5.4\" codeSystemName=\"ActCode\" displayName=\"Assertion\"/>",
+                "\n     <statusCode code=\"completed\"/>",
+                "\n     <value xsi:type=\"CD\" code=\"DENOM\" codeSystem=\"2.16.840.1.113883.5.4\" codeSystemName=\"ActCode\"/>",
+                "\n     <!-- Denominator Count-->",
+                "\n     <entryRelationship typeCode=\"SUBJ\" inversionInd=\"true\">",
+                "\n         <observation classCode=\"OBS\" moodCode=\"EVN\">",
+                "\n             <templateId root=\"2.16.840.1.113883.10.20.27.3.3\"/>",
+                "\n             <code code=\"MSRAGG\" codeSystem=\"2.16.840.1.113883.5.4\" codeSystemName=\"ActCode\" displayName=\"rate aggregation\"/>",
+                "\n             <statusCode code=\"completed\"/>",
+                "\n             <value xsi:type=\"INT\" value=\"800\"/>",
+                "\n             <methodCode code=\"COUNT\" codeSystem=\"2.16.840.1.113883.5.84\" codeSystemName=\"ObservationMethod\" displayName=\"Count\"/>",
+                "\n         </observation>",
+                "\n     </entryRelationship>",
+                "\n </observation>",
+                "\n</component>",
+                "\n</organizer>",
+                "\n</entry>",
+                "\n</root>\n");
+        return xmlFragment;
+    }
 }
--- java-conversion-tool/src/test/java/gov/cms/qpp/conversion/decode/AciProportionDenominatorDecoderTest.java
+++ java-conversion-tool/src/test/java/gov/cms/qpp/conversion/decode/AciProportionDenominatorDecoderTest.java
@@ -1,60 +1,99 @@
 package gov.cms.qpp.conversion.decode;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.not;
-import static org.hamcrest.CoreMatchers.nullValue;
-import static org.junit.Assert.assertThat;
-
-import org.junit.Test;
-
 import gov.cms.qpp.conversion.model.Node;
 import gov.cms.qpp.conversion.xml.XmlUtils;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.assertThat;
+
 /**
  * AciProportionDenominatorDecoderTest JUnit test for
  * AciProportionDenominatorDecoder
- *
  */
 public class AciProportionDenominatorDecoderTest {
 
-    /**
-     * decodeACIProportionDenominatorAsNode given a well formed xml fragment
-     * parses out the appropriate aggregateCount This test calls
-     * QppXmlDecoder.()decode() which in turn calls the only method in this
-     * class. AciProportionDenominatorDecoder().decode()
-     *
-     * @throws Exception
-     */
-    @Test
-    public void decodeACIProportionDenominatorAsNode() throws Exception {
+	/**
+	 * decodeACIProportionDenominatorAsNode given a well formed xml fragment
+	 * parses out the appropriate aggregateCount This test calls
+	 * QppXmlDecoder.()decode() which in turn calls the only method in this
+	 * class. AciProportionDenominatorDecoder().decode()
+	 *
+	 * @throws Exception
+	 */
+	@Test
+	public void decodeACIProportionDenominatorAsNode() throws Exception {
 
-        String xmlFragment = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n"
-                + "<component xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns=\"urn:hl7-org:v3\">\n"
-                + " <observation classCode=\"OBS\" moodCode=\"EVN\">\n"
-                + "     <!-- ACI Numerator Denominator Type Measure Denominator Data templateId -->\n"
-                + "     <templateId root=\"2.16.840.1.113883.10.20.27.3.32\" extension=\"2016-09-01\" />\n"
-                + "     <!-- Denominator Count -->\n"
-                + "     <entryRelationship typeCode=\"SUBJ\" inversionInd=\"true\">\n"
-                + "         <qed resultName=\"aggregateCount\" resultValue=\"800\">\n"
-                + "             <templateId root=\"Q.E.D\"/>\n"
-                + "         </qed>"
-                + "     </entryRelationship>\n"
-                + " </observation>\n"
-                + "</component>";
-        Node root = new QppXmlDecoder().decode(XmlUtils.stringToDOM(xmlFragment));
+		String xmlFragment = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n"
+				+ "<component xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns=\"urn:hl7-org:v3\">\n"
+				+ " <observation classCode=\"OBS\" moodCode=\"EVN\">\n"
+				+ "     <!-- ACI Numerator Denominator Type Measure Denominator Data templateId -->\n"
+				+ "     <templateId root=\"2.16.840.1.113883.10.20.27.3.32\" extension=\"2016-09-01\" />\n"
+				+ "     <!-- Denominator Count -->\n"
+				+ "     <entryRelationship typeCode=\"SUBJ\" inversionInd=\"true\">\n"
+				+ "         <qed resultName=\"aggregateCount\" resultValue=\"800\">\n"
+				+ "             <templateId root=\"Q.E.D\"/>\n"
+				+ "         </qed>"
+				+ "     </entryRelationship>\n"
+				+ " </observation>\n"
+				+ "</component>";
+		Node root = new QppXmlDecoder().decode(XmlUtils.stringToDOM(xmlFragment));
 
-        // This node is the place holder around the root node
-        assertThat("returned node should not be null", root, is(not(nullValue())));
+		// This node is the place holder around the root node
+		assertThat("returned node should not be null", root, is(not(nullValue())));
 
-        // For all decoders this should be either a value or child node
-        assertThat("returned node should have one child node", root.getChildNodes().size(), is(1));
-        // This is the child node that is produced by the intended decoder
-        Node aciProportionDenominatorNode = root.getChildNodes().get(0);
-        // Should have a aggregate count node
-        assertThat("returned node should have one child decoder node",
-                aciProportionDenominatorNode.getChildNodes().size(), is(1));
-        // This is stubbed node with the test value
-        Node target = aciProportionDenominatorNode.getChildNodes().get(0);
-        // Get the test value
-        assertThat("test value should be mytestvalue", target.getValue("aggregateCount"), is("800"));
-    }
+		// For all decoders this should be either a value or child node
+		assertThat("returned node should have one child node", root.getChildNodes().size(), is(1));
+		// This is the child node that is produced by the intended decoder
+		Node aciProportionDenominatorNode = root.getChildNodes().get(0);
+		// Should have a aggregate count node
+		assertThat("returned node should have one child decoder node",
+				aciProportionDenominatorNode.getChildNodes().size(), is(1));
+		// This is stubbed node with the test value
+		Node target = aciProportionDenominatorNode.getChildNodes().get(0);
+		// Get the test value
+		assertThat("test value should be mytestvalue", target.getValue("aggregateCount"), is("800"));
+	}
+
+	@Test
+	public void decodeInvalidACIProportionDenominatorAsNode() throws Exception {
+
+		String xmlFragment = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n"
+			+ "<component xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns=\"urn:hl7-org:v3\">\n"
+			+ " <observation classCode=\"OBS\" moodCode=\"EVN\">\n"
+			+ "     <!-- ACI Numerator Denominator Type Measure Denominator Data templateId -->\n"
+			+ "     <templateId root=\"2.16.840.1.113883.10.20.27.3.32\" extension=\"2016-09-01\" />\n"
+			+ "     <!-- Denominator Count -->\n"
+			+ "     <entryRelationship typeCode=\"SUBJ\" inversionInd=\"true\">\n"
+			+ "         <qed resultName=\"aggregateCount\" resultValue=\"800\">\n"
+			+ "             <templateId root=\"Q.E.D\"/>\n"
+			+ "         </qed>"
+			+ "     </entryRelationship>\n"
+			+ " </observation>\n"
+			+ "  <observation classCode=\"OBS\" moodCode=\"EVN\">"
+			+ "    <templateId root=\"2.16.840.1.113883.10.20.27.3.3\"/>"
+			+ "    <code code=\"MSRAGG\" codeSystem=\"2.16.840.1.113883.5.4\" codeSystemName=\"ActCode\" displayName=\"rate aggregation\"/>"
+			+ "    <statusCode code=\"completed\"/>"
+		    + "    <value xsi:type=\"INT\" value=\"600\"/>"
+			+ "    <methodCode code=\"COUNT\" codeSystem=\"2.16.840.1.113883.5.84\" codeSystemName=\"ObservationMethod\" displayName=\"Count\"/>"
+			+ "  </observation>"
+			+ " </component>";
+		Node root = new QppXmlDecoder().decode(XmlUtils.stringToDOM(xmlFragment));
+
+		// This node is the place holder around the root node
+		assertThat("returned node should not be null", root, is(not(nullValue())));
+
+		// For all decoders this should be either a value or child node
+		assertThat("returned node should have one child node", root.getChildNodes().size(), is(2));
+		// This is the child node that is produced by the intended decoder
+		Node aciProportionDenominatorNode = root.getChildNodes().get(0);
+		// Should have a aggregate count node
+		assertThat("returned node should have one child decoder node",
+				aciProportionDenominatorNode.getChildNodes().size(), is(1));
+		// This is stubbed node with the test value
+		Node target = aciProportionDenominatorNode.getChildNodes().get(0);
+		// Get the test value
+		assertThat("test value should be mytestvalue", target.getValue("aggregateCount"), is("800"));
+			}
+
 }
