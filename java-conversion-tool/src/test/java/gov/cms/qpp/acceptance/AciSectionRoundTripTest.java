d261e9a964771c52cf3ca25be21190d1f591bbf9
Adam Harrison <ad@mharrison.us>
2017-04-17 12:44:14 -0400

    Fix compiling

--- java-conversion-tool/src/test/java/gov/cms/qpp/acceptance/AciSectionRoundTripTest.java
+++ java-conversion-tool/src/test/java/gov/cms/qpp/acceptance/AciSectionRoundTripTest.java
@@ -42,7 +42,7 @@ public class AciSectionRoundTripTest {
 		                     + "</component>";
 
 		//execute
-		Node parentNode = new QppXmlDecoder().decode(XmlUtils.stringToDOM(xmlFragment));
+		Node parentNode = new QppXmlDecoder().decode(XmlUtils.stringToDom(xmlFragment));
 		DefaultDecoder.removeDefaultNode(parentNode.getChildNodes());
 
 		//assert
@@ -72,7 +72,7 @@ public class AciSectionRoundTripTest {
 		                     + "</component>";
 
 		//execute
-		Node parentNode = new QppXmlDecoder().decode(XmlUtils.stringToDOM(xmlFragment));
+		Node parentNode = new QppXmlDecoder().decode(XmlUtils.stringToDom(xmlFragment));
 		DefaultDecoder.removeDefaultNode(parentNode.getChildNodes());
 
 		//assert
