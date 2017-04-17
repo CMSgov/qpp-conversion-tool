ddcc932fbbec66e9bbb62ac9cf76e321b7052476
saquino0827 <saquino@flexion.us>
2017-04-17 10:02:25 -0500

    QPPCT-20 : Clean unit tests

--- java-conversion-tool/src/test/java/gov/cms/qpp/conversion/decode/AciProportionMeasureDecoderTest.java
+++ java-conversion-tool/src/test/java/gov/cms/qpp/conversion/decode/AciProportionMeasureDecoderTest.java
@@ -17,6 +17,7 @@ import java.util.List;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.CoreMatchers.nullValue;
+import static org.hamcrest.Matchers.hasSize;
 import static org.junit.Assert.assertThat;
 
 public class AciProportionMeasureDecoderTest {
@@ -35,81 +36,25 @@ public class AciProportionMeasureDecoderTest {
 
 	@Test
 	public void decodeACIProportionMeasureAsNode() throws Exception {
-		String xmlFragment
-				= "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n"
-				+ "<entry xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns=\"urn:hl7-org:v3\">\n"
-				+ "	<organizer classCode=\"CLUSTER\" moodCode=\"EVN\">\n"
-				+ "		<!-- Implied template Measure Reference templateId -->\n"
-				+ "		<templateId root=\"2.16.840.1.113883.10.20.24.3.98\"/>\n"
-				+ "		<!-- ACI Numerator Denominator Type Measure Reference and Results templateId -->\n"
-				+ "		<templateId root=\"2.16.840.1.113883.10.20.27.3.28\" extension=\"2016-09-01\"/>\n"
-				+ "		<id root=\"ac575aef-7062-4ea2-b723-df517cfa470a\"/>\n"
-				+ "		<statusCode code=\"completed\"/>\n"
-				+ "		<reference typeCode=\"REFR\">\n"
-				+ "			<!-- Reference to a particular ACI measure's unique identifier. -->\n"
-				+ "			<externalDocument classCode=\"DOC\" moodCode=\"EVN\">\n"
-				+ "				<!-- This is a temporary root OID that indicates this is an ACI measure identifier -->\n"
-				+ "				<!-- extension is the unique identifier for an ACI measure. \"ACI-PEA-1\" is for illustration only. -->\n"
-				+ "				<id root=\"2.16.840.1.113883.3.7031\" extension=\"ACI-PEA-1\"/>\n"
-				+ "				<!-- ACI measure title -->\n"
-				+ "				<text>Patient Access</text>\n"
-				+ "			</externalDocument>\n"
-				+ "		</reference>\n"
-				+ "		<component>\n"
-				+ "			<observation classCode=\"OBS\" moodCode=\"EVN\">\n"
-				+ "				<!-- Performance Rate templateId -->\n"
-				+ "				<templateId root=\"2.16.840.1.113883.10.20.27.3.30\"\n"
-				+ "					extension=\"2016-09-01\"/>\n"
-				+ "				<code code=\"72510-1\" codeSystem=\"2.16.840.1.113883.6.1\" codeSystemName=\"LOINC\" displayName=\"Performance Rate\"/>\n"
-				+ "				<statusCode code=\"completed\"/>\n"
-				+ "				<value xsi:type=\"REAL\" value=\"0.750000\"/>\n"
-				+ "			</observation>\n"
-				+ "		</component>\n"
-				+ "		<component>\n"
-				+ "			<observation classCode=\"OBS\" moodCode=\"EVN\">\n"
-				+ "				<!-- ACI Numerator Denominator Type Measure Numerator Data templateId -->\n"
-				+ "				<templateId root=\"2.16.840.1.113883.10.20.27.3.31\" extension=\"2016-09-01\"/>\n"
-				+ "				<entryRelationship resultName=\"aciNumeratorDenominator\" resultValue=\"600\">\n"
-				+ "					<templateId root=\"Q.E.D\"/>\n"
-				+ "				</entryRelationship>"
-				+ "			</observation>\n"
-				+ "		</component>\n"
-				+ "		<component>\n"
-				+ "			<observation classCode=\"OBS\" moodCode=\"EVN\">\n"
-				+ "				<!-- ACI Numerator Denominator Type Measure Denominator Data templateId -->\n"
-				+ "				<templateId root=\"2.16.840.1.113883.10.20.27.3.32\" extension=\"2016-09-01\"/>\n"
-				+ "				<entryRelationship resultName=\"aciNumeratorDenominator\" resultValue=\"800\">\n"
-				+ "					<templateId root=\"Q.E.D\"/>\n"
-				+ "				</entryRelationship>"
-				+ "			</observation>\n"
-				+ "		</component>\n"
-				+ "	</organizer>\n"
-				+ "</entry>";
+		String xmlFragment = getValidXmlFragment();
 
 		Node root = new QppXmlDecoder().decode(XmlUtils.stringToDOM(xmlFragment));
-		// remove default nodes (will fail if defaults change)
 		DefaultDecoder.removeDefaultNode(root.getChildNodes());
+		Node aciProportionMeasureNode = root.getChildNodes().get(0);
 
-		// This node is the place holder around the root node
 		assertThat("returned node should not be null", root, is(not(nullValue())));
 
-		// For all decoders this should be either a value or child node
-		assertThat("returned node should have one child node", root.getChildNodes().size(), is(1));
-		// This is the child node that is produced by the intended decoder
-		Node aciProportionMeasureNode = root.getChildNodes().get(0);
-		// Should have a aggregate count node 
+		assertThat("returned node should have one child node", root.getChildNodes(), hasSize(1));
+
 		assertThat("returned node should have two child decoder nodes", aciProportionMeasureNode.getChildNodes().size(), is(2));
 
 		assertThat("measureId should be " + MEASURE_ID,
 				aciProportionMeasureNode.getValue("measureId"), is(MEASURE_ID));
 
-		List<String> testTemplateIds = new ArrayList<>();
-		for (Node node : aciProportionMeasureNode.getChildNodes()) {
-			testTemplateIds.add(node.getId());
-		}
-
-		assertThat("Should have Numerator", testTemplateIds.contains(TemplateId.ACI_NUMERATOR.getTemplateId()), is(true));
-		assertThat("Should have Denominator", testTemplateIds.contains(TemplateId.ACI_DENOMINATOR.getTemplateId()), is(true));
+		assertThat("Should have Numerator", aciProportionMeasureNode.getChildNodes().get(0).getId(),
+				is(TemplateId.ACI_NUMERATOR.getTemplateId()));
+		assertThat("Should have Denominator", aciProportionMeasureNode.getChildNodes().get(1).getId(),
+				is(TemplateId.ACI_DENOMINATOR.getTemplateId()));
 	}
 
 	@Test
@@ -238,5 +183,59 @@ public class AciProportionMeasureDecoderTest {
 
 		//assert
 		assertThat("measureId should be " + MEASURE_ID, thisNode.getValue("measureId"), is(MEASURE_ID));
+	}
+
+	private String getValidXmlFragment() {
+		return XmlUtils.buildString(
+				"<?xml version=\"1.0\" encoding=\"utf-8\"?>\n"
+						+ "<entry xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns=\"urn:hl7-org:v3\">\n"
+						+ "	<organizer classCode=\"CLUSTER\" moodCode=\"EVN\">\n"
+						+ "		<!-- Implied template Measure Reference templateId -->\n"
+						+ "		<templateId root=\"2.16.840.1.113883.10.20.24.3.98\"/>\n"
+						+ "		<!-- ACI Numerator Denominator Type Measure Reference and Results templateId -->\n"
+						+ "		<templateId root=\"2.16.840.1.113883.10.20.27.3.28\" extension=\"2016-09-01\"/>\n"
+						+ "		<id root=\"ac575aef-7062-4ea2-b723-df517cfa470a\"/>\n"
+						+ "		<statusCode code=\"completed\"/>\n"
+						+ "		<reference typeCode=\"REFR\">\n"
+						+ "			<!-- Reference to a particular ACI measure's unique identifier. -->\n"
+						+ "			<externalDocument classCode=\"DOC\" moodCode=\"EVN\">\n"
+						+ "				<!-- This is a temporary root OID that indicates this is an ACI measure identifier -->\n"
+						+ "				<!-- extension is the unique identifier for an ACI measure. \"ACI-PEA-1\" is for illustration only. -->\n"
+						+ "				<id root=\"2.16.840.1.113883.3.7031\" extension=\"ACI-PEA-1\"/>\n"
+						+ "				<!-- ACI measure title -->\n"
+						+ "				<text>Patient Access</text>\n"
+						+ "			</externalDocument>\n"
+						+ "		</reference>\n"
+						+ "		<component>\n"
+						+ "			<observation classCode=\"OBS\" moodCode=\"EVN\">\n"
+						+ "				<!-- Performance Rate templateId -->\n"
+						+ "				<templateId root=\"2.16.840.1.113883.10.20.27.3.30\"\n"
+						+ "					extension=\"2016-09-01\"/>\n"
+						+ "				<code code=\"72510-1\" codeSystem=\"2.16.840.1.113883.6.1\" codeSystemName=\"LOINC\" displayName=\"Performance Rate\"/>\n"
+						+ "				<statusCode code=\"completed\"/>\n"
+						+ "				<value xsi:type=\"REAL\" value=\"0.750000\"/>\n"
+						+ "			</observation>\n"
+						+ "		</component>\n"
+						+ "		<component>\n"
+						+ "			<observation classCode=\"OBS\" moodCode=\"EVN\">\n"
+						+ "				<!-- ACI Numerator Denominator Type Measure Numerator Data templateId -->\n"
+						+ "				<templateId root=\"2.16.840.1.113883.10.20.27.3.31\" extension=\"2016-09-01\"/>\n"
+						+ "				<entryRelationship resultName=\"aciNumeratorDenominator\" resultValue=\"600\">\n"
+						+ "					<templateId root=\"Q.E.D\"/>\n"
+						+ "				</entryRelationship>"
+						+ "			</observation>\n"
+						+ "		</component>\n"
+						+ "		<component>\n"
+						+ "			<observation classCode=\"OBS\" moodCode=\"EVN\">\n"
+						+ "				<!-- ACI Numerator Denominator Type Measure Denominator Data templateId -->\n"
+						+ "				<templateId root=\"2.16.840.1.113883.10.20.27.3.32\" extension=\"2016-09-01\"/>\n"
+						+ "				<entryRelationship resultName=\"aciNumeratorDenominator\" resultValue=\"800\">\n"
+						+ "					<templateId root=\"Q.E.D\"/>\n"
+						+ "				</entryRelationship>"
+						+ "			</observation>\n"
+						+ "		</component>\n"
+						+ "	</organizer>\n"
+						+ "</entry>"
+		);
 	}
 }
