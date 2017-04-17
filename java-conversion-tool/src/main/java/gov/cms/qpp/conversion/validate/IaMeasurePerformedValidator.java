beebb4272ca1239e7173e7972802d40d5ef00e57
DaveLauta <dlauta@flexion.us>
2017-04-17 14:21:34 -0400

    QPPCT-27 Added IaMeasurePerformedValidator and test

---
+++ java-conversion-tool/src/main/java/gov/cms/qpp/conversion/validate/IaMeasurePerformedValidator.java
@@ -0,0 +1,53 @@
+package gov.cms.qpp.conversion.validate;
+
+import gov.cms.qpp.conversion.model.Node;
+import gov.cms.qpp.conversion.model.TemplateId;
+import gov.cms.qpp.conversion.model.ValidationError;
+import gov.cms.qpp.conversion.model.Validator;
+
+import java.util.List;
+
+/**
+ * Validates IA Measure Performed Value - expects a  Y or N
+ */
+@Validator(templateId = TemplateId.IA_MEASURE, required = true)
+public class IaMeasurePerformedValidator extends NodeValidator {
+
+	public static final String TYPE_ERROR = "Measure performed value is required and must be either a Y or an N.\n\t%s";
+	public static final String TOO_MANY_CHILDREN = "Measure performed must have exactly one child.\n\t%s";
+    private static final String FIELD = "measurePerformed";
+	/**
+	 * Validates a single IA Measure Performed Value {@link Node}.
+	 *
+	 * Validates the following.
+	 * <ul>
+	 *     <li>An string value named "measurePerformed" was decoded from the source element</li>
+	 *     <li>The string value is either a Y or an N</li>
+	 *</ul>
+	 *
+	 * @param node Node that represents a IA Measure Performed.
+	 */
+	@Override
+	protected void internalValidateSingleNode(Node node) {
+	//	check( node ).value( VALUE_ERROR, FIELD );
+		List<Node> children = node.getChildNodes();
+		if ( children == null || children.size() != 1 ){
+			addValidationError(new ValidationError(String.format(TOO_MANY_CHILDREN, node.toString())));
+			return;
+		}
+		String value = children.get(0).getValue(FIELD);
+		if (!( "Y".equals(value ) || "N".equals(value))){
+			addValidationError(new ValidationError(String.format(TYPE_ERROR, node.toString())));
+		}
+	}
+
+	/**
+	 * Checks the interdependancy of nodes in the parsed tree.
+	 * IA Measure Performed has no dependancies on other nodes in the document.
+	 * @param nodes The list of nodes to validate.
+	 */
+	@Override
+	protected void internalValidateSameTemplateIdNodes(List<Node> nodes) {
+		// No current cross node Aggregate Count validations
+	}
+}
---
+++ java-conversion-tool/src/test/java/gov/cms/qpp/conversion/validate/IaMeasurePerformedValidatorTest.java
@@ -0,0 +1,81 @@
+package gov.cms.qpp.conversion.validate;
+
+import gov.cms.qpp.conversion.model.Node;
+import gov.cms.qpp.conversion.model.TemplateId;
+import gov.cms.qpp.conversion.model.ValidationError;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.collection.IsEmptyCollection.empty;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test class for IaMeasurePerformedValidator
+ */
+public class IaMeasurePerformedValidatorTest {
+
+	/**
+	 * Validate a correct set of Nodes
+	 * @throws Exception
+	 */
+	@Test
+	public void internalValidateSingleNode() throws Exception {
+		Node iaMeasureNode = new Node(TemplateId.IA_MEASURE.getTemplateId());
+		Node iaMeasurePerformedNode = new Node(iaMeasureNode, TemplateId.ACI_MEASURE_PERFORMED.getTemplateId());
+		iaMeasureNode.addChildNode(iaMeasurePerformedNode);
+		iaMeasurePerformedNode.putValue("measurePerformed", "Y");
+
+		IaMeasurePerformedValidator validator = new IaMeasurePerformedValidator();
+		List<ValidationError> errors = validator.validateSingleNode(iaMeasureNode);
+		assertThat("no errors should be present", errors, empty());
+	}
+	/**
+	 * Validate an invalid value child Node
+	 * @throws Exception
+	 */
+	@Test
+	public void internalValidateSingleInvalidValueNode() throws Exception {
+		Node iaMeasureNode = new Node(TemplateId.IA_MEASURE.getTemplateId());
+		Node iaMeasurePerformedNode = new Node(iaMeasureNode, TemplateId.ACI_MEASURE_PERFORMED.getTemplateId());
+		iaMeasureNode.addChildNode(iaMeasurePerformedNode);
+		iaMeasurePerformedNode.putValue("measurePerformed", "abc");
+
+		IaMeasurePerformedValidator validator = new IaMeasurePerformedValidator();
+		List<ValidationError> errors = validator.validateSingleNode(iaMeasureNode);
+		assertThat("An invalid value error should be present", errors.size(), is(1));
+	}
+
+	/**
+	 * Validate a missing child
+	 * @throws Exception
+	 */
+	@Test
+	public void testMissingNode() throws Exception {
+		Node iaMeasureNode = new Node(TemplateId.IA_MEASURE.getTemplateId());
+
+		IaMeasurePerformedValidator validator = new IaMeasurePerformedValidator();
+		List<ValidationError> errors = validator.validateSingleNode(iaMeasureNode);
+		assertThat("A missing child errors should be present", errors.size(), is(1));
+	}
+	/**
+	 * Validate a missing child
+	 * @throws Exception
+	 */
+	@Test
+	public void testTooManyChildren() throws Exception {
+		Node iaMeasureNode = new Node(TemplateId.IA_MEASURE.getTemplateId());
+		Node iaMeasurePerformedNode1 = new Node(iaMeasureNode, TemplateId.ACI_MEASURE_PERFORMED.getTemplateId());
+		Node iaMeasurePerformedNode2 = new Node(iaMeasureNode, TemplateId.ACI_MEASURE_PERFORMED.getTemplateId());
+		iaMeasureNode.addChildNode(iaMeasurePerformedNode1);
+		iaMeasurePerformedNode1.putValue("measurePerformed", "Y");
+		iaMeasureNode.addChildNode(iaMeasurePerformedNode2);
+		iaMeasurePerformedNode1.putValue("measurePerformed", "N");
+
+		IaMeasurePerformedValidator validator = new IaMeasurePerformedValidator();
+		List<ValidationError> errors = validator.validateSingleNode(iaMeasureNode);
+		assertThat("A Too Many children errors should be present", errors.size(), is(1));
+	}
+
+}
