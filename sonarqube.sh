03cbe94b6536009aa69af17b6f6fd49ea5152300
halprin <pkendall@flexion.us>
2017-04-18 18:11:53 -0600

    Sonar clean-up

--- sonarqube.sh
+++ sonarqube.sh
@@ -9,7 +9,7 @@ elif [[ ! -z $CI_PULL_REQUEST ]]; then
 	export PR_NUMBER=${CI_PULL_REQUEST##*/}
 	echo "Doing preview SonarQube run on PR $PR_NUMBER"
 	./sonar-scanner-3.0.1.733/bin/sonar-scanner -Dsonar.analysis.mode=preview \
-		                                        -Dsonar.github.pullRequest=$PR_NUMBER \
+	                                            -Dsonar.github.pullRequest=${PR_NUMBER} \
 	                                            -Dsonar.github.repository=CMSgov/qpp-conversion-tool \
 	                                            -Dsonar.github.oauth=${SONAR_PR_KEY} \
 	                                            -Dsonar.login=${SONAR_KEY}
