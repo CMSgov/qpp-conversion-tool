c97f9d73f49faea370c11edf076a8767928e4659
saquino0827 <saquino@flexion.us>
2017-04-20 15:11:58 -0500

    QPPCT-24 : Add validation for no duplicated top level sections

--- java-conversion-tool/src/main/java/gov/cms/qpp/conversion/validate/ClinicalDocumentValidator.java
+++ java-conversion-tool/src/main/java/gov/cms/qpp/conversion/validate/ClinicalDocumentValidator.java
@@ -13,34 +13,54 @@ import java.util.List;
 @Validator(templateId = TemplateId.CLINICAL_DOCUMENT, required = true)
 public class ClinicalDocumentValidator extends NodeValidator {
 
-	public static final String CLINICAL_DOCUMENT_REQUIRED = "Clinical Document Node is required";
-	public static final String EXACTLY_ONE_DOCUMENT_ALLOWED = "Only one Clinical Document Node is allowed";
-	public static final String ONE_CHILD_REQUIRED = "Clinical Document Node must have at least one Aci "
-			+ "or Ia Section Node as a child";
+	protected static final String CLINICAL_DOCUMENT_REQUIRED = "Clinical Document Node is required";
+	protected static final String EXACTLY_ONE_DOCUMENT_ALLOWED = "Only one Clinical Document Node is allowed";
+	protected static final String ONE_CHILD_REQUIRED = "Clinical Document Node must have at least one Aci "
+			+ "or IA or eCQM Section Node as a child";
+	protected static final String CONTAINS_PROGRAM_NAME = "Clinical Document must have a program name";
+	protected static final String CONTAINS_PERFORMANCE_YEAR = "Clinical Document must have a performance year";
+	protected static final String CONTAINS_TAX_ID_NUMBER = "Clinical Document must have Tax Id Number (TIN)";
+	protected static final String NO_DUPLICATE_SECTIONS = "Clinical Document must not contain any duplicate ACI/IA/eCQM sections";
+	protected static final String REPORTING_PARAMETER_REQUIRED = "Clinical Document must have Report Parameters Section";
 
 	/**
-	 * Validates a single Clinical Document {@link gov.cms.qpp.conversion.model.Node}.
-	 *
-	 * <p>
+	 * Validates a single Clinical Document Node.
 	 * Validates the following.
 	 * <ul>
-	 *     <li>At least one child exists.</li>
-	 *     <li>At least one ACI or IA section exists.</li>
+	 * <li>At least one child exists.</li>
+	 * <li>At least one ACI or IA or eCQM (MEASURE_SECTION_V2) section exists.</li>
+	 * <li>Program name is required</li>
+	 * <li>TIN name is required</li>
+	 * <li>Performance year is required</li>
 	 * </ul>
 	 * </p>
 	 *
 	 * @param node Node that represents a Clinical Document.
 	 */
 	@Override
-	protected void internalValidateSingleNode(Node node) {
+	protected void internalValidateSingleNode(final Node node) {
 		check(node)
-			.hasChildren(ONE_CHILD_REQUIRED)
-			.childMinimum(ONE_CHILD_REQUIRED, 1, TemplateId.ACI_SECTION, TemplateId.IA_SECTION);
+				.hasChildren(ONE_CHILD_REQUIRED)
+				.childMinimum(ONE_CHIcLD_REQUIRED, 1,
+						TemplateId.ACI_SECTION, TemplateId.IA_SECTION, TemplateId.MEASURE_SECTION_V2)
+				.childMinimum(REPORTING_PARAMETER_REQUIRED, 1, TemplateId.REPORTING_PARAMETERS_SECTION)
+				.childMaximum(NO_DUPLICATE_SECTIONS, 1, TemplateId.ACI_SECTION)
+				.childMaximum(NO_DUPLICATE_SECTIONS, 1, TemplateId.IA_SECTION)
+				.childMaximum(NO_DUPLICATE_SECTIONS, 1, TemplateId.MEASURE_SECTION_V2)
+				.value(CONTAINS_PROGRAM_NAME, "programName")
+				.value(CONTAINS_TAX_ID_NUMBER, "taxpayerIdentificationNumber");
+
+		Node reportingParametersAct = node.findFirstNode(TemplateId.REPORTING_PARAMETERS_ACT.getTemplateId());
+		if (reportingParametersAct == null) {
+			getValidationErrors().add(new ValidationError(CONTAINS_PERFORMANCE_YEAR, node.getPath()));
+		} else {
+			check(reportingParametersAct).value(CONTAINS_PERFORMANCE_YEAR, "performanceStart");
+		}
 	}
 
 	/**
 	 * Validates all the Clinical Documents together.
-	 *
+	 * <p>
 	 * <p>
 	 * Validates that one and only one Clinical Document {@link gov.cms.qpp.conversion.model.Node} exists.
 	 * </p>
