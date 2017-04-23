0f983b4a0076e821dfb58e14a104e551d6fdfdbf
Clyde Tedrick <iamclyde@gmail.com>
2017-04-23 04:42:22 -0400

    QPPCT-68: verify IA_SECTION

--- java-conversion-tool/src/test/java/gov/cms/qpp/conversion/ScopedConversionTest.java
+++ java-conversion-tool/src/test/java/gov/cms/qpp/conversion/ScopedConversionTest.java
@@ -1,5 +1,6 @@
 package gov.cms.qpp.conversion;
 
+import gov.cms.qpp.BaseTest;
 import gov.cms.qpp.conversion.model.TemplateId;
 import org.junit.After;
 import org.junit.BeforeClass;
@@ -13,7 +14,7 @@ import java.util.HashMap;
 import static gov.cms.qpp.util.JsonHelper.readJson;
 import static junit.framework.TestCase.assertEquals;
 
-public class ScopedConversionTest {
+public class ScopedConversionTest extends BaseTest {
 
 	private static final String TEMPLATE_SCOPE = "--" + ConversionEntry.TEMPLATE_SCOPE;
 	private static HashMap<String,Object> FIXTURES;
@@ -32,6 +33,23 @@ public class ScopedConversionTest {
 	public void testScopedAciSectionConversion() throws IOException {
 		//setup
 		String testSection = TemplateId.ACI_SECTION.name();
+
+		//when
+		ConversionEntry.main(TEMPLATE_SCOPE, testSection,
+				"src/test/resources/valid-QRDA-III.xml");
+
+		HashMap<String,Object> content = readJson("valid-QRDA-III.qpp.json");
+
+		//then
+		assertEquals("content should match valid " + testSection + " fixture",
+				FIXTURES.get(testSection), content);
+	}
+
+
+	@Test
+	public void testScopedIaSectionConversion() throws IOException {
+		//setup
+		String testSection = TemplateId.IA_SECTION.name();
 
 		//when
 		ConversionEntry.main(TEMPLATE_SCOPE, testSection,
