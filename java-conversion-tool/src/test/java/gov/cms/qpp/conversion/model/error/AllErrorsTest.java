0734685fd979d4c5d6aadc3d5ab2c9666fd7ae5c
Clyde Tedrick <iamclyde@gmail.com>
2017-04-26 10:29:20 -0400

    increase coverage

--- java-conversion-tool/src/test/java/gov/cms/qpp/conversion/model/error/AllErrorsTest.java
+++ java-conversion-tool/src/test/java/gov/cms/qpp/conversion/model/error/AllErrorsTest.java
@@ -2,6 +2,8 @@ package gov.cms.qpp.conversion.model.error;
 
 import org.junit.Test;
 
+import java.util.Collections;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
 import static org.hamcrest.core.Is.is;
@@ -16,6 +18,31 @@ public class AllErrorsTest {
 		assertThat("The error sources should have been null at first", objectUnderTest.getErrorSources(), is(nullValue()));
 
 		objectUnderTest.addErrorSource(new ErrorSource());
+
+		assertThat("The error sources should no longer be null", objectUnderTest.getErrorSources(), is(not(nullValue())));
+		assertThat("The list should be one", objectUnderTest.getErrorSources(), hasSize(1));
+	}
+
+	@Test
+	public void addErrorSources() {
+		AllErrors objectUnderTest = new AllErrors();
+
+		assertThat("The error sources should have been null at first", objectUnderTest.getErrorSources(), is(nullValue()));
+
+		objectUnderTest.addErrorSource(new ErrorSource());
+		objectUnderTest.addErrorSource(new ErrorSource());
+
+		assertThat("The error sources should no longer be null", objectUnderTest.getErrorSources(), is(not(nullValue())));
+		assertThat("The list should be one", objectUnderTest.getErrorSources(), hasSize(2));
+	}
+
+	@Test
+	public void setErrorSources() {
+		AllErrors objectUnderTest = new AllErrors();
+
+		assertThat("The error sources should have been null at first", objectUnderTest.getErrorSources(), is(nullValue()));
+
+		objectUnderTest.setErrorSources(Collections.singletonList(new ErrorSource()));
 
 		assertThat("The error sources should no longer be null", objectUnderTest.getErrorSources(), is(not(nullValue())));
 		assertThat("The list should be one", objectUnderTest.getErrorSources(), hasSize(1));
