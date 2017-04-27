9a66bd0fa2b76579537c4062af9736ea906cea63
halprin <pkendall@flexion.us>
2017-04-27 16:51:14 -0600

    QPPCT-69: Ignore quality measures with stratum

--- java-conversion-tool/src/test/java/gov/cms/qpp/conversion/decode/QualityMeasureIdDecoderTest.java
+++ java-conversion-tool/src/test/java/gov/cms/qpp/conversion/decode/QualityMeasureIdDecoderTest.java
@@ -14,6 +14,8 @@ import static org.junit.Assert.assertThat;
  * Test for the QualityMeasureIdDecoder
  */
 public class QualityMeasureIdDecoderTest {
+	QualityMeasureIdDecoder objectUnderTest = new QualityMeasureIdDecoder();
+
 	/**
 	 * Tests the decoder for a valid xml fragment
 	 *
@@ -24,7 +26,7 @@ public class QualityMeasureIdDecoderTest {
 		QualityMeasureIdDecoder decoder = new QualityMeasureIdDecoder();
 
 		Node qualityMeasureIdNode = new Node();
-		Element qualityMeasureIdElement = XmlUtils.stringToDom(getXmlFragment());
+		Element qualityMeasureIdElement = XmlUtils.stringToDom(getXmlFragmentWithMeasureGuid("Measurement Id Value"));
 		decoder.setNamespace(qualityMeasureIdElement, decoder);
 		decoder.internalDecode(qualityMeasureIdElement, qualityMeasureIdNode);
 
@@ -39,29 +41,93 @@ public class QualityMeasureIdDecoderTest {
 	 */
 	@Test
 	public void internalDecodeMissingId() throws XmlException {
-		String xmlFragment = getXmlFragment().replace("<id ", "<noid ");
+		String xmlFragment = getXmlFragmentWithMeasureGuid("Measurement Id Value").replace("<id ", "<noid ");
 		QualityMeasureIdDecoder decoder = new QualityMeasureIdDecoder();
 
 		Node qualityMeasureIdNode = new Node();
 		Element qualityMeasureIdElement = XmlUtils.stringToDom(xmlFragment);
 		decoder.setNamespace(qualityMeasureIdElement, decoder);
-		decoder.internalDecode(qualityMeasureIdElement, qualityMeasureIdNode);
+		DecodeResult decodeResult = decoder.internalDecode(qualityMeasureIdElement, qualityMeasureIdNode);
 
+		assertThat("The incorrect DecodeResult was returned.", decodeResult, is(DecodeResult.TREE_CONTINUE));
 		String value = qualityMeasureIdNode.getValue("measureId");
 		assertThat("Expect to not have a value", value, is(nullValue()));
 	}
 
+	@Test
+	public void incorrectRoot() throws XmlException {
+		//set-up
+		Element qualityMeasureIdElement = XmlUtils.stringToDom(getBadXmlFragmentWithIncorrectRoot());
+		Node qualityMeasureIdNode = new Node();
 
-	private String getXmlFragment() {
-		return "<?xml version=\"1.0\" encoding=\"utf-8\"?>"
-				+ "<root xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns=\"urn:hl7-org:v3\">"
-				+ "	<templateId root=\"2.16.840.1.113883.10.20.27.3.17\" extension=\"2016-11-01\"/> "
-				+ "	<reference typeCode=\"REFR\"> "
-				+ " 	<externalDocument classCode=\"DOC\" moodCode=\"EVN\"> "
-				+ " 		<id root=\"2.16.840.1.113883.4.738\" extension=\"Measurement Id Value\"/> "
-				+ "		</externalDocument> "
-				+ " </reference> "
-				+ " </root>";
+		objectUnderTest.setNamespace(qualityMeasureIdElement, objectUnderTest);
+
+		//execute
+		DecodeResult decodeResult = objectUnderTest.internalDecode(qualityMeasureIdElement, qualityMeasureIdNode);
+
+		//assert
+		assertThat("The incorrect DecodeResult was returned.", decodeResult, is(DecodeResult.TREE_CONTINUE));
+		assertThat("The node should not have a value.", qualityMeasureIdNode.getValue("measureId"), is(nullValue()));
 	}
 
+	@Test
+	public void ignoreStratumMeasure1() throws XmlException {
+		ignoreStratumMeasure(getXmlFragmentWithMeasureGuid("40280381-528a-60ff-0152-8e089ed20376"));
+	}
+
+	@Test
+	public void ignoreStratumMeasure2() throws XmlException {
+		ignoreStratumMeasure(getXmlFragmentWithMeasureGuid("40280381-51f0-825b-0152-22b52da917ba"));
+	}
+
+	@Test
+	public void ignoreStratumMeasure3() throws XmlException {
+		ignoreStratumMeasure(getXmlFragmentWithMeasureGuid("40280381-51f0-825b-0152-22b695b217dc"));
+	}
+
+	@Test
+	public void ignoreStratumMeasure4() throws XmlException {
+		ignoreStratumMeasure(getXmlFragmentWithMeasureGuid("40280381-52fc-3a32-0153-1f6962df0f9c"));
+	}
+
+	private void ignoreStratumMeasure(String xmlFragment) throws XmlException {
+		//set-up
+		QualityMeasureIdDecoder decoder = new QualityMeasureIdDecoder();
+
+		Element qualityMeasureIdElement = XmlUtils.stringToDom(xmlFragment);
+		Node qualityMeasureIdNode = new Node();
+
+		decoder.setNamespace(qualityMeasureIdElement, decoder);
+
+		//execute
+		DecodeResult decodeResult = decoder.internalDecode(qualityMeasureIdElement, qualityMeasureIdNode);
+
+		//assert
+		assertThat("The incorrect DecodeResult was returned.", decodeResult, is(DecodeResult.TREE_ESCAPED));
+		assertThat("The node should not have a value.", qualityMeasureIdNode.getValue("measureId"), is(nullValue()));
+	}
+
+	private String getXmlFragmentWithMeasureGuid(String measureGuid) {
+		return "<?xml version=\"1.0\" encoding=\"utf-8\"?>"
+			       + "<root xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns=\"urn:hl7-org:v3\">"
+			       + "	<templateId root=\"2.16.840.1.113883.10.20.27.3.17\" extension=\"2016-11-01\"/> "
+			       + "	<reference typeCode=\"REFR\"> "
+			       + " 	<externalDocument classCode=\"DOC\" moodCode=\"EVN\"> "
+			       + " 		<id root=\"2.16.840.1.113883.4.738\" extension=\"" + measureGuid + "\"/> "
+			       + "		</externalDocument> "
+			       + " </reference> "
+			       + " </root>";
+	}
+
+	private String getBadXmlFragmentWithIncorrectRoot() {
+		return "<?xml version=\"1.0\" encoding=\"utf-8\"?>"
+			       + "<root xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns=\"urn:hl7-org:v3\">"
+			       + "	<templateId root=\"2.16.840.1.113883.10.20.27.3.17\" extension=\"2016-11-01\"/> "
+			       + "	<reference typeCode=\"REFR\"> "
+			       + " 	<externalDocument classCode=\"DOC\" moodCode=\"EVN\"> "
+			       + " 		<id root=\"2.16.840.1.113883.4.739\" extension=\"measure GUID\"/> "
+			       + "		</externalDocument> "
+			       + " </reference> "
+			       + " </root>";
+	}
 }
