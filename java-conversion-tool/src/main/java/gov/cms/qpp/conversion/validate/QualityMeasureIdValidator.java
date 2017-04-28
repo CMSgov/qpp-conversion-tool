6da663804e00278a88de8ce80b8adaaf74690c94
halprin <pkendall@flexion.us>
2017-04-28 14:06:52 -0600

    QPPCT-69: Added JavaDocs to validator

--- java-conversion-tool/src/main/java/gov/cms/qpp/conversion/validate/QualityMeasureIdValidator.java
+++ java-conversion-tool/src/main/java/gov/cms/qpp/conversion/validate/QualityMeasureIdValidator.java
@@ -6,8 +6,20 @@ import gov.cms.qpp.conversion.model.Validator;
 
 import java.util.List;
 
+/**
+ * Validates a Measure Reference Results node.
+ */
 @Validator(templateId = TemplateId.MEASURE_REFERENCE_RESULTS_CMS_V2)
 public class QualityMeasureIdValidator extends NodeValidator {
+	/**
+	 * Validates that the Measure Reference Results node contains...
+	 * <ul>
+	 *     <li>A measure GUID.</li>
+	 *     <li>At least one quality measure.</li>
+	 * </ul>
+	 *
+	 * @param node The node to validate.
+	 */
 	@Override
 	protected void internalValidateSingleNode(final Node node) {
 		check(node)
@@ -15,6 +27,11 @@ public class QualityMeasureIdValidator extends NodeValidator {
 			.childMinimum("The measure reference results must have at least one measure", 1, TemplateId.MEASURE_DATA_CMS_V2);
 	}
 
+	/**
+	 * Does nothing.
+	 *
+	 * @param nodes The list of nodes to validate.
+	 */
 	@Override
 	protected void internalValidateSameTemplateIdNodes(final List<Node> nodes) {
 		//no cross-node validations required
