6e589f740ea7dd72434be4517d1e696688fb32d1
halprin <pkendall@flexion.us>
2017-05-02 12:55:17 -0600

    Added child verification

--- java-conversion-tool/src/main/java/gov/cms/qpp/conversion/validate/AciSectionValidator.java
+++ java-conversion-tool/src/main/java/gov/cms/qpp/conversion/validate/AciSectionValidator.java
@@ -9,10 +9,15 @@ import java.util.List;
 @Validator(templateId = TemplateId.ACI_SECTION, required = true)
 public class AciSectionValidator extends NodeValidator {
 
+	protected static final String ACI_NUMERATOR_DENOMINATOR_NODE_REQUIRED =
+		"At least one Aci Numerator Denominator Measure Node is required";
+	protected static final String NO_REQUIRED_MEASURE =
+		"The required measure ''{0}'' is not present in the source file. "
+			+ "Please add the ACI measure and try again.";
 
 	@Override
 	protected void internalValidateSingleNode(final Node node) {
-
+		check(node).childMinimum(ACI_NUMERATOR_DENOMINATOR_NODE_REQUIRED, 1, TemplateId.ACI_NUMERATOR_DENOMINATOR);
 	}
 
 	@Override
