ebdecbfc69fe9e1c790ba3ff4b719c9d3b5494ab
Seth Alt <salt@flexion.us>
2017-05-03 10:24:01 -0500

    QPPCT_78 Add starter test file

---
+++ frontend/src/app/app.component.spec.ts
@@ -0,0 +1,8 @@
+import {async, ComponentFixture} from '@angular/core/testing';
+import {AppComponent} from './app.component';
+import {configureTests} from '../tests.configure';
+
+describe('AppComponent', () => {
+	const fixture: ComponentFixture<AppComponent>;
+	const app: AppComponent;
+};
