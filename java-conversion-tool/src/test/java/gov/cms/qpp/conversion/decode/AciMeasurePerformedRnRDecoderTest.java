ab6d1e846453148571383ec68cec66e076ea587e
halprin <pkendall@flexion.us>
2017-05-04 11:16:39 -0600

    QPPCT-75: Moved xml fragment to a fixture file

--- java-conversion-tool/src/test/java/gov/cms/qpp/conversion/decode/AciMeasurePerformedRnRDecoderTest.java
+++ java-conversion-tool/src/test/java/gov/cms/qpp/conversion/decode/AciMeasurePerformedRnRDecoderTest.java
@@ -1,25 +1,29 @@
 package gov.cms.qpp.conversion.decode;
 
+import gov.cms.qpp.BaseTest;
 import gov.cms.qpp.conversion.model.Node;
+import gov.cms.qpp.conversion.model.TemplateId;
 import gov.cms.qpp.conversion.xml.XmlException;
 import gov.cms.qpp.conversion.xml.XmlUtils;
 import org.jdom2.Element;
 import org.jdom2.Namespace;
 import org.junit.Test;
 
+import java.io.IOException;
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.core.IsNot.not;
 import static org.hamcrest.core.IsNull.nullValue;
 import static org.junit.Assert.assertThat;
 
-public class AciMeasurePerformedRnRDecoderTest {
+public class AciMeasurePerformedRnRDecoderTest extends BaseTest {
 	private static final String MEASURE_ID = "ACI_INFBLO_1";
-
-	private AciMeasurePerformedRnRDecoder objectUnderTest = new AciMeasurePerformedRnRDecoder();
 
 	@Test
 	public void internalDecodeReturnsTreeContinue() {
 		//set-up
+		AciMeasurePerformedRnRDecoder objectUnderTest = new AciMeasurePerformedRnRDecoder();
+		
 		Namespace rootns = Namespace.getNamespace("urn:hl7-org:v3");
 		Namespace ns = Namespace.getNamespace("xsi", "http://www.w3.org/2001/XMLSchema-instance");
 
@@ -51,48 +55,18 @@ public class AciMeasurePerformedRnRDecoderTest {
 	}
 
 	@Test
-	public void testUpperLevel() throws XmlException {
-		Node wrapperNode = new QppXmlDecoder().decode(XmlUtils.stringToDom(getValidXmlFragment()));
+	public void testUpperLevel() throws XmlException, IOException {
+		String needsFormattingXml = getFixture("AciMeasurePerformedIsolated.xml");
+		String xml = String.format(needsFormattingXml, MEASURE_ID);
+		Node wrapperNode = new QppXmlDecoder().decode(XmlUtils.stringToDom(xml));
 		Node aciMeasurePerformedNode = wrapperNode.getChildNodes().get(0);
 
 		String actualMeasureId = aciMeasurePerformedNode.getValue("measureId");
 
 		assertThat("The measureId must not be null.", actualMeasureId, is(not(nullValue())));
 		assertThat("The measureId is incorrect.", actualMeasureId, is(MEASURE_ID));
-	}
-
-	private String getValidXmlFragment() {
-		return XmlUtils.buildString(
-			"<root xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns=\"urn:hl7-org:v3\">",
-			"\n<entry>",
-			"\n<organizer classCode=\"CLUSTER\" moodCode=\"EVN\">",
-			"\n<!-- Implied template Measure Reference templateId -->",
-			"\n<templateId root=\"2.16.840.1.113883.10.20.24.3.98\"/>",
-			"\n<!-- ACI Numerator Denominator Type Measure Reference and Results templateId -->",
-			"\n<templateId root=\"2.16.840.1.113883.10.20.27.3.29\" extension=\"2016-09-01\"/>",
-			"\n<id root=\"ac575aef-7062-4ea2-b723-df517cfa470a\"/>",
-			"\n<statusCode code=\"completed\"/>",
-			"\n<reference typeCode=\"REFR\">",
-			"\n <!-- Reference to a particular ACI measure's unique identifier. -->",
-			"\n <externalDocument classCode=\"DOC\" moodCode=\"EVN\">",
-			"\n     <!-- This is a temporary root OID that indicates this is an ACI measure identifier -->",
-			"\n     <!-- extension is the unique identifier for an ACI measure. \"ACI-PEA-1\" is for illustration only. -->",
-			"\n     <id root=\"2.16.840.1.113883.3.7031\" extension=\"" + MEASURE_ID + "\"/>",
-			"\n     <!-- ACI measure title -->",
-			"\n     <text>Patient Access</text>",
-			"\n </externalDocument>",
-			"\n</reference>",
-			"\n<component>",
-			"\n<observation classCode=\"OBS\" moodCode=\"EVN\">",
-			"\n     <!-- Measure Performed templateId -->",
-			"\n     <templateId root=\"2.16.840.1.113883.10.20.27.3.27\" extension=\"2016-09-01\"/>",
-			"\n     <code code=\"ASSERTION\" codeSystem=\"2.16.840.1.113883.5.4\" codeSystemName=\"ActCode\" displayName=\"Assertion\"/>",
-			"\n     <statusCode code=\"completed\"/>",
-			"\n     <value xsi:type=\"CD\" code=\"Y\" displayName=\"Yes\" codeSystemName=\"Yes/no indicator (HL7 Table 0136)\" codeSystem=\"2.16.840.1.113883.12.136\"/>",
-			"\n</observation>",
-			"\n</component>",
-			"\n</organizer>",
-			"\n</entry>",
-			"\n</root>\n");
+		long measurePerformedCount = aciMeasurePerformedNode.getChildNodes(
+			node -> node.getId().equals(TemplateId.MEASURE_PERFORMED.getTemplateId())).count();
+		assertThat("There must be one Measure Performed child node.", measurePerformedCount, is(1L));
 	}
 }
