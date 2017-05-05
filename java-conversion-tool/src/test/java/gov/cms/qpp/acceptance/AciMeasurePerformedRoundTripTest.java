c1459ff2a0b8b8ee8898a07a2daef8bb466d9d3b
halprin <pkendall@flexion.us>
2017-05-05 10:43:12 -0600

    QPPCT-82: Added initial fixture and test

---
+++ java-conversion-tool/src/test/java/gov/cms/qpp/acceptance/AciMeasurePerformedRoundTripTest.java
@@ -0,0 +1,11 @@
+package gov.cms.qpp.acceptance;
+
+import gov.cms.qpp.BaseTest;
+import org.junit.Test;
+
+public class AciMeasurePerformedRoundTripTest extends BaseTest{
+	@Test
+	public void testGarbage() {
+
+	}
+}
