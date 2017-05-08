d0f283e5a8b0b1448149ac3efbc0791b1eea22fb
saquino0827 <saquino@flexion.us>
2017-05-08 10:31:45 -0500

    QPPCT-86 : Refactor IaSectionDecoderTest - Add a new IaSection Fixture - Refactor tests to use a better method of finding nodes.

--- java-conversion-tool/src/test/java/gov/cms/qpp/conversion/decode/IaSectionDecoderTest.java
+++ java-conversion-tool/src/test/java/gov/cms/qpp/conversion/decode/IaSectionDecoderTest.java
@@ -1,12 +1,11 @@
 package gov.cms.qpp.conversion.decode;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.not;
-import static org.hamcrest.CoreMatchers.nullValue;
 import static org.junit.Assert.assertThat;
 
+import gov.cms.qpp.BaseTest;
+import gov.cms.qpp.conversion.model.TemplateId;
 import gov.cms.qpp.conversion.xml.XmlException;
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -14,19 +13,21 @@ import gov.cms.qpp.conversion.decode.placeholder.DefaultDecoder;
 import gov.cms.qpp.conversion.model.Node;
 import gov.cms.qpp.conversion.xml.XmlUtils;
 
-public class IaSectionDecoderTest {
+import java.io.IOException;
+
+public class IaSectionDecoderTest extends BaseTest {
 	private String xmlFragment;
 
 	@Before
-	public void setUp() {
-		xmlFragment = createAciSectionXmlFragment();
+	public void setUp() throws IOException {
+		xmlFragment = getFixture("IaSection.xml");
 	}
 
 	@Test
 	public void decodeAciSectionAsNode() throws XmlException {
 		Node root = executeDecoderWithoutDefaults();
 
-		Node iaSectionNode = root.getChildNodes().get(0);
+		Node iaSectionNode = root.findFirstNode(TemplateId.IA_SECTION.getTemplateId());
 
 		assertThat("returned category", iaSectionNode.getValue("category"), is("ia"));
 	}
@@ -35,7 +36,7 @@ public class IaSectionDecoderTest {
 	public void testDecodeIaSectionContainsIaMeasureSectionId() throws XmlException {
 		Node root = executeDecoderWithoutDefaults();
 
-		Node iaMeasureNode = root.getChildNodes().get(0).getChildNodes().get(0);
+		Node iaMeasureNode = root.findFirstNode(TemplateId.IA_MEASURE.getTemplateId());
 
 		assertThat("returned should have measureId", iaMeasureNode.getValue("measureId"), is("IA_EPA_1"));
 	}
@@ -44,7 +45,7 @@ public class IaSectionDecoderTest {
 	public void testDecodeIaMeasureSectionContainsMeasurePerformed() throws XmlException {
 		Node root = executeDecoderWithoutDefaults();
 
-		Node iaMeasurePerformedNode = root.getChildNodes().get(0).getChildNodes().get(0).getChildNodes().get(0);
+		Node iaMeasurePerformedNode = root.findFirstNode(TemplateId.MEASURE_PERFORMED.getTemplateId());
 		assertThat("returned measurePerformed", iaMeasurePerformedNode.getValue("measurePerformed"), is("Y"));
 	}
 
@@ -54,7 +55,7 @@ public class IaSectionDecoderTest {
 				"\n<Stuff arbitrary=\"123\"><newnode>Some extra stuff</newnode></Stuff>Unexpected stuff appears here\n\n<statusCode ");
 
 		Node root = executeDecoderWithoutDefaults();
-		Node iaSectionNode = root.getChildNodes().get(0);
+		Node iaSectionNode = root.findFirstNode(TemplateId.IA_SECTION.getTemplateId());
 
 		assertThat("returned category", iaSectionNode.getValue("category"), is("ia"));
 	}
@@ -63,47 +64,5 @@ public class IaSectionDecoderTest {
 		Node root = new QppXmlDecoder().decode(XmlUtils.stringToDom(xmlFragment));
 		DefaultDecoder.removeDefaultNode(root.getChildNodes());
 		return root;
-	}
-
-	private String createAciSectionXmlFragment() {
-		return "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" +
-		"<component xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns=\"urn:hl7-org:v3\">\n" +
-		"	<section>\n" +
-		"		<!-- Measure Section -->\n" +
-		"		<templateId root=\"2.16.840.1.113883.10.20.24.2.2\"/>\n" +
-		"		<!-- Improvement Activity Section templateId -->\n" +
-		"		<templateId root=\"2.16.840.1.113883.10.20.27.2.4\" extension=\"2016-09-01\"/>\n" +
-		"		<code code=\"55186-1\" codeSystem=\"2.16.840.1.113883.6.1\" displayName=\"Measure Section\"/>\n" +
-		"		<entry>\n" +
-		"			<organizer classCode=\"CLUSTER\" moodCode=\"EVN\">\n" +
-		"				<!-- Implied template Measure Reference templateId -->\n" +
-		"				<templateId root=\"2.16.840.1.113883.10.20.24.3.98\"/>\n" +
-		"				<!-- Improvement Activity Performed Reference and Results templateId -->\n" +
-		"				<templateId root=\"2.16.840.1.113883.10.20.27.3.33\" extension=\"2016-09-01\"/>\n" +
-		"				<id root=\"ac575aef-7062-4ea2-b723-df517cfa470a\"/>\n" +
-		"				<statusCode code=\"completed\"/>\n" +
-		"				<reference typeCode=\"REFR\">\n" +
-		"					<!-- Reference to a particular ACI measure's unique identifier. -->\n" +
-		"					<externalDocument classCode=\"DOC\" moodCode=\"EVN\">\n" +
-		"						<!-- This is a temporary root OID that indicates this is an improvement activity identifier -->\n" +
-		"						<!-- extension is the unique identifier for an improvement activity. \"IA_EPA_1\" is for illustration only. -->\n" +
-		"						<id root=\"2.16.840.1.113883.3.7034\" extension=\"IA_EPA_1\"/>\n" +
-		"						<!-- Improvement activity narrative text (for reference) -->\n" +
-		"						<text> Collection of patient experience and satisfaction data on										access to care and development of an improvement plan, such										as outlining steps for improving communications with										patients to help understanding of urgent access needs.									</text>\n" +
-		"					</externalDocument>\n" +
-		"				</reference>\n" +
-		"				<component>\n" +
-		"					<observation classCode=\"OBS\" moodCode=\"EVN\">\n" +
-		"						<!-- Measure Performed templateId -->\n" +
-		"						<templateId root=\"2.16.840.1.113883.10.20.27.3.27\" extension=\"2016-09-01\"/>\n" +
-		"						<code code=\"ASSERTION\" codeSystem=\"2.16.840.1.113883.5.4\" codeSystemName=\"ActCode\" displayName=\"Assertion\"/>\n" +
-		"						<statusCode code=\"completed\"/>\n" +
-		"						<value xsi:type=\"CD\" code=\"Y\" displayName=\"Yes\" codeSystemName=\"Yes/no indicator (HL7 Table 0136)\" codeSystem=\"2.16.840.1.113883.12.136\"/>\n" +
-		"					</observation>\n" +
-		"				</component>\n" +
-		"			</organizer>\n" +
-		"		</entry>" +
-		"	</section>\n" +
-		"</component>";
 	}
 }
