2f013307e6441d7723dd57a7ada77da70afedd32
Clyde Tedrick <iamclyde@gmail.com>
2017-05-08 16:34:06 -0400

    QPPCT-87: Clinical Document assertions

--- lambdize/src/test/java/gov/cms/qpp/conversion/ScopeTest.java
+++ lambdize/src/test/java/gov/cms/qpp/conversion/ScopeTest.java
@@ -9,12 +9,14 @@ import com.amazonaws.services.s3.model.S3ObjectSummary;
 import com.jayway.jsonpath.PathNotFoundException;
 import gov.cms.qpp.conversion.aws.history.HistoricalTestRunner;
 import gov.cms.qpp.conversion.util.JsonHelper;
+import net.minidev.json.JSONArray;
 import org.apache.commons.cli.ParseException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
@@ -24,6 +26,8 @@ import static com.amazonaws.util.IOUtils.copy;
 import static gov.cms.qpp.conversion.ConversionEntry.checkFlags;
 import static gov.cms.qpp.conversion.ConversionEntry.cli;
 import static gov.cms.qpp.conversion.ConversionEntry.validatedScope;
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertTrue;
 import static junit.framework.TestCase.fail;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
@@ -43,15 +47,30 @@ public class ScopeTest {
 	}
 
 	@Test
+	@SuppressWarnings("unchecked")
 	public void historicalClinicalDocumentScope() throws IOException, ParseException {
 		//setup
 		String[] args = {"-t", "CLINICAL_DOCUMENT", "-b"};
 		validatedScope(checkFlags(cli(args)));
+		String errorMessage =
+				"Clinical Document Node must have at least one Aci or IA or eCQM Section Node as a child";
 
-		//expect
-		List<List<Map<String, ?>>> results =
-				iterateBucketContents(convertEm("$.errorSources", TransformationStatus.ERROR));
+		//when
+		List<JSONArray> results =
+				iterateBucketContents(
+						convertEm("$.errorSources[*].validationErrors[*]", TransformationStatus.ERROR));
+
+		//then
+		results.stream()
+				.flatMap(Collection::stream)
+				.forEach( r -> {
+			Map<String, String> result = (Map<String, String>) r;
+			assertEquals(result.get("errorText"),
+					"Clinical Document Node must have at least one Aci or IA or eCQM Section Node as a child");
+			assertTrue(result.get("path").contains("ClinicalDocument"));
+		});
 	}
+
 
 	/**
 	 * Iterate over qrda history bucket contents and apply specified conversion.
@@ -59,11 +78,12 @@ public class ScopeTest {
 	 * @param conversion the conversion to be applied
 	 * @return a list of results
 	 */
-	private List<List<Map<String, ?>>> iterateBucketContents(Function<S3Object, List<Map<String, ?>>> conversion) {
+	@SuppressWarnings("unchecked")
+	private <T> T iterateBucketContents(Function<S3Object, List<?>> conversion) {
 		AmazonS3 s3Client = AmazonS3ClientBuilder.standard().withRegion("us-east-1").build();
 		ObjectListing listing
 				= s3Client.listObjects(BUCKET);
-		return listing.getObjectSummaries().parallelStream()
+		return (T) listing.getObjectSummaries().parallelStream()
 				.map(S3ObjectSummary::getKey)
 				.map(key -> s3Client.getObject(BUCKET, key))
 				.map(conversion)
@@ -78,7 +98,7 @@ public class ScopeTest {
 	 * @return a conversion function
 	 */
 	@SuppressWarnings("unchecked")
-	private Function<S3Object, List<Map<String, ?>>> convertEm(String jsonPath, TransformationStatus expectedStatus) {
+	private Function<S3Object, List<?>> convertEm(String jsonPath, TransformationStatus expectedStatus) {
 		return s3Object -> {
 			Converter convert = null;
 			TransformationStatus status = TransformationStatus.ERROR;
@@ -95,7 +115,7 @@ public class ScopeTest {
 			assertThat("The transformation resulted as expected. i.e. " + expectedStatus,
 					status, is(expectedStatus));
 
-			List<Map<String, ?>> retrieved = null;
+			List<?> retrieved = null;
 			try(InputStream jsonStream = convert.getConversionResult()){
 				retrieved = JsonHelper.readJsonAtJsonPath(jsonStream,
 						jsonPath, List.class);
