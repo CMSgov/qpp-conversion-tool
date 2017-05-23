bc275d2c27f57df29bdf88ceec72110a44253fe6
Adam Harrison <ad@mharrison.us>
2017-05-23 15:34:23 -0400

    Refactor Extension constants

--- java-conversion-tool/src/main/java/gov/cms/qpp/conversion/model/TemplateId.java
+++ java-conversion-tool/src/main/java/gov/cms/qpp/conversion/model/TemplateId.java
@@ -6,27 +6,27 @@ import gov.cms.qpp.conversion.ConversionEntry;
  * An enumeration of known templates IDs.
  */
 public enum TemplateId {
-	CLINICAL_DOCUMENT("2.16.840.1.113883.10.20.27.1.2", Constants.NOVEMBER_EXTENSION),
+	CLINICAL_DOCUMENT("2.16.840.1.113883.10.20.27.1.2", Extension.NOVEMBER),
 	ACI_AGGREGATE_COUNT("2.16.840.1.113883.10.20.27.3.3"),
-	IA_SECTION("2.16.840.1.113883.10.20.27.2.4", Constants.SEPTEMBER_EXTENSION),
-	ACI_SECTION("2.16.840.1.113883.10.20.27.2.5", Constants.SEPTEMBER_EXTENSION),
-	MEASURE_PERFORMED("2.16.840.1.113883.10.20.27.3.27", Constants.SEPTEMBER_EXTENSION),
-	ACI_NUMERATOR_DENOMINATOR("2.16.840.1.113883.10.20.27.3.28", Constants.SEPTEMBER_EXTENSION),
-	ACI_NUMERATOR("2.16.840.1.113883.10.20.27.3.31", Constants.SEPTEMBER_EXTENSION),
-	ACI_DENOMINATOR("2.16.840.1.113883.10.20.27.3.32", Constants.SEPTEMBER_EXTENSION),
-	IA_MEASURE("2.16.840.1.113883.10.20.27.3.33", Constants.SEPTEMBER_EXTENSION),
-	REPORTING_PARAMETERS_ACT("2.16.840.1.113883.10.20.27.3.23", Constants.NOVEMBER_EXTENSION),
-	MEASURE_DATA_CMS_V2("2.16.840.1.113883.10.20.27.3.16", Constants.NOVEMBER_EXTENSION),
-	MEASURE_SECTION_V2("2.16.840.1.113883.10.20.27.2.3", Constants.NOVEMBER_EXTENSION),
-	REPORTING_PARAMETERS_SECTION("2.16.840.1.113883.10.20.27.2.6", Constants.NOVEMBER_EXTENSION),
-	MEASURE_REFERENCE_RESULTS_CMS_V2("2.16.840.1.113883.10.20.27.3.17", Constants.NOVEMBER_EXTENSION),
-	ACI_MEASURE_PERFORMED_REFERENCE_AND_RESULTS("2.16.840.1.113883.10.20.27.3.29", Constants.SEPTEMBER_EXTENSION),
+	IA_SECTION("2.16.840.1.113883.10.20.27.2.4", Extension.SEPTEMBER),
+	ACI_SECTION("2.16.840.1.113883.10.20.27.2.5", Extension.SEPTEMBER),
+	MEASURE_PERFORMED("2.16.840.1.113883.10.20.27.3.27", Extension.SEPTEMBER),
+	ACI_NUMERATOR_DENOMINATOR("2.16.840.1.113883.10.20.27.3.28", Extension.SEPTEMBER),
+	ACI_NUMERATOR("2.16.840.1.113883.10.20.27.3.31", Extension.SEPTEMBER),
+	ACI_DENOMINATOR("2.16.840.1.113883.10.20.27.3.32", Extension.SEPTEMBER),
+	IA_MEASURE("2.16.840.1.113883.10.20.27.3.33", Extension.SEPTEMBER),
+	REPORTING_PARAMETERS_ACT("2.16.840.1.113883.10.20.27.3.23", Extension.NOVEMBER),
+	MEASURE_DATA_CMS_V2("2.16.840.1.113883.10.20.27.3.16", Extension.NOVEMBER),
+	MEASURE_SECTION_V2("2.16.840.1.113883.10.20.27.2.3", Extension.NOVEMBER),
+	REPORTING_PARAMETERS_SECTION("2.16.840.1.113883.10.20.27.2.6", Extension.NOVEMBER),
+	MEASURE_REFERENCE_RESULTS_CMS_V2("2.16.840.1.113883.10.20.27.3.17", Extension.NOVEMBER),
+	ACI_MEASURE_PERFORMED_REFERENCE_AND_RESULTS("2.16.840.1.113883.10.20.27.3.29", Extension.SEPTEMBER),
 
 	//unimplemented
-	PERFORMANCE_RATE("2.16.840.1.113883.10.20.27.3.30", Constants.SEPTEMBER_EXTENSION),
+	PERFORMANCE_RATE("2.16.840.1.113883.10.20.27.3.30", Extension.SEPTEMBER),
 	CONTINUOUS_VARIABLE_MEASURE_VALUE_CMS("2.16.840.1.113883.10.20.27.3.26"),
 	REPORTING_STRATUM_CMS("2.16.840.1.113883.10.20.27.3.20"),
-	PERFORMANCE_RATE_PROPORTION_MEASURE_CMS_V2("2.16.840.1.113883.10.20.27.3.25", Constants.NOVEMBER_EXTENSION),
+	PERFORMANCE_RATE_PROPORTION_MEASURE_CMS_V2("2.16.840.1.113883.10.20.27.3.25", Extension.NOVEMBER),
 	CMS_AGGREGATE_COUNT("2.16.840.1.113883.10.20.27.3.24"),
 
 	//miscellaneous
@@ -38,27 +38,34 @@ public enum TemplateId {
 	/**
 	 * Defined TemplateId Constants
 	 */
-	private static class Constants {
-		public static final String SEPTEMBER_EXTENSION = "2016-09-01";
-		public static final String NOVEMBER_EXTENSION = "2016-11-01";
+	private enum Extension {
+		NONE(""),
+		SEPTEMBER("2016-09-01"),
+		NOVEMBER("2016-11-01");
 
-		private Constants() {
-			//Constructor to ensure Constants will not be instantiated
+		private final String extension;
+
+		private Extension(String extension) {
+			this.extension = extension;
+		}
+
+		@Override
+		public String toString()
+		{
+			return this.extension;
 		}
 	}
 
 	private final String root;
-
-	private final String extension;
+	private final Extension extension;
 
 	/**
 	 * Constructs a TemplateId with just a root.
 	 *
 	 * @param root The root of the template ID.  Normally numbers with decimal points in between.
 	 */
-	TemplateId(final String root) {
-		this.root = root;
-		extension = "";
+	TemplateId(String root) {
+		this(root, Extension.NONE);
 	}
 
 	/**
@@ -67,7 +74,7 @@ public enum TemplateId {
 	 * @param root The root of the template ID.  Normally numbers with decimal points in between.
 	 * @param extension The extension of the template ID.  Normally a date.
 	 */
-	TemplateId(final String root, final String extension) {
+	TemplateId(String root, Extension extension) {
 		this.root = root;
 		this.extension = extension;
 	}
@@ -83,7 +90,7 @@ public enum TemplateId {
 	 * @return The extension of the template ID.
 	 */
 	public String getExtension() {
-		return extension;
+		return extension.toString();
 	}
 
 	/**
@@ -91,7 +98,7 @@ public enum TemplateId {
 	 * extension.
 	 */
 	public String getTemplateId() {
-		return generateTemplateIdString(root, extension);
+		return generateTemplateIdString(root, getExtension());
 	}
 
 	/**
@@ -116,7 +123,7 @@ public enum TemplateId {
 	 * @param extension The extension part of the templateId.
 	 * @return The template ID if found.  Else {@code TemplateId.DEFAULT}.
 	 */
-	public static TemplateId getTypeById(final String root, final String extension) {
+	public static TemplateId getTypeById(String root, String extension) {
 		for (TemplateId currentTemplateId : TemplateId.values()) {
 			if (ConversionEntry.isHistorical() && currentTemplateId.getRoot().equals(root)) {
 				return currentTemplateId;
@@ -134,7 +141,7 @@ public enum TemplateId {
 	 * @param extension The extension part of the templateId.
 	 * @return A string that concatenates the arguments the same way the enumeration does.
 	 */
-	public static String generateTemplateIdString(final String root, final String extension) {
+	public static String generateTemplateIdString(String root, String extension) {
 		String templateId = root;
 
 		if (!ConversionEntry.isHistorical() && extension != null && !extension.isEmpty()) {
