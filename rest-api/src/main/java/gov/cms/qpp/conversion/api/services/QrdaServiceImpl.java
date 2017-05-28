6703c772f72f9908a8185de203796c221f325bec
halprin <pkendall@flexion.us>
2017-05-27 19:39:17 -0600

    QPPCT-114: Removed extraneous try/catch block because logging is working

--- rest-api/src/main/java/gov/cms/qpp/conversion/api/services/QrdaServiceImpl.java
+++ rest-api/src/main/java/gov/cms/qpp/conversion/api/services/QrdaServiceImpl.java
@@ -24,21 +24,11 @@ public class QrdaServiceImpl implements QrdaService {
 	 */
 	@Override
 	public ConversionResult convertQrda3ToQpp(final InputStream fileInputStream) throws IOException {
-		InputStream conversionResult;
-		TransformationStatus status;
+		Converter converter = new Converter(fileInputStream);
+		converter.transform();
 
-		try {
-			Converter converter = new Converter(fileInputStream);
-			converter.transform();
-
-			conversionResult = converter.getConversionResult();
-			status = converter.getStatus();
-		} catch (Exception exception) {
-			//here because the logging doesn't work yet
-			System.out.println("Exception!");
-			exception.printStackTrace();
-			throw exception;
-		}
+		InputStream conversionResult = converter.getConversionResult();
+		TransformationStatus status = converter.getStatus();
 
 		String stringConversionResult = IOUtils.toString(conversionResult, "UTF-8");
 		//need to work on optimization so this doesn't try to copy a potentially large QPP file
