ed6b90b7b9ce8be1e8e5dc8b38b86011db1916c7
David Uselmann <duselmann@compcc.com>
2017-05-31 08:34:22 -0500

    run with jar-with-dependencies

--- benchmark/benchmark.sh
+++ benchmark/benchmark.sh
@@ -2,4 +2,4 @@
 
 # build and run the benchmark project
 mvn package
-java -jar target/benchmarks.jar >benchmarks.log 2>benchmarks.err
+java -jar target/benchmark-jar-with-dependencies.jar >benchmarks.log 2>benchmarks.err
