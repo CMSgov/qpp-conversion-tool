75cefcde0e12ec85e6648f154abf2df350debdd3
Adam Harrison <ad@mharrison.us>
2017-05-31 11:20:01 -0400

    Ignore payer, ethnicity, sex, race

--- java-conversion-tool/src/main/java/gov/cms/qpp/conversion/decode/placeholder/IgnoredDecoder.java
+++ java-conversion-tool/src/main/java/gov/cms/qpp/conversion/decode/placeholder/IgnoredDecoder.java
@@ -4,13 +4,35 @@ import org.jdom2.Element;
 
 import gov.cms.qpp.conversion.decode.DecodeResult;
 import gov.cms.qpp.conversion.decode.QppXmlDecoder;
+import gov.cms.qpp.conversion.model.Decoder;
 import gov.cms.qpp.conversion.model.Node;
+import gov.cms.qpp.conversion.model.TemplateId;
 
 public class IgnoredDecoder extends QppXmlDecoder {
 
 	@Override
 	protected DecodeResult internalDecode(Element element, Node thisnode) {
-		return DecodeResult.TREE_CONTINUE; // TODO Adam Edwards 5/30/2017 - should this be continue?
+		return DecodeResult.TREE_CONTINUE;
+	}
+
+	@Decoder(TemplateId.SEX_SUPPLEMENTAL_DATA_ELEMENT_CMS_V2)
+	public static class SexSupplementalDataElementalCmsV2Decoder extends IgnoredDecoder {
+
+	}
+
+	@Decoder(TemplateId.RACE_SUPPLEMENTAL_DATA_ELEMENT_CMS_V2)
+	public static class RaceSupplementalDataElementCmsV2Decoder extends IgnoredDecoder {
+
+	}
+
+	@Decoder(TemplateId.PAYER_SUPPLEMENTAL_DATA_ELEMENT_CMS_V2)
+	public static class PayerSupplementalDataElementCmsV2Decoder extends IgnoredDecoder {
+
+	}
+
+	@Decoder(TemplateId.ETHNICITY_SUPPLEMENTAL_DATA_ELEMENT_CMS_V2)
+	public static class EthnicitySupplementalDataElementCmsV2Decoder extends IgnoredDecoder {
+
 	}
 
 }
---
+++ java-conversion-tool/src/main/java/gov/cms/qpp/conversion/encode/placeholder/IgnoredEncoder.java
@@ -0,0 +1,40 @@
+package gov.cms.qpp.conversion.encode.placeholder;
+
+import gov.cms.qpp.conversion.encode.JsonOutputEncoder;
+import gov.cms.qpp.conversion.encode.JsonWrapper;
+import gov.cms.qpp.conversion.model.Encoder;
+import gov.cms.qpp.conversion.model.Node;
+import gov.cms.qpp.conversion.model.TemplateId;
+
+/**
+ * Encoder used as a placeholder when a fully implemented encoder has yet to be
+ * developed.
+ */
+public class IgnoredEncoder extends JsonOutputEncoder {
+
+	@Override
+	protected void internalEncode(JsonWrapper wrapper, Node node) {
+
+	}
+
+	@Encoder(TemplateId.SEX_SUPPLEMENTAL_DATA_ELEMENT_CMS_V2)
+	public static class SexSupplementalDataElementalCmsV2Encoder extends IgnoredEncoder {
+
+	}
+
+	@Encoder(TemplateId.RACE_SUPPLEMENTAL_DATA_ELEMENT_CMS_V2)
+	public static class RaceSupplementalDataElementCmsV2Encoder extends IgnoredEncoder {
+
+	}
+
+	@Encoder(TemplateId.PAYER_SUPPLEMENTAL_DATA_ELEMENT_CMS_V2)
+	public static class PayerSupplementalDataElementCmsV2Encoder extends IgnoredEncoder {
+
+	}
+
+	@Encoder(TemplateId.ETHNICITY_SUPPLEMENTAL_DATA_ELEMENT_CMS_V2)
+	public static class EthnicitySupplementalDataElementCmsV2Encoder extends IgnoredEncoder {
+
+	}
+
+}
