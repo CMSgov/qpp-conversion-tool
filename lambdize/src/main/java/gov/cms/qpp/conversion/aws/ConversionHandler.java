252556a50f206625e7fafb50ee5d31b6bcc40121
saquino0827 <saquino@flexion.us>
2017-06-02 09:49:24 -0500

    First fix attempt on nested try catch sonar qube complaint

--- lambdize/src/main/java/gov/cms/qpp/conversion/aws/ConversionHandler.java
+++ lambdize/src/main/java/gov/cms/qpp/conversion/aws/ConversionHandler.java
@@ -31,50 +31,52 @@ public class ConversionHandler implements RequestHandler<S3Event, String> {
 
 	@Override
 	public String handleRequest(S3Event s3event, Context context) {
-		try {
-			S3EventNotificationRecord record = s3event.getRecords().get(0);
-			String srcBucket = record.getS3().getBucket().getName();
-			String srcKey = formatSourceKey(record);
-			String filename = srcKey.replaceAll(".*/", "");
+		S3EventNotificationRecord record = s3event.getRecords().get(0);
+		String srcBucket = record.getS3().getBucket().getName();
+		String srcKey = formatSourceKey(record);
+		String filename = srcKey.replaceAll(".*/", "");
 
-			AmazonS3 s3Client = getClient();
-			S3Object s3Object = s3Client.getObject(new GetObjectRequest(srcBucket, srcKey));
+		AmazonS3 s3Client = getClient();
+		S3Object s3Object = s3Client.getObject(new GetObjectRequest(srcBucket, srcKey));
 
-			JsonWrapper qpp = null;
-			AllErrors errors = null;
+		JsonWrapper qpp = null;
+		AllErrors errors = null;
 
-			try (InputStream input = new NamedInputStream(s3Object.getObjectContent(), srcKey)) {
-				Converter converter = new Converter(input);
-				qpp = converter.transform();
-			} catch (TransformException exception) {
-				errors = exception.getDetails();
-				DEV_LOG.info("Transformation error occurred", exception);
-			}
-
-			InputStream returnStream = null;
-			if (qpp != null) {
-				returnStream = qppToInputStream(qpp);
-			} else if (errors != null) {
-				returnStream = errorsToInputStream(errors);
-			}
-
-			String dstKey = "post-conversion/" + getOutputFile(filename, qpp != null);
-			ObjectMetadata meta = new ObjectMetadata();
-			s3Client.putObject(srcBucket, dstKey, returnStream, meta);
-
-			return "Ok";
-		} catch (IOException e) {
-			throw new RuntimeException(e);
+		try (InputStream input = new NamedInputStream(s3Object.getObjectContent(), srcKey)) {
+			Converter converter = new Converter(input);
+			qpp = converter.transform();
+		} catch (TransformException exception) {
+			errors = exception.getDetails();
+			DEV_LOG.info("Transformation error occurred", exception);
+		} catch (IOException exception) {
+			throw new RuntimeException(exception);
 		}
+
+		InputStream returnStream = null;
+		if (qpp != null) {
+			returnStream = qppToInputStream(qpp);
+		} else if (errors != null) {
+			returnStream = errorsToInputStream(errors);
+		}
+
+		String dstKey = "post-conversion/" + getOutputFile(filename, qpp != null);
+		ObjectMetadata meta = new ObjectMetadata();
+		s3Client.putObject(srcBucket, dstKey, returnStream, meta);
+
+		return "Ok";
 	}
 
 	public AmazonS3 getClient() {
 		return AmazonS3ClientBuilder.defaultClient(); // ignore coverage
 	}
 
-	protected String formatSourceKey(S3EventNotificationRecord record) throws UnsupportedEncodingException {
-		String srcKey = record.getS3().getObject().getKey().replace('+', ' ');
-		return URLDecoder.decode(srcKey, "UTF-8");
+	protected String formatSourceKey(S3EventNotificationRecord record) {
+		try {
+			String srcKey = record.getS3().getObject().getKey().replace('+', ' ');
+			return URLDecoder.decode(srcKey, "UTF-8");
+		} catch (UnsupportedEncodingException exception) {
+			throw new RuntimeException(exception);
+		}
 	}
 
 	private InputStream qppToInputStream(JsonWrapper jsonWrapper) {
