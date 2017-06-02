ac6a4eccabbbc7cf6badd9a2908687018885d768
halprin <pkendall@flexion.us>
2017-06-02 08:53:09 -0600

    QPPCT-114: Removed unnecessary things from ReST API integration test

--- rest-api/src/test/java/gov/cms/qpp/conversion/api/integration/QrdaRestTest.java
+++ rest-api/src/test/java/gov/cms/qpp/conversion/api/integration/QrdaRestTest.java
@@ -1,15 +1,9 @@
 package gov.cms.qpp.conversion.api.integration;
 
 import gov.cms.qpp.conversion.api.RestApiApplication;
-import gov.cms.qpp.conversion.api.controllers.v1.QrdaControllerV1;
-import gov.cms.qpp.conversion.api.services.QrdaService;
-import gov.cms.qpp.conversion.encode.JsonWrapper;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
@@ -21,19 +15,14 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-//@RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = RestApiApplication.class)
-//@ContextConfiguration(classes = RestApiApplication.class)
 @WebAppConfiguration
 @RunWith(SpringRunner.class)
 public class QrdaRestTest {
@@ -41,21 +30,11 @@ public class QrdaRestTest {
 	@Autowired
 	private WebApplicationContext webApplicationContext;
 
-	@Mock
-	private QrdaService qrdaService;
-
-	@InjectMocks
-	private QrdaControllerV1 qrdaControllerV1;
-
 	private MockMvc mockMvc;
 
 	@Before
 	public void setUp() {
 		mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
-		MockitoAnnotations.initMocks(this);
-		JsonWrapper jsonWrapper = new JsonWrapper();
-		jsonWrapper.putString("key", "mockJsonWrapper");
-		when(qrdaService.convertQrda3ToQpp(any(InputStream.class))).thenReturn(jsonWrapper);
 	}
 
 	@Test
