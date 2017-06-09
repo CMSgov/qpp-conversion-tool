c181fa8696d951302b2b599ffddf29f26dc4e145
Clyde Tedrick <iamclyde@gmail.com>
2017-06-09 11:11:10 -0400

    QPPCT-147: introduce CPCPlus sample file

---
+++ java-conversion-tool/src/test/java/gov/cms/qpp/acceptance/MultipleTinRoundTripTest.java
@@ -0,0 +1,37 @@
+package gov.cms.qpp.acceptance;
+
+import com.jayway.jsonpath.JsonPath;
+import com.jayway.jsonpath.ReadContext;
+import gov.cms.qpp.acceptance.helper.JsonPathToXpathHelper;
+import gov.cms.qpp.conversion.encode.JsonWrapper;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Map;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class MultipleTinRoundTripTest {
+	private static JsonWrapper wrapper = new JsonWrapper();
+	private static JsonPathToXpathHelper helper;
+	private static ReadContext ctx;
+
+	@BeforeClass
+	public static void setup() throws IOException {
+		Path path = Paths.get("../qrda-files/ComprehensivePrimaryCare_Sample_QRDA_III.xml");
+		helper = new JsonPathToXpathHelper(path, wrapper, false);
+		ctx = JsonPath.parse(wrapper.toString());
+	}
+
+	@Test
+	public void hasMultipleNpiTinCombo() {
+		List<Map<String, Object>> topLevel = ctx.read("$");
+		assertThat("", topLevel.size(), is(5));
+	}
+
+}
