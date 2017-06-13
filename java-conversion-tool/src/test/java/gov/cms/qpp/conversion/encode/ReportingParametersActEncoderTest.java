68b235c87eab35d31f4e0049dbb3ab448a4488bc
DaveLauta <dlauta@flexion.us>
2017-06-13 09:36:44 -0400

    QPPCT-174 add eCQM reporting parameters - remove CRLF

--- java-conversion-tool/src/test/java/gov/cms/qpp/conversion/encode/ReportingParametersActEncoderTest.java
+++ java-conversion-tool/src/test/java/gov/cms/qpp/conversion/encode/ReportingParametersActEncoderTest.java
@@ -1,1 +1,43 @@
-package gov.cms.qpp.conversion.encode;import gov.cms.qpp.conversion.model.Node;import gov.cms.qpp.conversion.model.TemplateId;import org.junit.Test;import static org.hamcrest.MatcherAssert.assertThat;import static org.hamcrest.core.Is.is;import static org.hamcrest.core.IsNull.nullValue;/** * Test class for ReportingParametersActEncoder */public class ReportingParametersActEncoderTest {	@Test	public void internalEncode() throws Exception {		Node reportingParametersActNode = new Node(TemplateId.REPORTING_PARAMETERS_ACT);		reportingParametersActNode.putValue(ReportingParametersActEncoder.PERFORMANCE_START,"20170101");		reportingParametersActNode.putValue(ReportingParametersActEncoder.PERFORMANCE_END,"20171231");		JsonWrapper outputWrapper = new JsonWrapper();		ReportingParametersActEncoder encoder = new ReportingParametersActEncoder();		encoder.internalEncode(outputWrapper, reportingParametersActNode);		String performanceStart = outputWrapper.getString(ReportingParametersActEncoder.PERFORMANCE_START);		String performanceEnd = outputWrapper.getString(ReportingParametersActEncoder.PERFORMANCE_END);		assertThat("Performance Start = 2017-01-01", performanceStart, is("2017-01-01"));		assertThat("Performance End = 2017-12-31", performanceEnd, is("2017-12-31"));	}	@Test	public void missingValuesTest() throws Exception {		Node reportingParametersActNode = new Node(TemplateId.REPORTING_PARAMETERS_ACT);		JsonWrapper outputWrapper = new JsonWrapper();		ReportingParametersActEncoder encoder = new ReportingParametersActEncoder();		encoder.internalEncode(outputWrapper, reportingParametersActNode);		String performanceStart = outputWrapper.getString(ReportingParametersActEncoder.PERFORMANCE_START);		String performanceEnd = outputWrapper.getString(ReportingParametersActEncoder.PERFORMANCE_END);		assertThat("Performance Start is null", performanceStart, is(nullValue()));		assertThat("Performance End = is null", performanceEnd, is(nullValue()));	}}
+package gov.cms.qpp.conversion.encode;
+
+import gov.cms.qpp.conversion.model.Node;
+import gov.cms.qpp.conversion.model.TemplateId;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNull.nullValue;
+
+/**
+ * Test class for ReportingParametersActEncoder
+ */
+public class ReportingParametersActEncoderTest {
+	@Test
+	public void internalEncode() throws Exception {
+		Node reportingParametersActNode = new Node(TemplateId.REPORTING_PARAMETERS_ACT);
+		reportingParametersActNode.putValue(ReportingParametersActEncoder.PERFORMANCE_START,"20170101");
+		reportingParametersActNode.putValue(ReportingParametersActEncoder.PERFORMANCE_END,"20171231");
+		JsonWrapper outputWrapper = new JsonWrapper();
+		ReportingParametersActEncoder encoder = new ReportingParametersActEncoder();
+		encoder.internalEncode(outputWrapper, reportingParametersActNode);
+		String performanceStart = outputWrapper.getString(ReportingParametersActEncoder.PERFORMANCE_START);
+		String performanceEnd = outputWrapper.getString(ReportingParametersActEncoder.PERFORMANCE_END);
+
+		assertThat("Performance Start = 2017-01-01", performanceStart, is("2017-01-01"));
+		assertThat("Performance End = 2017-12-31", performanceEnd, is("2017-12-31"));
+	}
+
+	@Test
+	public void missingValuesTest() throws Exception {
+		Node reportingParametersActNode = new Node(TemplateId.REPORTING_PARAMETERS_ACT);
+
+		JsonWrapper outputWrapper = new JsonWrapper();
+		ReportingParametersActEncoder encoder = new ReportingParametersActEncoder();
+		encoder.internalEncode(outputWrapper, reportingParametersActNode);
+		String performanceStart = outputWrapper.getString(ReportingParametersActEncoder.PERFORMANCE_START);
+		String performanceEnd = outputWrapper.getString(ReportingParametersActEncoder.PERFORMANCE_END);
+
+		assertThat("Performance Start is null", performanceStart, is(nullValue()));
+		assertThat("Performance End = is null", performanceEnd, is(nullValue()));
+	}
+}
