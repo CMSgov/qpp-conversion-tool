a37112b38e207164b3dbd0233ed3ae968130c69e
Adam Harrison <ad@mharrison.us>
2017-06-16 10:55:53 -0400

    Log silenced exception

--- java-conversion-tool/src/main/java/gov/cms/qpp/conversion/util/ExceptionHelper.java
+++ java-conversion-tool/src/main/java/gov/cms/qpp/conversion/util/ExceptionHelper.java
@@ -2,7 +2,14 @@ package gov.cms.qpp.conversion.util;
 
 import java.util.concurrent.Callable;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import gov.cms.qpp.conversion.decode.placeholder.DefaultDecoder;
+
 public class ExceptionHelper {
+
+	private static final Logger DEV_LOG = LoggerFactory.getLogger(DefaultDecoder.class);
 
 	private ExceptionHelper() {
 	}
@@ -10,7 +17,8 @@ public class ExceptionHelper {
 	public static <T> T runOrSilence(Callable<T> callable) {
 		try {
 			return callable.call();
-		} catch (Exception thrown) {
+		} catch (Exception ignore) {
+			DEV_LOG.warn("Silenced exception", ignore);
 			return null;
 		}
 	}
