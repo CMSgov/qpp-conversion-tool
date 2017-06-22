fd096ab74a7a4e875decbd940e1dcff7ccaef431
Clyde Tedrick <iamclyde@gmail.com>
2017-06-21 20:32:15 -0400

    QPPCT-116: test clean up, review comments

--- benchmark/src/test/java/gov/cms/qpp/conversion/ParameterizedBenchmarkTest.java
+++ benchmark/src/test/java/gov/cms/qpp/conversion/ParameterizedBenchmarkTest.java
@@ -1,6 +1,7 @@
 package gov.cms.qpp.conversion;
 
 
+import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openjdk.jmh.annotations.Mode;
@@ -17,24 +18,47 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Stream;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.greaterThan;
-import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.lessThan;
 
 public class ParameterizedBenchmarkTest {
 	private static String[] paths;
+	private static List<BenchmarkResult> benchResults;
 
 	@BeforeClass
-	public static void loadPaths() throws IOException {
+	public static void loadPaths() throws IOException, RunnerException {
 		paths = Files.walk(Paths.get("../qrda-files"))
 				.filter(Files::isRegularFile)
 				.map(path -> path.toFile().getAbsolutePath())
 				.filter(path -> path.endsWith("-latest.xml"))
 				.toArray(String[]::new);
+
+		Options opt = new OptionsBuilder()
+				.mode(Mode.Throughput)
+				.mode(Mode.AverageTime)
+				.include(".*" + ParameterizedBenchmark.class.getSimpleName() + ".*")
+				.param("fileName", paths)
+				.forks(1)
+				.build();
+
+		List<RunResult> results = new ArrayList<>(new Runner(opt).run());
+		benchResults = results.stream()
+				.map(RunResult::getAggregatedResult)
+				.collect(Collectors.toList());
+	}
+
+	@AfterClass
+	public static void cleanUp() throws IOException {
+		for (String path : paths) {
+			String deleteMe = path
+				.substring(path.lastIndexOf('/') + 1)
+				.replace(".xml", ".qpp.json");
+			Files.deleteIfExists(Paths.get(deleteMe));
+		}
 	}
 
 	@Test
@@ -44,44 +68,27 @@ public class ParameterizedBenchmarkTest {
 
 	@Test
 	public void testParameterizedBenchmarkThroughput() throws RunnerException {
-		Options opt = getOptions(Mode.Throughput, ParameterizedBenchmark.class);
-		Stream<BenchmarkResult> results = executeBenchmark(opt);
+		benchResults.stream()
+			.filter(result -> result.getParams().getMode().equals(Mode.Throughput))
+			.forEach(br -> {
+				String fileName = br.getParams().getParam("fileName");
+				Result result = br.getPrimaryResult();
 
-		results.forEach(br -> {
-			String fileName = br.getParams().getParam("fileName");
-			Result result = br.getPrimaryResult();
-
-			assertThat("Throughput should be greater than 2 for file: " + fileName,
-					result.getScore(), greaterThan(2.0));
-		});
+				assertThat("Throughput should be greater than 2 for file: " + fileName,
+						result.getScore(), greaterThan(2.0));
+			});
 	}
 
 	@Test
 	public void testParameterizedBenchmarkAverageTime() throws RunnerException {
-		Options opt = getOptions(Mode.AverageTime, ParameterizedBenchmark.class);
-		Stream<BenchmarkResult> results = executeBenchmark(opt);
+		benchResults.stream()
+			.filter(result -> result.getParams().getMode().equals(Mode.AverageTime))
+			.forEach(br -> {
+				String fileName = br.getParams().getParam("fileName");
+				Result result = br.getPrimaryResult();
 
-		results.forEach(br -> {
-			String fileName = br.getParams().getParam("fileName");
-			Result result = br.getPrimaryResult();
-
-			assertThat("Average time should be less than .5 seconds" + fileName,
-					result.getScore(), lessThan(0.5));
-		});
-	}
-
-	private Options getOptions(Mode mode, Class benchmarkClass) {
-		return new OptionsBuilder()
-				.mode(mode)
-				.include(".*" + benchmarkClass.getSimpleName() + ".*")
-				.param("fileName", paths)
-				.forks(1)
-				.build();
-	}
-
-	private Stream<BenchmarkResult> executeBenchmark(Options opt) throws RunnerException {
-		List<RunResult> results = new ArrayList<>(new Runner(opt).run());
-		assertThat("Should return a benchmark result for each path", results, hasSize(paths.length));
-		return results.stream().map(RunResult::getAggregatedResult);
+				assertThat("Average time should be less than .5 seconds" + fileName,
+						result.getScore(), lessThan(0.5));
+			});
 	}
 }
