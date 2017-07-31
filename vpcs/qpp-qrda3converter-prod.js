a81455ddb86713f6d7a9e476d1a131c89fe655a7
Gabe Smedresman <gabesmed@yahoo.com>
2017-07-30 17:44:42 -0700

    add prod environment

--- vpcs/qpp-qrda3converter-prod.js
+++ vpcs/qpp-qrda3converter-prod.js
@@ -1,15 +1,15 @@
 var env = {
   organization: 'Qpp',
   application: 'Qrda3Converter',
-  environment: 'Impl',
+  environment: 'Prod',
   region: 'us-east-1',
-  bucket: 'aws-hhs-cms-ccsq-qpp-navadevops-nonprod-us-east-1',
-  cidr: '10.138.168.0/21',
+  bucket: 'aws-hhs-cms-ccsq-qpp-navadevops-prod-us-east-1',
+  cidr: '10.245.200.0/21',
   creator: 'Gabe Smedresman',
   keyName: 'nava-sandbox',
   // temporary ssl cert
   sslCertificateId: 'arn:aws:acm:us-east-1:003384571330:certificate/8f110a1b-fc4a-4b94-90b9-f455fc0012c7',
-  defaultIamInstanceProfile: 'server-nonprod',
+  defaultIamInstanceProfile: 'server-prod',
   alarmEmail: 'devops@navahq.com',
   machineImageOwners: ['self'],
   externalSecurityGroupRefs: [{ Ref: 'OpenHttpSecurityGroup' }]
@@ -21,12 +21,12 @@ env.s3Url = 's3://' + env.bucket + '/' + env.name.toDash();
 env.configPath = env.s3Url + '/config.yaml';
 env.fqdn = env.name.toDash() + '.qpp.internal';
 
-env.vpcId = 'vpc-48f26131';
+env.vpcId = 'vpc-fa45d483';
 env.gditSubnets = {
-  application: ['subnet-bd3f72f5', 'subnet-0f21a355', 'subnet-31957055'],
-  management:  ['subnet-c8397480', 'subnet-5c23a106', 'subnet-a78b6ec3'],
-  private:     ['subnet-dd3a7795', 'subnet-1e199b44', 'subnet-ed977289'],
-  public:      ['subnet-4f3b7607', 'subnet-091c9e53', 'subnet-e98a6f8d']
+  application: ['subnet-3df1b275', 'subnet-624ecd38', 'subnet-383e245d'],
+  management:  ['subnet-59f1b211', 'subnet-8244c7d8', 'subnet-d52a30b0'],
+  private:     ['subnet-aaeba8e2', 'subnet-2b44c771', 'subnet-25372d40'],
+  public:      ['subnet-b4e8abfc', 'subnet-f245c6a8', 'subnet-32342e57']
 };
 
 env.configureLayers = function() {
