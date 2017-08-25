176ad2b048e7b7048928c7cdeef1ac1dbdb88d5d
Clyde Tedrick <iamclyde@gmail.com>
2017-08-25 15:00:06 -0400

    QPPCT-279: equals verifier

--- java-conversion-tool/src/test/java/gov/cms/qpp/conversion/model/error/DetailTest.java
+++ java-conversion-tool/src/test/java/gov/cms/qpp/conversion/model/error/DetailTest.java
@@ -34,45 +34,10 @@ public class DetailTest {
 	}
 
 //	@Test
-//	public void testEquals () throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
-//		Detail detail = new Detail();
-//		Detail nullDetail = new Detail(null, null, null, null);
-//		Detail emptyValues = new Detail("","","","");
-//		String[] mutators = {"setMessage", "setPath", "setValue", "setType"};
-//
-//		for (String methodName : mutators) {
-//			Method setter = Detail.class.getMethod(methodName, String.class);
-//			setter.invoke(detail, "meep");
-//
-//			Detail empty = new Detail();
-//			Detail copy = new Detail(detail);
-//
-//			assertEquals("detail should equal detail", detail, detail);
-//			assertEquals("Copied detail should equal original", detail, copy);
-//			assertNotEquals("Empty detail should not equal initialized detail", empty, detail);
-//			assertNotEquals("Empty values detail should not equal initialized detail", emptyValues, detail);
-//			assertNotEquals("Null detail should not equal initialized detail", nullDetail, detail);
-//
-//			setter.invoke(nullDetail, "meep");
-//			setter.invoke(emptyValues, "meep");
-//		}
-//	}
-//
-//	@Test
-//	public void moreEqualsTesting() {
-//		Detail detail = new Detail("meep","meeo","meep","meep");
+//	public void testHashCode() {
+//		Detail emptyValues = new Detail("meep","meeo","meep","meep");
 //		Detail nullDetail = new Detail(null,null,null,null);
-//
-//		assertEquals("Null detail should equal Null detail", nullDetail, new Detail(nullDetail));
-//		assertNotEquals("detail should not equal null", detail, null);
-//		assertNotEquals("detail should not equal a non-Detail class", detail, "meep");
+//		assertEquals(emptyValues.hashCode(), -29068033);
+//		assertEquals(nullDetail.hashCode(), 0);
 //	}
-
-	@Test
-	public void testHashCode() {
-		Detail emptyValues = new Detail("meep","meeo","meep","meep");
-		Detail nullDetail = new Detail(null,null,null,null);
-		assertEquals(emptyValues.hashCode(), -29068033);
-		assertEquals(nullDetail.hashCode(), 0);
-	}
 }
