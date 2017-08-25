3bd7557d018f963257b9942185dbb9fe414c1234
Clyde Tedrick <iamclyde@gmail.com>
2017-08-25 10:58:28 -0400

    QPPCT-279: downsize big tests

--- java-conversion-tool/src/test/java/gov/cms/qpp/conversion/model/error/DetailTest.java
+++ java-conversion-tool/src/test/java/gov/cms/qpp/conversion/model/error/DetailTest.java
@@ -1,6 +1,7 @@
 package gov.cms.qpp.conversion.model.error;
 
 
+import nl.jqno.equalsverifier.EqualsVerifier;
 import org.junit.Test;
 
 import java.lang.reflect.InvocationTargetException;
@@ -27,39 +28,44 @@ public class DetailTest {
 	}
 
 	@Test
-	public void testEquals () throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
-		Detail detail = new Detail();
-		Detail nullDetail = new Detail(null, null, null, null);
-		Detail emptyValues = new Detail("","","","");
-		String[] mutators = {"setMessage", "setPath", "setValue", "setType"};
-
-		for (String methodName : mutators) {
-			Method setter = Detail.class.getMethod(methodName, String.class);
-			setter.invoke(detail, "meep");
-
-			Detail empty = new Detail();
-			Detail copy = new Detail(detail);
-
-			assertEquals("detail should equal detail", detail, detail);
-			assertEquals("Copied detail should equal original", detail, copy);
-			assertNotEquals("Empty detail should not equal initialized detail", empty, detail);
-			assertNotEquals("Empty values detail should not equal initialized detail", emptyValues, detail);
-			assertNotEquals("Null detail should not equal initialized detail", nullDetail, detail);
-
-			setter.invoke(nullDetail, "meep");
-			setter.invoke(emptyValues, "meep");
-		}
+	public void equalsContract() {
+		EqualsVerifier.forClass(Detail.class).verify();
 	}
 
-	@Test
-	public void moreEqualsTesting() {
-		Detail detail = new Detail("meep","meeo","meep","meep");
-		Detail nullDetail = new Detail(null,null,null,null);
-
-		assertEquals("Null detail should equal Null detail", nullDetail, new Detail(nullDetail));
-		assertNotEquals("detail should not equal null", detail, null);
-		assertNotEquals("detail should not equal a non-Detail class", detail, "meep");
-	}
+//	@Test
+//	public void testEquals () throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+//		Detail detail = new Detail();
+//		Detail nullDetail = new Detail(null, null, null, null);
+//		Detail emptyValues = new Detail("","","","");
+//		String[] mutators = {"setMessage", "setPath", "setValue", "setType"};
+//
+//		for (String methodName : mutators) {
+//			Method setter = Detail.class.getMethod(methodName, String.class);
+//			setter.invoke(detail, "meep");
+//
+//			Detail empty = new Detail();
+//			Detail copy = new Detail(detail);
+//
+//			assertEquals("detail should equal detail", detail, detail);
+//			assertEquals("Copied detail should equal original", detail, copy);
+//			assertNotEquals("Empty detail should not equal initialized detail", empty, detail);
+//			assertNotEquals("Empty values detail should not equal initialized detail", emptyValues, detail);
+//			assertNotEquals("Null detail should not equal initialized detail", nullDetail, detail);
+//
+//			setter.invoke(nullDetail, "meep");
+//			setter.invoke(emptyValues, "meep");
+//		}
+//	}
+//
+//	@Test
+//	public void moreEqualsTesting() {
+//		Detail detail = new Detail("meep","meeo","meep","meep");
+//		Detail nullDetail = new Detail(null,null,null,null);
+//
+//		assertEquals("Null detail should equal Null detail", nullDetail, new Detail(nullDetail));
+//		assertNotEquals("detail should not equal null", detail, null);
+//		assertNotEquals("detail should not equal a non-Detail class", detail, "meep");
+//	}
 
 	@Test
 	public void testHashCode() {
