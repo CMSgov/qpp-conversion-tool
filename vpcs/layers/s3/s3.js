6b7a61fce0f7e58605e78c68e93b525e56d22c93
Clyde Tedrick <iamclyde@gmail.com>
2017-08-29 11:22:21 -0400

    QPPCT-253: more flexible bucket naming

--- vpcs/layers/s3/s3.js
+++ vpcs/layers/s3/s3.js
@@ -1,9 +1,6 @@
 var merge = rootRequire('./lib/merge_all');
-var virtualEnvironment = rootRequire('./virtual_environment_defs');
 
-module.exports = function(env) {
-    var awsAccount = env.awsAccount || "";
-    var bucketName = (awsAccount + virtualEnvironment.name + "Bucket").toLowerCase();
+module.exports = function(bucketName) {
     var setup = {
         "Resources": {}
     }
@@ -12,10 +9,7 @@ module.exports = function(env) {
         "Type" : "AWS::S3::Bucket",
         "Properties" : {
             "AccessControl" : "BucketOwnerFullControl",
-            "BucketName" : bucketName,
-            "VersioningConfiguration" : {
-                "Status" : "Enabled"
-            }
+            "BucketName" : bucketName
         },
         "DeletionPolicy" : "Retain"
     };
