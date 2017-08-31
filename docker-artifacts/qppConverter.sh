4f61fbba717f80d7b92c1fbbd5d86115eaf95152
Rebecca Kilberg <rekilberg@gmail.com>
2017-08-31 15:51:12 -0700

    Adding env variables so the 3 apps each report to a different endpoint on newrelic

--- docker-artifacts/qppConverter.sh
+++ docker-artifacts/qppConverter.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 sed -i -e "s/NEWRELIC_API_KEY/$NEWRELIC_API_KEY/g" ./newrelic/newrelic.yml
+sed -i -e "s/APP_ENV/$APP_ENV/g" ./newrelic/newrelic.yml
 exec java $JAVA_OPTS -javaagent:./newrelic/newrelic.jar -jar ./rest-api.jar
