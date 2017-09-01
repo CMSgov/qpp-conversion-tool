d08334c3107d546876b4a33c8fd8bdf594e76de8
Adam Harrison <ad@mharrison.us>
2017-09-01 19:23:05 -0400

    Update sonarqube.sh

--- bin/sonarqube.sh
+++ bin/sonarqube.sh
@@ -8,9 +8,9 @@ if [[ "$CIRCLE_BRANCH" == "master" || ( ! -z $SONAR_OTHER_BRANCH && "$CIRCLE_BRA
 	./sonar-scanner-3.0.1.733/bin/sonar-scanner -Dsonar.login=${SONAR_KEY}
 	status=$(curl -sS ${SONAR_HOST}/api/qualitygates/project_status?projectKey=gov.cms.qpp.conversion:qpp-conversion | jq '.projectStatus.status')
 	if [[ ${status} = '"ERROR"' ]]; then
-      echo "exiting with non-zero status code"
-      exit 1
-    fi
+		echo "sonar failed: " ${status}
+		exit 1
+	fi
 elif [[ ! -z $CI_PULL_REQUESTS ]]; then
 	#Do a PR preview SonarCube run
 	#This build could be on multiple PRs
