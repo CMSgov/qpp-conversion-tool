d3aac408a087c2e75f7ec97d65556a37e5eb306b
Clyde Tedrick <iamclyde@gmail.com>
2017-09-07 09:05:21 -0400

    QPPCT-314: clean up unused

--- converter/src/test/java/gov/cms/qpp/conversion/model/validation/SubPopulationTest.java
+++ converter/src/test/java/gov/cms/qpp/conversion/model/validation/SubPopulationTest.java
@@ -17,16 +17,6 @@ import static org.junit.Assert.assertNull;
  * SubPopulation Test class to increase JaCoCo Code Coverage
  */
 public class SubPopulationTest {
-	private static List<Method> mutators;
-
-	@BeforeClass
-	public static void setup() {
-		mutators = Arrays.stream(SubPopulation.class.getDeclaredMethods())
-				.filter(method -> method.getName().startsWith("set"))
-				.filter(method ->
-						Arrays.equals(method.getParameterTypes(), new Class<?>[] {String.class}))
-				.collect(Collectors.toList());
-	}
 
 	@Test
 	public void getStrata1() {
@@ -40,13 +30,6 @@ public class SubPopulationTest {
 		SubPopulation sp = new SubPopulation();
 		String strata2 = sp.getStrata2();
 		assertNull(strata2);
-	}
-
-	private void initWith(SubPopulation subPop, String value)
-			throws InvocationTargetException, IllegalAccessException {
-		for (Method mutator : mutators) {
-			mutator.invoke(subPop, value);
-		}
 	}
 
 	@Test
