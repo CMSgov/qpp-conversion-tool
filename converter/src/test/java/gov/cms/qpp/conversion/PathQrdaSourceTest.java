b2c6723660e5a8f0482f06817b63b8df98bd2525
Clyde Tedrick <iamclyde@gmail.com>
2017-09-12 10:27:10 -0400

    QPPCT-334: added coverage

--- converter/src/test/java/gov/cms/qpp/conversion/PathQrdaSourceTest.java
+++ converter/src/test/java/gov/cms/qpp/conversion/PathQrdaSourceTest.java
@@ -1,8 +1,17 @@
 package gov.cms.qpp.conversion;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.URI;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.FileSystem;
+import java.nio.file.LinkOption;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.WatchEvent;
+import java.nio.file.WatchKey;
+import java.nio.file.WatchService;
+import java.util.Iterator;
 
 import org.apache.commons.io.IOUtils;
 import org.junit.Assert;
@@ -10,6 +19,8 @@ import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.isEmptyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class PathQrdaSourceTest extends QrdaSourceTestSuite {
 
@@ -26,6 +37,14 @@ public class PathQrdaSourceTest extends QrdaSourceTestSuite {
 	@Test
 	public void testNullPath() {
 		PathQrdaSource testSource = new PathQrdaSource(null);
+		assertThat(testSource.getName(), isEmptyString());
+	}
+
+	@Test
+	public void testNullFileName() {
+		Path mockPath = mock(Path.class);
+		when(mockPath.getFileName()).thenReturn(null);
+		PathQrdaSource testSource = new PathQrdaSource(mockPath);
 		assertThat(testSource.getName(), isEmptyString());
 	}
 
