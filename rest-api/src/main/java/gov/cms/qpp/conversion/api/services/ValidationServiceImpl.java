6a3791c59ace37a5b4cbc4e84ea5bd308016683b
Clyde Tedrick <iamclyde@gmail.com>
2017-09-19 13:03:16 -0400

    QPPCT-217: use reference

--- rest-api/src/main/java/gov/cms/qpp/conversion/api/services/ValidationServiceImpl.java
+++ rest-api/src/main/java/gov/cms/qpp/conversion/api/services/ValidationServiceImpl.java
@@ -75,11 +75,11 @@ public class ValidationServiceImpl implements ValidationService {
 		HttpHeaders headers = new HttpHeaders();
 		headers.add(HttpHeaders.CONTENT_TYPE, "application/json");
 		headers.add(HttpHeaders.ACCEPT, "application/json");
-		
+
 		String submissionToken = environment.getProperty(SUBMISSION_API_TOKEN);
 		if (submissionToken == null || submissionToken.isEmpty()) {
 			headers.add(HttpHeaders.AUTHORIZATION,
-					"Bearer " + environment.getProperty(SUBMISSION_API_TOKEN));
+					"Bearer " + submissionToken);
 		}
 
 		HttpEntity<String> request = new HttpEntity<>(qpp.toString(), headers);
