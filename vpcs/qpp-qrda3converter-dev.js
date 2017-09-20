938f8898228f055065db116d434649058f48bd9d
Gabe Smedresman <gabesmed@yahoo.com>
2017-09-20 14:22:38 -0700

    add comment

--- vpcs/qpp-qrda3converter-dev.js
+++ vpcs/qpp-qrda3converter-dev.js
@@ -2,6 +2,7 @@ var env = {
   name: 'qpp-qrda3converter-dev',
   region: 'us-east-1',
   bucket: 'aws-hhs-cms-ccsq-qpp-navadevops-nonprod-us-east-1',
+  // Used by vpc-env to store environmnet variables
   s3Url: 's3://aws-hhs-cms-ccsq-qpp-navadevops-nonprod-us-east-1/qpp-qrda3converter-dev',
   keyName: 'nava-sandbox',
   awsAccount: 'aws-hhs-cms-ccsq-qpp-navadevops'
