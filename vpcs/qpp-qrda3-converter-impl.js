cd9e904b0b104147053d7acb4f3cd1b59864deaf
scott smith <scott@ohlol.net>
2017-09-21 14:11:53 -0700

    Create Terraform Templates for the IMPL and PROD Environments (#324)
    Create Terraform Templates for the IMPL and PROD Environments
    
    These templates allow us to deploy to the remaining environments
    using Terraform rather than Cfn.
    
    - Create qpp-qrda3converter-impl env
    - Create qpp-qrda3converter-prod env
    - Update impl and prod corevpc env configs

--- vpcs/qpp-qrda3-converter-impl.js
+++ vpcs/qpp-qrda3-converter-impl.js
@@ -1,76 +1,32 @@
 var env = {
-  organization: 'Qpp',
-  application: 'Qrda3Converter',
-  environment: 'Impl',
+  name: 'qpp-qrda3converter-impl',
   region: 'us-east-1',
   bucket: 'aws-hhs-cms-ccsq-qpp-navadevops-nonprod-us-east-1',
-  cidr: '10.138.168.0/21',
-  creator: 'Gabe Smedresman',
+  // Used by vpc-env to store environmnet variables
+  s3Url: 's3://aws-hhs-cms-ccsq-qpp-navadevops-nonprod-us-east-1/qpp-qrda3converter-impl',
   keyName: 'nava-sandbox',
-  // temporary ssl cert
-  sslCertificateId: 'arn:aws:acm:us-east-1:003384571330:certificate/8f110a1b-fc4a-4b94-90b9-f455fc0012c7',
-  defaultIamInstanceProfile: 'server-nonprod',
-  alarmEmail: 'devops@navahq.com',
-  machineImageOwners: ['self'],
-  internalSecurityGroupRefs: [{ Ref: 'OpenHttpSecurityGroup' }],
   awsAccount: 'aws-hhs-cms-ccsq-qpp-navadevops'
 };
 
-// Boilerplate dependent variables.
-env.name = env.organization + env.application + env.environment;
-env.s3Url = 's3://' + env.bucket + '/' + env.name.toDash();
-env.configPath = env.s3Url + '/config.yaml';
-env.fqdn = env.name.toDash() + '.qpp.internal';
-
-env.vpcId = 'vpc-48f26131';
-env.gditSubnets = {
-  application: ['subnet-bd3f72f5', 'subnet-0f21a355', 'subnet-31957055'],
-  management:  ['subnet-c8397480', 'subnet-5c23a106', 'subnet-a78b6ec3'],
-  private:     ['subnet-dd3a7795', 'subnet-1e199b44', 'subnet-ed977289'],
-  public:      ['subnet-4f3b7607', 'subnet-091c9e53', 'subnet-e98a6f8d']
-};
-
-env.configureLayers = function() {
-   // Replaces external ELB with an internal ELB
-  const virtualEnvironment = rootRequire('./virtual_environment_defs');
-  const internalBalancer = rootRequire('./layers/app/balancers/internal_balancer');
-  internalBalancer['Resources']['AppElb']['Properties']['Subnets'] = [
-    {
-      'Ref': virtualEnvironment.zones.private[0].Name
-    },
-    {
-      'Ref': virtualEnvironment.zones.private[1].Name
-    },
-    {
-      'Ref': virtualEnvironment.zones.private[2].Name
-    }
-  ];
-
-  Object.assign(internalBalancer['Resources']['AppElb']['Properties']['Listeners'][0], {
-    InstancePort: 3000,
-    LoadBalancerPort: 443,
-    Protocol: 'HTTPS',
-    InstanceProtocol: 'HTTP',
-    // ACM certificate for impl.qpp-qrda3-converter.navapbc.com
-    SSLCertificateId: 'arn:aws:acm:us-east-1:003384571330:certificate/0fb69207-0392-478a-8099-66fc99baa0d9'
-  });
-
-  var s3 = rootRequire('./vpcs/layers/s3/s3ForPii');
-  var s3BuckName = env.name.toLowerCase() + 'audit';
-  var rootAccountForAccess = '003384571330'; //Nava root account ID
-  var roleUserIdServerNonProd = 'AROAIL6OBZZTME7ZPAK3E'; //RoleID for the server-nonprod role
-
-  return {
-    app: rootRequire('./layers/app/api'),
-    s3: s3(s3BuckName, rootAccountForAccess, roleUserIdServerNonProd),
-    jump: rootRequire('./layers/jump/jump'),
-    internalBalancer,
-    net: rootRequire('./layers/net/gdit')
-  };
+/**
+ * Configure packer builds.
+ */
+env.build = {
+  region: 'us-east-1',
+  playbookName: 'service-docker',
+  applicationName: 'Qrda3Converter', // should be the same as env.application
+  varFiles: {
+    base: ['vpcs/packer-common.json', 'vpcs/packer-base-gdit-july.json'],
+    app: ['vpcs/packer-common.json', 'vpcs/packer-app.json']
+  }
 };
 
 /**
- * Run the AMI build script and other checks before cloudforming.
+ * Legacy preform script to build AMIs. This script is called by the
+ * `cloudform` command before running cloudform. Remove this when we've ported
+ * CI over to use the python build-ami script -- at that point, `build-ami`
+ * should be run first, then `cloudform` (or terraform) second,
+ * as independent steps.
  */
 env.preformScript =
   'cd $APP_BASE_DIR; ' +
