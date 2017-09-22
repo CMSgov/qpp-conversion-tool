41d091f539961797926342cc11df6337fdfd5aac
halprin <pkendall@flexion.us>
2017-09-22 17:04:56 -0600

    QPPCT-106: Added JavaDocs

--- rest-api/src/main/java/gov/cms/qpp/conversion/api/services/StorageService.java
+++ rest-api/src/main/java/gov/cms/qpp/conversion/api/services/StorageService.java
@@ -4,6 +4,17 @@ package gov.cms.qpp.conversion.api.services;
 import java.io.InputStream;
 import java.util.concurrent.CompletableFuture;
 
+/**
+ * Interface to store an {@link InputStream} in S3.
+ */
 public interface StorageService {
+
+	/**
+	 * Stores the {@link InputStream} as an object in the S3 bucket.
+	 *
+	 * @param keyName The requested key name for the object.
+	 * @param inStream The {@link InputStream} to write out to an object in S3.
+	 * @return A {@link CompletableFuture} that will eventually contain the S3 object key.
+	 */
 	CompletableFuture<String> store(String keyName, InputStream inStream);
 }
--- rest-api/src/main/java/gov/cms/qpp/conversion/api/services/StorageServiceImpl.java
+++ rest-api/src/main/java/gov/cms/qpp/conversion/api/services/StorageServiceImpl.java
@@ -15,6 +15,9 @@ import org.springframework.stereotype.Service;
 import java.io.InputStream;
 import java.util.concurrent.CompletableFuture;
 
+/**
+ * Used to store an {@link InputStream} in S3.
+ */
 @Service
 public class StorageServiceImpl extends InOrderAsyncActionService<PutObjectRequest, String>
 		implements StorageService {
@@ -27,6 +30,13 @@ public class StorageServiceImpl extends InOrderAsyncActionService<PutObjectReque
 	@Autowired
 	private Environment environment;
 
+	/**
+	 * Stores the {@link InputStream} as an object in the S3 bucket.
+	 *
+	 * @param keyName The requested key name for the object.
+	 * @param inStream The {@link InputStream} to write out to an object in S3.
+	 * @return A {@link CompletableFuture} that will eventually contain the S3 object key.
+	 */
 	@Override
 	public CompletableFuture<String> store(String keyName, InputStream inStream) {
 		final String bucketName = environment.getProperty(BUCKET_NAME);
@@ -38,6 +48,12 @@ public class StorageServiceImpl extends InOrderAsyncActionService<PutObjectReque
 		return actOnItem(new PutObjectRequest(bucketName, keyName, inStream, new ObjectMetadata()));
 	}
 
+	/**
+	 * Uses the {@link TransferManager} to upload a file.
+	 *
+	 * @param objectToActOn The put request.
+	 * @return The object key in the bucket.
+	 */
 	@Override
 	protected String asynchronousAction(PutObjectRequest objectToActOn) {
 		String returnValue;
