79de758fa9f7fc99057466f5feb24ad01645c2f4
halprin <pkendall@flexion.us>
2017-09-22 12:37:30 -0600

    QPPCT-106: Amazon S3 client falls back to us-east-1

--- rest-api/src/main/java/gov/cms/qpp/conversion/api/config/S3Config.java
+++ rest-api/src/main/java/gov/cms/qpp/conversion/api/config/S3Config.java
@@ -1,6 +1,7 @@
 package gov.cms.qpp.conversion.api.config;
 
 
+import com.amazonaws.SdkClientException;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.transfer.TransferManager;
@@ -8,14 +9,40 @@ import com.amazonaws.services.s3.transfer.TransferManagerBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * Spring configuration file.
+ *
+ * Configures {@link Bean}s associated with S3.
+ */
 @Configuration
 public class S3Config {
 
+	/**
+	 * Creates the S3 client {@link Bean}.
+	 *
+	 * Uses the default client, but if a region is unspecified, uses {@code us-east-1}.
+	 *
+	 * @return The S3 client.
+	 */
 	@Bean
 	public AmazonS3 s3client() {
-		return AmazonS3ClientBuilder.defaultClient();
+		AmazonS3 client = null;
+
+		try {
+			client = AmazonS3ClientBuilder.defaultClient();
+		} catch (SdkClientException exception) {
+			client = AmazonS3ClientBuilder.standard().withRegion("us-east-1").build();
+		}
+
+		return client;
 	}
 
+	/**
+	 * Creates a standard Transfer Manager {@link Bean} based on an available {@link AmazonS3} {@link Bean}.
+	 *
+	 * @param s3Client The {@link AmazonS3} {@link Bean}.
+	 * @return The S3 Transfer Manager.
+	 */
 	@Bean
 	public TransferManager s3TransferManager(AmazonS3 s3Client) {
 		return TransferManagerBuilder.standard().withS3Client(s3Client).build();
--- rest-api/src/test/java/gov/cms/qpp/conversion/api/config/ConfigTest.java
+++ rest-api/src/test/java/gov/cms/qpp/conversion/api/config/ConfigTest.java
@@ -2,7 +2,6 @@ package gov.cms.qpp.conversion.api.config;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.transfer.TransferManager;
-import gov.cms.qpp.conversion.api.RestApiApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,14 +12,14 @@ import org.springframework.test.context.junit4.SpringRunner;
 import static org.junit.Assert.assertNotNull;
 
 
-@SpringBootTest(classes = { RestApiApplication.class, S3Config.class })
+@SpringBootTest
 @RunWith(SpringRunner.class)
 public class ConfigTest {
 	@Autowired
 	private AmazonS3 s3client;
 
 	@Autowired
-	public TransferManager s3TransferManager;
+	private TransferManager s3TransferManager;
 
 	@Autowired
 	private TaskExecutor executor;
@@ -28,7 +27,7 @@ public class ConfigTest {
 	@Test
 	public void initializedConfig() {
 		assertNotNull("S3Client should be injectable", s3client);
-		assertNotNull("S3TransferManager should be injectable",s3TransferManager);
-		assertNotNull("Executor should be injectable",executor);
+		assertNotNull("S3TransferManager should be injectable", s3TransferManager);
+		assertNotNull("Executor should be injectable", executor);
 	}
 }
