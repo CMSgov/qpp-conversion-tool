e8d7fef112dc55ec3f78102b47960b218b13f09c
halprin <pkendall@flexion.us>
2017-09-28 22:19:23 -0600

    QPPCT-176: Make DbServiceImpl a Service

--- rest-api/src/main/java/gov/cms/qpp/conversion/api/services/DbServiceImpl.java
+++ rest-api/src/main/java/gov/cms/qpp/conversion/api/services/DbServiceImpl.java
@@ -3,21 +3,29 @@ package gov.cms.qpp.conversion.api.services;
 
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 import gov.cms.qpp.conversion.api.model.Metadata;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class DbServiceImpl extends InOrderAsyncActionService<Metadata, Metadata>
 		implements DbService {
+
+	private static final Logger API_LOG = LoggerFactory.getLogger("API_LOG");
 
 	@Autowired
 	private DynamoDBMapper mapper;
 
 	public Metadata write(Metadata meta) {
+		API_LOG.info("Writing item to DynamoDB");
 		return asynchronousAction(meta);
 	}
 
 	@Override
 	protected Metadata asynchronousAction(Metadata meta) {
 		mapper.save(meta);
+		API_LOG.info("Wrote item to DynamoDB");
 		return meta;
 	}
 }
