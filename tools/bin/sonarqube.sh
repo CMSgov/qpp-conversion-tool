4866831a6e56df7a8584d6194a435dbf2efd82c0
Scott Fradkin <sfradkin@flexion.us>
2017-10-19 11:32:39 -0500

    QPPCT-199, begin changes for sonarcloud

--- tools/bin/sonarqube.sh
+++ tools/bin/sonarqube.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
 DIR=$(dirname "$0")
-SONAR_HOST=http://sonar.shareddev.flexion.us:9000
+SONAR_HOST=https://sonarcloud.io
+ORG_KEY=cmsgov
 
 if [[ "$CIRCLE_BRANCH" == "master" || ( ! -z $SONAR_OTHER_BRANCH && "$CIRCLE_BRANCH" == "$SONAR_OTHER_BRANCH" ) ]]; then
 	#Do a full SonarQube run
 	echo "Doing full SonarQube run"
-	./sonar-scanner-3.0.1.733/bin/sonar-scanner -Dsonar.login=${SONAR_KEY}
+	./sonar-scanner-3.0.1.733/bin/sonar-scanner -Dsonar.host.url=${SONAR_HOST} \
+    																					-Dsonar.organization=${ORG_KEY} \
+    																					-Dsonar.login=${SONAR_KEY_NEW}
 	sleep 3
 	project_status=$(curl -sS ${SONAR_HOST}/api/qualitygates/project_status?projectKey=gov.cms.qpp.conversion:qpp-conversion)
 	status=$(echo ${project_status} | jq '.projectStatus.status')
@@ -28,6 +31,7 @@ elif [[ ! -z $CI_PULL_REQUESTS ]]; then
 		                                            -Dsonar.github.pullRequest=${PR_NUMBER} \
 		                                            -Dsonar.github.repository=CMSgov/qpp-conversion-tool \
 		                                            -Dsonar.github.oauth=${SONAR_PR_KEY} \
+																								-Dsonar.organization=${ORG_KEY} \
 		                                            -Dsonar.login=${SONAR_KEY}
 	done
 else
