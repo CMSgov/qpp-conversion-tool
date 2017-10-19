9181f2a5e5004f58f9e1295f081475d750a77a21
Samuel Aquino <saquino@flexion.us>
2017-10-19 16:20:02 -0500

    Remove complicated test duplication and refactor AciMeasurePerformedRnRDecoderTest

--- converter/src/test/java/gov/cms/qpp/conversion/decode/AciMeasurePerformedRnRDecoderTest.java
+++ converter/src/test/java/gov/cms/qpp/conversion/decode/AciMeasurePerformedRnRDecoderTest.java
@@ -9,65 +9,37 @@ import gov.cms.qpp.conversion.xml.XmlUtils;
 import java.io.IOException;
 import org.jdom2.Element;
 import org.jdom2.Namespace;
+import org.junit.Before;
 import org.junit.Test;
 
+import static com.google.common.truth.Truth.assertThat;
 import static com.google.common.truth.Truth.assertWithMessage;
 
 public class AciMeasurePerformedRnRDecoderTest {
 	private static final String MEASURE_ID = "ACI_INFBLO_1";
+	private Node aciMeasurePerformedNode;
 
-	@Test
-	public void internalDecodeReturnsTreeContinue() {
-		//set-up
-		AciMeasurePerformedRnRDecoder objectUnderTest = new AciMeasurePerformedRnRDecoder(new Context());
-		
-		Namespace rootns = Namespace.getNamespace("urn:hl7-org:v3");
-		Namespace ns = Namespace.getNamespace("xsi", "http://www.w3.org/2001/XMLSchema-instance");
-
-		Element element = new Element("organizer", rootns);
-		Element templateIdElement = new Element("templateId", rootns)
-			                            .setAttribute("root","2.16.840.1.113883.10.20.27.3.28");
-		Element referenceElement = new Element("reference", rootns);
-		Element externalDocumentElement = new Element("externalDocument", rootns);
-		Element idElement = new Element("id", rootns).setAttribute("extension", MEASURE_ID);
-
-		externalDocumentElement.addContent(idElement);
-		referenceElement.addContent(externalDocumentElement);
-		element.addContent(templateIdElement);
-		element.addContent(referenceElement);
-		element.addNamespaceDeclaration(ns);
-
-		Node aciMeasurePerformedNode = new Node();
-
-		objectUnderTest.setNamespace(element, objectUnderTest);
-
-		//execute
-		DecodeResult decodeResult = objectUnderTest.internalDecode(element, aciMeasurePerformedNode);
-
-		//assert
-		assertWithMessage("The decode result is incorrect.")
-				.that(decodeResult)
-				.isEquivalentAccordingToCompareTo(DecodeResult.TREE_CONTINUE);
-		String actualMeasureId = aciMeasurePerformedNode.getValue("measureId");
-		assertWithMessage("measureId is incorrect.")
-				.that(actualMeasureId)
-				.isEqualTo(MEASURE_ID);
-	}
-
-	@Test
-	public void testUpperLevel() throws XmlException, IOException {
+	@Before
+	public void setUp() throws IOException, XmlException {
 		String needsFormattingXml = TestHelper.getFixture("AciMeasurePerformedIsolated.xml");
 		String xml = String.format(needsFormattingXml, MEASURE_ID);
 		Node wrapperNode = new QppXmlDecoder(new Context()).decode(XmlUtils.stringToDom(xml));
-		Node aciMeasurePerformedNode = wrapperNode.getChildNodes().get(0);
+		aciMeasurePerformedNode = wrapperNode.getChildNodes().get(0);
+	}
 
+
+	@Test
+	public void testMeasureIsDecoded() throws XmlException, IOException {
 		String actualMeasureId = aciMeasurePerformedNode.getValue("measureId");
 
-		assertWithMessage("The measureId is incorrect.").that(actualMeasureId).isEqualTo(MEASURE_ID);
+		assertThat(actualMeasureId).isEqualTo(MEASURE_ID);
+	}
+
+	@Test
+	public void testSectionOnlyDecodesOneMeasurePerformed() throws XmlException, IOException {
 		long measurePerformedCount = aciMeasurePerformedNode.getChildNodes(
-			node -> node.getType() == TemplateId.MEASURE_PERFORMED).count();
-		assertWithMessage("There must be one Measure Performed child node.")
-				.that( measurePerformedCount)
-				.isEqualTo(1L);
+				node -> node.getType() == TemplateId.MEASURE_PERFORMED).count();
+		
+		assertThat(measurePerformedCount).isEqualTo(1L);
 	}
 }
