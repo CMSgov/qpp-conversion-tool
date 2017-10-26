9ec2a1a8e4c2b0370115f2f201bf305ea08c48a2
Clyde Tedrick <iamclyde@gmail.com>
2017-10-26 12:28:32 -0400

    QPPCT-407: retry status

--- tools/bin/signal_success.sh
+++ tools/bin/signal_success.sh
@@ -1,20 +1,37 @@
 #!/usr/bin/env bash
-BASE_URL=https://api.github.com/repos/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME
-STATUS_UPDATE=$BASE_URL/statuses/$CIRCLE_SHA1
+BASE_URL=https://api.github.com/repos/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME;
+STATUS_UPDATE=$BASE_URL/statuses/$CIRCLE_SHA1;
 STATUS_URL=$BASE_URL/status/$CIRCLE_SHA1;
-curl $STATUS_URL -o status.json
-STATUS=$(cat status.json | jq '.state' | tr -d '"')
+STATUS=pending;
 
-PAYLOAD=$(jq --arg key0   'state' \
-   --arg value0 "$STATUS" \
-   --arg key1   'target_url' \
-   --arg value1 "https://circleci.com/workflow-run/$CIRCLE_WORKFLOW_WORKSPACE_ID" \
-   --arg key2   'description' \
-   --arg value2 "The build resulted in $STATUS" \
-   --arg key3   'context' \
-   --arg value3 'ci/circleci' \
-   '. | .[$key0]=$value0 | .[$key1]=$value1 | .[$key2]=$value2 | .[$key3]=$value3' \
-   <<<'{}');
+function sendStatus {
+    curl -H "Content-Type: application/json" -H "Authorization: token $BUILD_STATUS_TOKEN" \
+        -d "$PAYLOAD" -X POST $STATUS_UPDATE;
+}
 
-curl -H "Content-Type: application/json" -H "Authorization: token $BUILD_STATUS_TOKEN" \
-   -d "$PAYLOAD" -X POST $STATUS_UPDATE
+function makePayload {
+    PAYLOAD=$(jq --arg key0   'state' \
+        --arg value0 "$STATUS" \
+        --arg key1   'target_url' \
+        --arg value1 "https://circleci.com/workflow-run/$CIRCLE_WORKFLOW_WORKSPACE_ID" \
+        --arg key2   'description' \
+        --arg value2 "The build resulted in $STATUS" \
+        --arg key3   'context' \
+        --arg value3 'ci/circleci' \
+        '. | .[$key0]=$value0 | .[$key1]=$value1 | .[$key2]=$value2 | .[$key3]=$value3' \
+        <<<'{}');
+}
+
+COUNT=0;
+while [ "$STATUS" == 'pending' ] && [ $COUNT -lt 5 ]; do
+    curl $STATUS_URL -o status.json;
+    STATUS=$(cat status.json | jq '.state' | tr -d '"');
+
+    if ["$STATUS" == 'pending']; then
+        let COUNT=COUNT+1;
+        sleep 5;
+    else
+        makePayload
+        sendStatus
+    fi
+done
