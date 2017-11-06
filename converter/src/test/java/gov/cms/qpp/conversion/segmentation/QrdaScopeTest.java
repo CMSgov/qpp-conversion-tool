96fbbe97597ece862df9fc133ba0bd59215b3af2
Adam Harrison <ad@mharrison.us>
2017-11-06 12:17:10 -0500

    Update QrdaScopeTest to junit 5

--- converter/src/test/java/gov/cms/qpp/conversion/segmentation/QrdaScopeTest.java
+++ converter/src/test/java/gov/cms/qpp/conversion/segmentation/QrdaScopeTest.java
@@ -1,18 +1,18 @@
 package gov.cms.qpp.conversion.segmentation;
 
-
-import org.junit.Test;
-
-import java.util.HashSet;
-import java.util.Set;
-
 import static com.google.common.truth.Truth.assertWithMessage;
 import static gov.cms.qpp.conversion.segmentation.QrdaScope.ACI_AGGREGATE_COUNT;
 import static gov.cms.qpp.conversion.segmentation.QrdaScope.MEASURE_PERFORMED;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.jupiter.api.Test;
+
 public class QrdaScopeTest {
+
 	@Test
-	public void testGetTemplates() {
+	void testGetTemplates() {
 		//when
 		Set<QrdaScope> scopes = new HashSet<>();
 		scopes.add(ACI_AGGREGATE_COUNT);
@@ -24,21 +24,21 @@ public class QrdaScopeTest {
 	}
 
 	@Test
-	public void testGetTemplatesNull() {
+	void testGetTemplatesNull() {
 		//expect
 		assertWithMessage("Should be no scopes")
 				.that(QrdaScope.getTemplates(null)).isEmpty();
 	}
 
 	@Test
-	public void testGetTemplatesEmpty() {
+	void testGetTemplatesEmpty() {
 		//expect
 		assertWithMessage("Should be no scopes")
 				.that(QrdaScope.getTemplates(new HashSet<>())).isEmpty();
 	}
 
 	@Test
-	public void testValueOfString() {
+	void testValueOfString() {
 		//JaCoCo coverage test
 		QrdaScope scope = QrdaScope.valueOf("CLINICAL_DOCUMENT");
 		assertWithMessage("QrdaScope of CLINICAL_DOCUMENT equals TemplateId")
