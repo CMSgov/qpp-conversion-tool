7bbfd8a95227041373624bc6df96bdcf789b6e2f
Adam Harrison <ad@mharrison.us>
2017-11-06 12:24:57 -0500

    Update CpcTest to junit 5

--- converter/src/test/java/gov/cms/qpp/acceptance/CpcTest.java
+++ converter/src/test/java/gov/cms/qpp/acceptance/CpcTest.java
@@ -1,49 +1,46 @@
 package gov.cms.qpp.acceptance;
 
+import java.nio.file.Paths;
+import java.util.Collections;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import gov.cms.qpp.conversion.Converter;
 import gov.cms.qpp.conversion.PathQrdaSource;
 import gov.cms.qpp.conversion.model.error.TransformException;
 import gov.cms.qpp.conversion.segmentation.QrdaScope;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.ExpectedException;
 
-import java.nio.file.Paths;
-import java.util.Collections;
-
-public class CpcTest {
+class CpcTest {
 
 	private static final String CPC_FILE = "src/test/resources/cpc1.xml";
 
-	@Rule
-	public ExpectedException thrown = ExpectedException.none();
-
 	@Test
-	public void historicalAciSectionScope() {
+	void historicalAciSectionScope() {
 		run("ACI_SECTION");
 	}
 
 	@Test
-	public void historicalIaSectionScope() {
+	void historicalIaSectionScope() {
 		run("IA_SECTION");
 	}
 
 	@Test
-	public void historicalIAMeasurePerformedScope() {
+	void historicalIAMeasurePerformedScope() {
 		run("IA_MEASURE");
 	}
 
 	@Test
-	public void historicalClinicalDocumentScope() {
-		thrown.expect(TransformException.class);
-		run("CLINICAL_DOCUMENT");
+	void historicalClinicalDocumentScope() {
+		Assertions.assertThrows(TransformException.class, () -> run("CLINICAL_DOCUMENT"));
 	}
 
 	@Test
-	public void historicalFull() {
-		thrown.expect(TransformException.class);
-		Converter converter = new Converter(new PathQrdaSource(Paths.get(CPC_FILE)));
-		converter.transform();
+	void historicalFull() {
+		Assertions.assertThrows(TransformException.class, () -> {
+			Converter converter = new Converter(new PathQrdaSource(Paths.get(CPC_FILE)));
+			converter.transform();
+		});
 	}
 
 	private void run(String type) {
