29ad086f0c869f835c5cf4c95a73ded405f0867c
Clyde Tedrick <iamclyde@gmail.com>
2017-11-13 17:43:01 -0500

    QPPCT-406: rough outcome verification

--- converter/src/test/java/gov/cms/qpp/acceptance/cpc/CpcPlusAcceptanceTest.java
+++ converter/src/test/java/gov/cms/qpp/acceptance/cpc/CpcPlusAcceptanceTest.java
@@ -1,5 +1,14 @@
 package gov.cms.qpp.acceptance.cpc;
 
+import gov.cms.qpp.conversion.Converter;
+import gov.cms.qpp.conversion.PathQrdaSource;
+import gov.cms.qpp.conversion.model.error.AllErrors;
+import gov.cms.qpp.conversion.model.error.Detail;
+import gov.cms.qpp.conversion.model.error.TransformException;
+import gov.cms.qpp.conversion.util.JsonHelper;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
 import java.io.IOException;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
@@ -9,13 +18,8 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
-import org.junit.jupiter.api.Test;
-
-import gov.cms.qpp.conversion.Converter;
-import gov.cms.qpp.conversion.PathQrdaSource;
-import gov.cms.qpp.conversion.model.error.AllErrors;
-import gov.cms.qpp.conversion.model.error.TransformException;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import static com.google.common.truth.Truth.assertThat;
 
@@ -24,6 +28,13 @@ class CpcPlusAcceptanceTest {
 	private static final Path BASE = Paths.get("src/test/resources/cpc_plus/");
 	private static final Path SUCCESS = BASE.resolve("success");
 	private static final Path FAILURE = BASE.resolve("failure");
+	private static final Path FAILURE_FIXTURE = FAILURE.resolve("fixture.json");
+	private static Map fixtureValues;
+
+	@BeforeAll
+	static void setup() throws IOException {
+		fixtureValues = JsonHelper.readJson(FAILURE_FIXTURE, Map.class);
+	}
 
 	@Test
 	void testCpcPlusFileSuccesses() throws IOException {
@@ -49,18 +60,39 @@ class CpcPlusAcceptanceTest {
 		List<Path> successesThatShouldBeErrors = new ArrayList<>();
 		try (DirectoryStream<Path> stream = Files.newDirectoryStream(FAILURE)) {
 			for (Path entry : stream) {
+				if (!entry.toAbsolutePath().endsWith("CPCPlus_CMS122v5IncUUID_SampleQRDA-III.xml")) {
+					continue;
+				}
 				Converter converter = new Converter(new PathQrdaSource(entry));
 
 				try {
 					converter.transform();
 					successesThatShouldBeErrors.add(entry);
 				} catch (TransformException expected) {
+					//runnning conversions on individual files
+					List<Detail> details = expected.getDetails().getErrors().get(0).getDetails();
+					verifyOutcome(entry.toFile().getName(), details);
 					System.out.println();
 				}
 			}
 		}
 
 		assertThat(successesThatShouldBeErrors).isEmpty();
+	}
+
+	private void verifyOutcome(String filename, List<Detail> details) {
+
+		List<Map<String, Object>> expectedErrors = (List<Map<String, Object>>) fixtureValues.get(filename);
+		assertThat(details).hasSize(expectedErrors.size());
+
+		Map<String, Detail> detailMap = details.stream()
+				.collect(Collectors.toMap(Detail::getPath, Function.identity()));
+		expectedErrors.forEach(expectedError -> {
+			Detail deet = detailMap.get(expectedError.get("path"));
+			assertThat(deet).isNotNull();
+			assertThat(deet.getMessage()).isEqualTo(expectedError.get("message"));
+			assertThat(deet.getErrorCode()).isEqualTo(expectedError.get("errorCode"));
+		});
 	}
 
 	@Test
