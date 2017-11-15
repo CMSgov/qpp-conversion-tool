fd776973d66461dcc92639a4d91dab3317d901ac
Clyde Tedrick <iamclyde@gmail.com>
2017-11-15 11:42:11 -0500

    QPPCT-406: big brother

--- converter/src/main/java/gov/cms/qpp/conversion/validate/CpcQualityMeasureIdValidator.java
+++ converter/src/main/java/gov/cms/qpp/conversion/validate/CpcQualityMeasureIdValidator.java
@@ -17,7 +17,6 @@ import gov.cms.qpp.conversion.model.validation.SubPopulations;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
