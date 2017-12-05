0bb9ed37a36d8ddc9a66d7770b3f796133544a52
Adam Harrison <ad@mharrison.us>
2017-12-05 11:59:49 -0500

    Expand health check endpoint

--- rest-api/src/main/java/gov/cms/qpp/conversion/api/controllers/HealthCheckController.java
+++ rest-api/src/main/java/gov/cms/qpp/conversion/api/controllers/HealthCheckController.java
@@ -1,10 +1,17 @@
 package gov.cms.qpp.conversion.api.controllers;
 
-import org.springframework.http.HttpStatus;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import gov.cms.qpp.conversion.api.model.HealthCheck;
+import gov.cms.qpp.conversion.api.services.VersionService;
 
 /**
  * Controller to simply respond with status 200 with a GET /health call.
@@ -13,12 +20,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/health")
 public class HealthCheckController {
 
+	@Autowired
+	private VersionService version;
+
 	/**
 	 * Invoked with an HTTP GET call.
 	 */
 	@RequestMapping(method = RequestMethod.GET)
-	@ResponseStatus(HttpStatus.OK)
-	public void health() {
-		//for the health check
+	@ResponseBody
+	public ResponseEntity<HealthCheck> health() {
+		HealthCheck healthCheck = new HealthCheck();
+		healthCheck.setEnvironmentVariables(new ArrayList<>(System.getenv().keySet()));
+		healthCheck.setSystemProperties(
+				System.getProperties().keySet().stream().map(String::valueOf).collect(Collectors.toList()));
+		healthCheck.setImplementationVersion(version.getImplementationVersion());
+
+		return ResponseEntity.ok(healthCheck);
 	}
 }
