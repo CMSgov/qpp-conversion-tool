429d6cf8a40fd3c6db429b2c3afb160ba1ab0f78
Clyde Tedrick <iamclyde@gmail.com>
2017-12-06 19:50:28 -0500

    QPPCT-474 : clean up

--- converter/src/main/java/gov/cms/qpp/conversion/encode/ClinicalDocumentEncoder.java
+++ converter/src/main/java/gov/cms/qpp/conversion/encode/ClinicalDocumentEncoder.java
@@ -10,8 +10,6 @@ import gov.cms.qpp.conversion.model.TemplateId;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.google.common.base.Strings;
-
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.function.Function;
