219cd61505934fd6814a38bbcbdddd86a3ad1cf2
Clyde Tedrick <iamclyde@gmail.com>
2017-12-11 16:50:19 -0500

    QPPCT-549: the forgotten test

--- converter/src/test/java/gov/cms/qpp/conversion/decode/ClinicalDocumentDecoderTest.java
+++ converter/src/test/java/gov/cms/qpp/conversion/decode/ClinicalDocumentDecoderTest.java
@@ -246,7 +246,7 @@ class ClinicalDocumentDecoderTest {
 				.isEqualTo(ClinicalDocumentDecoder.CPCPLUS_PROGRAM_NAME);
 		assertWithMessage("Clinical Document doesn't contain entity type")
 				.that(testParentNode.getValue(ClinicalDocumentDecoder.ENTITY_TYPE))
-				.isEqualTo(ClinicalDocumentDecoder.ENTITY_GROUP);
+				.isEqualTo(ClinicalDocumentDecoder.ENTITY_INDIVIDUAL);
 		assertWithMessage("Clinical Document doesn't contain national provider")
 				.that(testParentNode.getValue(ClinicalDocumentDecoder.NATIONAL_PROVIDER_IDENTIFIER))
 				.isEqualTo("2567891421");
