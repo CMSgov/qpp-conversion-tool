f7d6317516ccb4d758ab6ffe164836c63258323b
Clyde Tedrick <iamclyde@gmail.com>
2017-12-19 10:27:02 -0500

    QPPCT-552: clean up commit history

--- tools/bin/docker_deploy.sh
+++ tools/bin/docker_deploy.sh
@@ -10,14 +10,14 @@ function build_and_tar_converter_image() {
 
 function run_ansible() {
 	echo "Running the Ansible playbook"
-	ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -c paramiko -i "$1," --extra-vars "remote_username=$2 sudoer_password=$3" ./tools/ansible/conversion_playbook.yml
+	ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i "$1," --extra-vars "remote_username=$2 sudoer_password=$3" ./tools/ansible/conversion_playbook.yml
 }
 
-#if [[ "$CIRCLE_BRANCH" == "master" || ( ! -z $DOCKER_DEPLOY_OTHER_BRANCH && "$CIRCLE_BRANCH" == "$DOCKER_DEPLOY_OTHER_BRANCH" ) ]]; then
+if [[ "$CIRCLE_BRANCH" == "master" || ( ! -z $DOCKER_DEPLOY_OTHER_BRANCH && "$CIRCLE_BRANCH" == "$DOCKER_DEPLOY_OTHER_BRANCH" ) ]]; then
 	build_and_tar_converter_image
 	run_ansible ${DOCKER_DEPLOY_HOSTS} ${DOCKER_DEPLOY_REMOTE_USERNAME} ${DOCKER_DEPLOY_SUDOER_PASSWORD}
 
 	echo "Done!"
-#else
-#	echo "Not on master so not deploying Docker image."
-#fi
+else
+	echo "Not on master so not deploying Docker image."
+fi
