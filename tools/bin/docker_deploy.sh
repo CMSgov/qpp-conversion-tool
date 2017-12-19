af85f8ae2974250c77f66d8ab71d6a3b9b49626e
Clyde Tedrick <iamclyde@gmail.com>
2017-12-18 22:09:52 -0500

    # This is a combination of 2 commits. # This is the 1st commit message:
    QPPCT-552: install ansible
    
    # This is the commit message #2:
    
    QPPCT-552: bypass privilege escalation

--- tools/bin/docker_deploy.sh
+++ tools/bin/docker_deploy.sh
@@ -10,22 +10,10 @@ function build_and_tar_converter_image() {
 
 function run_ansible() {
 	echo "Running the Ansible playbook"
-	ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i "$1," --extra-vars "remote_username=$2 sudoer_password=$3" ./tools/ansible/conversion_playbook.yml
+	ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -c paramiko -i "$1," --extra-vars "remote_username=$2 sudoer_password=$3" ./tools/ansible/conversion_playbook.yml
 }
 
 #if [[ "$CIRCLE_BRANCH" == "master" || ( ! -z $DOCKER_DEPLOY_OTHER_BRANCH && "$CIRCLE_BRANCH" == "$DOCKER_DEPLOY_OTHER_BRANCH" ) ]]; then
-
-    sudo apt-get update
-    sudo export RUNLEVEL=1
-    sudo apt-get install apt-transport-https ca-certificates curl gnupg2 software-properties-common
-    curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg | sudo  apt-key add -
-    sudo apt-key fingerprint 0EBFCD88
-    sudo add-apt-repository    "deb [arch=amd64] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") \
-    $(lsb_release -cs) stable"
-    sudo apt-get update
-    sudo apt-get install docker-ce
-    sudo service --status-all
-
 	build_and_tar_converter_image
 	run_ansible ${DOCKER_DEPLOY_HOSTS} ${DOCKER_DEPLOY_REMOTE_USERNAME} ${DOCKER_DEPLOY_SUDOER_PASSWORD}
 
