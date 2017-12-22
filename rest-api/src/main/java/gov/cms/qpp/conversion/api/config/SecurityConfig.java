c94d75c7a9cafe151fad4f5a21601cdf416f848d
Samuel Aquino <saquino@flexion.us>
2017-12-22 16:20:26 -0600

    Refactor to remove method that only calls super

--- rest-api/src/main/java/gov/cms/qpp/conversion/api/config/SecurityConfig.java
+++ rest-api/src/main/java/gov/cms/qpp/conversion/api/config/SecurityConfig.java
@@ -3,18 +3,26 @@ package gov.cms.qpp.conversion.api.config;
 import gov.cms.qpp.conversion.api.security.JwtAuthorizationFilter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 
+/**
+ * Web Security Configuration
+ */
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	private static final String CPC_WILDCARD = "/cpc/**";
 
+	/**
+	 * Configures the path to be authorized by the JWT token
+	 *
+	 * @param http Object that holds configuration
+	 * @throws Exception check for any Exception that may occur
+	 */
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.antMatcher(CPC_WILDCARD).authorizeRequests()
@@ -24,10 +32,5 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 				.and().cors()
 				.and().csrf().disable();
-	}
-
-	@Override
-	public AuthenticationManager authenticationManagerBean() throws Exception {
-		return super.authenticationManagerBean();
 	}
 }
