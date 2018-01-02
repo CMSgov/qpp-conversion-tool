0b877ea993052bacc1ff2b3b85567edc3162ea84
halprin <pkendall@flexion.us>
2018-01-02 14:59:43 -0600

    QPPCT-566: Submissions Integration Test works again

--- converter/src/test/java/gov/cms/qpp/acceptance/SubmissionIntegrationTest.java
+++ converter/src/test/java/gov/cms/qpp/acceptance/SubmissionIntegrationTest.java
@@ -1,15 +1,11 @@
 package gov.cms.qpp.acceptance;
 
-import com.jayway.jsonpath.PathNotFoundException;
 import gov.cms.qpp.conversion.Converter;
 import gov.cms.qpp.conversion.PathSource;
 import gov.cms.qpp.conversion.encode.JsonWrapper;
-import gov.cms.qpp.conversion.util.JsonHelper;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpDelete;
-import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.ByteArrayEntity;
 import org.apache.http.impl.client.HttpClientBuilder;
@@ -19,10 +15,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.List;
 import java.util.Map;
 
 import static com.google.common.truth.Truth.assertThat;
@@ -31,29 +25,13 @@ import static com.google.common.truth.Truth.assertWithMessage;
 class SubmissionIntegrationTest {
 
 	private static HttpClient client;
-	private static String serviceUrl = "https://qpp-submissions-sandbox.navapbc.com/public/validate-submission";
+	private static final String SERVICE_URL = "https://val.qpp-submissions.navapbc.com/public/validate-submission";
 	private JsonWrapper qpp;
 
 	@BeforeAll
 	@SuppressWarnings("unchecked")
-	static void setup() throws IOException {
+	static void setup() {
 		client = HttpClientBuilder.create().build();
-		HttpGet request = new HttpGet(serviceUrl);
-		request.setHeader("qpp-taxpayer-identification-number", "000777777");
-		request.setHeader("Content-Type", "application/json");
-		HttpResponse response = client.execute(request);
-		Assumptions.assumeTrue(endpointIsUp(response), "Submissions api is down");
-
-		try {
-			List<Map<?, ?>> subs = JsonHelper.readJsonAtJsonPath(response.getEntity().getContent(),
-					"$.data.submissions", List.class);
-			subs.forEach( sub -> {
-				String subId = sub.get("id").toString();
-				deleteSubmission(subId);
-			});
-		} catch (PathNotFoundException ex) {
-			System.out.println("No existing submissions to delete.");
-		}
 	}
 
 	private static boolean endpointIsUp(final HttpResponse response) {
@@ -68,8 +46,7 @@ class SubmissionIntegrationTest {
 	@Test
 	void testSubmissionApiPostSuccess() throws IOException {
 		HttpResponse httpResponse = servicePost(qpp);
-		Assumptions.assumeTrue(endpointIsUp(httpResponse), "Submissions api is down");
-		cleanUp(httpResponse);
+		Assumptions.assumeTrue(endpointIsUp(httpResponse), "Validation api is down");
 
 		assertThat(getStatus(httpResponse)).isEqualTo(200);
 	}
@@ -80,7 +57,7 @@ class SubmissionIntegrationTest {
 		Map<String, Object> obj = (Map<String, Object>) qpp.getObject();
 		obj.remove("performanceYear");
 		HttpResponse httpResponse = servicePost(qpp);
-		Assumptions.assumeTrue(endpointIsUp(httpResponse), "Submissions api is down");
+		Assumptions.assumeTrue(endpointIsUp(httpResponse), "Validation api is down");
 
 		assertWithMessage("QPP submission should be unprocessable")
 				.that(getStatus(httpResponse))
@@ -95,30 +72,10 @@ class SubmissionIntegrationTest {
 
 	private HttpResponse servicePost(JsonWrapper qpp) throws IOException {
 		HttpEntity entity = new ByteArrayEntity(qpp.toString().getBytes("UTF-8"));
-		HttpPost request = new HttpPost(serviceUrl);
+		HttpPost request = new HttpPost(SERVICE_URL);
 		request.setHeader("Content-Type", "application/json");
 		request.setEntity(entity);
 		return client.execute(request);
-	}
-
-	private void cleanUp(HttpResponse httpResponse) throws IOException {
-		int statusCode = getStatus(httpResponse);
-		if (statusCode == 201) {
-			InputStream inStream = httpResponse.getEntity().getContent();
-			Map<?, ?> json = JsonHelper.readJson(inStream, Map.class);
-			String subId = (String) ((Map<?, ?>)((Map<?, ?>) json.get("data")).get("submission")).get("id");
-			deleteSubmission(subId);
-		}
-	}
-
-	private static void deleteSubmission(String subId ) {
-		try {
-			HttpDelete cleanUp = new HttpDelete(serviceUrl + "/" + subId);
-			client.execute(cleanUp);
-		} catch (Exception ex) {
-			//don't care
-		}
-
 	}
 
 	private int getStatus(HttpResponse httpResponse) {
