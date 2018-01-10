4efb408ec0b552564ec2fe4d79cf2d6a4d679098
Clyde Tedrick <iamclyde@gmail.com>
2018-01-10 11:37:12 -0500

    QPPCT-581: review comments

--- rest-api/src/test/java/gov/cms/qpp/conversion/api/services/ValidationServiceImplTest.java
+++ rest-api/src/test/java/gov/cms/qpp/conversion/api/services/ValidationServiceImplTest.java
@@ -16,10 +16,8 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.Spy;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -48,8 +46,6 @@ import static org.mockito.Mockito.when;
 @ExtendWith(MockitoExtension.class)
 class ValidationServiceImplTest {
 
-//	@InjectMocks
-//	@Spy
 	private ValidationServiceImpl objectUnderTest;
 
 	@Mock
