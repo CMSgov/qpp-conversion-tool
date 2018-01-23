6c043f386f662d5b8839f45673919dbdc3f79861
halprin <pkendall@flexion.us>
2018-01-23 16:14:16 -0700

    QPPCCT-611: Slim down DecodeResult

--- converter/src/main/java/gov/cms/qpp/conversion/decode/DecodeResult.java
+++ converter/src/main/java/gov/cms/qpp/conversion/decode/DecodeResult.java
@@ -4,5 +4,7 @@ package gov.cms.qpp.conversion.decode;
  * DecodeResult informs the main decoding process of how to proceed when control is returned from a template decoder.
  */
 public enum DecodeResult {
-	TREE_ESCAPED, TREE_FINISHED, TREE_CONTINUE, NO_ACTION, ERROR;
+	TREE_ESCAPED,
+	TREE_FINISHED,
+	TREE_CONTINUE
 }
