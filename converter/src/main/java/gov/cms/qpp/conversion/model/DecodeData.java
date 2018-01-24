fb3089b40d95eb047034682f637f73216ef6d0e8
halprin <pkendall@flexion.us>
2018-01-24 14:36:40 -0700

    QPPCT-611: Remove unessisary methods and add JavaDocs to DecodeData

--- converter/src/main/java/gov/cms/qpp/conversion/model/DecodeData.java
+++ converter/src/main/java/gov/cms/qpp/conversion/model/DecodeData.java
@@ -2,28 +2,39 @@ package gov.cms.qpp.conversion.model;
 
 import gov.cms.qpp.conversion.decode.DecodeResult;
 
+/**
+ * A "tuple" that contains the {@link DecodeResult} and decoded {@link Node}.
+ */
 public class DecodeData {
 	private DecodeResult decodeResult;
 	private Node node;
 
+	/**
+	 * Constructor!
+	 *
+	 * @param decodeResult The {@link DecodeResult} to hold.
+	 * @param node The {@link Node} to hold.
+	 */
 	public DecodeData(final DecodeResult decodeResult, final Node node) {
 		this.decodeResult = decodeResult;
 		this.node = node;
 	}
 
+	/**
+	 * Gets the held {@link DecodeResult}.
+	 *
+	 * @return The decoded result.
+	 */
 	public DecodeResult getDecodeResult() {
 		return decodeResult;
 	}
 
-	public void setDecodeResult(final DecodeResult decodeResult) {
-		this.decodeResult = decodeResult;
-	}
-
+	/**
+	 * Gets the held {@link Node}.
+	 *
+	 * @return The node.
+	 */
 	public Node getNode() {
 		return node;
-	}
-
-	public void setNode(final Node node) {
-		this.node = node;
 	}
 }
