ffc701c4114ef1b93e530a3e53f1e97ac85f9712
Adam Harrison <ad@mharrison.us>
2018-01-31 10:32:47 -0500

    Javadocs

--- commons/src/main/java/gov/cms/qpp/conversion/util/MeasuredInputStreamSupplier.java
+++ commons/src/main/java/gov/cms/qpp/conversion/util/MeasuredInputStreamSupplier.java
@@ -9,8 +9,17 @@ import java.util.function.Supplier;
 
 import org.apache.commons.io.IOUtils;
 
+/**
+ * A reusable input stream supplier that is first measured for length
+ */
 public class MeasuredInputStreamSupplier implements Supplier<InputStream> {
 
+	/**
+	 * Terminally uses an {@link InputStream} to create a {@link MeasuredInputStreamSupplier}
+	 *
+	 * @param source the source for the new {@link MeasuredInputStreamSupplier}. Must not be null.
+	 * @return a new {@link MeasuredInputStreamSupplier} from the given {@link InputStream}
+	 */
 	public static MeasuredInputStreamSupplier terminallyTransformInputStream(InputStream source) {
 		Objects.requireNonNull(source, "source");
 
@@ -31,11 +40,21 @@ public class MeasuredInputStreamSupplier implements Supplier<InputStream> {
 		this.size = byteArray.length;
 	}
 
+	/**
+	 * Gets a new one-time use {@link InputStream}
+	 *
+	 * @return A new one-time use {@link InputStream}, from the original source
+	 */
 	@Override
 	public InputStream get() {
 		return delegate.get();
 	}
 
+	/**
+	 * Gets the size of the original {@link InputStream}
+	 *
+	 * @return The size of the original {@link InputStream}. This is cached.
+	 */
 	public int size() {
 		return size;
 	}
