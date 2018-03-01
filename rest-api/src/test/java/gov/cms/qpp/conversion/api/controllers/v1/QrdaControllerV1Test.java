21e64aa2aa490646a10ccad1e1f68639d00c20d0
Adam Harrison <ad@mharrison.us>
2018-03-01 12:10:07 -0500

    Adds another test

--- rest-api/src/test/java/gov/cms/qpp/conversion/api/controllers/v1/QrdaControllerV1Test.java
+++ rest-api/src/test/java/gov/cms/qpp/conversion/api/controllers/v1/QrdaControllerV1Test.java
@@ -1,34 +1,5 @@
 package gov.cms.qpp.conversion.api.controllers.v1;
 
-import static com.google.common.truth.Truth.assertThat;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.isNull;
-import static org.mockito.Mockito.atLeastOnce;
-import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.spy;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.UncheckedIOException;
-import java.nio.charset.StandardCharsets;
-
-import org.apache.commons.io.IOUtils;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.springframework.http.ResponseEntity;
-import org.springframework.mock.web.MockMultipartFile;
-import org.springframework.web.multipart.MultipartFile;
-
 import gov.cms.qpp.conversion.Converter;
 import gov.cms.qpp.conversion.Source;
 import gov.cms.qpp.conversion.api.services.AuditService;
@@ -38,12 +9,40 @@ import gov.cms.qpp.conversion.encode.JsonWrapper;
 import gov.cms.qpp.conversion.model.error.TransformException;
 import gov.cms.qpp.test.MockitoExtension;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UncheckedIOException;
+import java.nio.charset.StandardCharsets;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.isNull;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.apache.commons.io.IOUtils;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
+
 @ExtendWith(MockitoExtension.class)
 class QrdaControllerV1Test {
 
 	private static final String GOOD_FILE_CONTENT = "Good file";
-	private static MultipartFile multipartFile;
-	private static Converter.ConversionReport report;
+
+	private MultipartFile multipartFile;
 
 	@InjectMocks
 	private QrdaControllerV1 objectUnderTest;
@@ -57,16 +56,15 @@ class QrdaControllerV1Test {
 	@Mock
 	private AuditService auditService;
 
-	@BeforeAll
-	static void initialization() {
-		JsonWrapper wrapper = new JsonWrapper();
-		wrapper.putString("key", "Good Qpp");
-		report = mock(Converter.ConversionReport.class);
-		when(report.getEncoded()).thenReturn(wrapper);
-	}
+	@Mock
+	private Converter.ConversionReport report;
 
 	@BeforeEach
-	void setUp() throws IOException {
+	void initialization() throws IOException {
+		JsonWrapper wrapper = new JsonWrapper();
+		wrapper.putString("key", "Good Qpp");
+		when(report.getEncoded()).thenReturn(wrapper);
+
 		multipartFile = new MockMultipartFile(GOOD_FILE_CONTENT,
 				new ByteArrayInputStream(GOOD_FILE_CONTENT.getBytes()));
 	}
@@ -83,6 +81,20 @@ class QrdaControllerV1Test {
 
 		assertThat(qppResponse.getBody())
 				.isEqualTo(report.getEncoded().toString());
+	}
+
+	@Test
+	void uploadTestQrdaFile() throws IOException {
+		ArgumentCaptor<Source> peopleCaptor = ArgumentCaptor.forClass(Source.class);
+
+		when(qrdaService.convertQrda3ToQpp(peopleCaptor.capture())).thenReturn(report);
+		when(auditService.success(any(Converter.ConversionReport.class)))
+				.then(invocation -> null);
+
+		when(report.isTest()).thenReturn(true);
+		ResponseEntity qppResponse = objectUnderTest.uploadQrdaFile(multipartFile, true);
+
+		assertThat(peopleCaptor.getValue().isTest()).isTrue();
 	}
 
 	@Test
