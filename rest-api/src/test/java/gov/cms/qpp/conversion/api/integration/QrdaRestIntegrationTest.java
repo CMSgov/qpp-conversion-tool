cf9be11b617bc95fcf3b6bfa75b6fb1c5c737441
Adam Harrison <ad@mharrison.us>
2018-03-21 22:24:53 +0200

    Correct deprecated apis

--- rest-api/src/test/java/gov/cms/qpp/conversion/api/integration/QrdaRestIntegrationTest.java
+++ rest-api/src/test/java/gov/cms/qpp/conversion/api/integration/QrdaRestIntegrationTest.java
@@ -38,7 +38,7 @@ public class QrdaRestIntegrationTest {
 	void testDefaultValidQpp() throws Exception {
 		MockMultipartFile qrda3File = new MockMultipartFile("file", Files.newInputStream(Paths.get("../qrda-files/valid-QRDA-III-latest.xml")));
 		mockMvc.perform(MockMvcRequestBuilders
-			.fileUpload("/").file(qrda3File))
+			.multipart("/").file(qrda3File))
 			.andExpect(status().is(201))
 			.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
 			.andExpect(jsonPath("$.taxpayerIdentificationNumber").exists());
@@ -48,7 +48,7 @@ public class QrdaRestIntegrationTest {
 	void testValidQpp() throws Exception {
 		MockMultipartFile qrda3File = new MockMultipartFile("file", Files.newInputStream(Paths.get("../qrda-files/valid-QRDA-III-latest.xml")));
 		mockMvc.perform(MockMvcRequestBuilders
-				.fileUpload("/").file(qrda3File).accept(Constants.V1_API_ACCEPT))
+				.multipart("/").file(qrda3File).accept(Constants.V1_API_ACCEPT))
 				.andExpect(status().is(201))
 				.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
 				.andExpect(jsonPath("$.taxpayerIdentificationNumber").exists());
@@ -59,7 +59,7 @@ public class QrdaRestIntegrationTest {
 	void testInvalidQpp() throws Exception {
 		MockMultipartFile qrda3File = new MockMultipartFile("file", Files.newInputStream(Paths.get("../qrda-files/not-a-QDRA-III-file.xml")));
 		mockMvc.perform(MockMvcRequestBuilders
-			.fileUpload("/").file(qrda3File))
+			.multipart("/").file(qrda3File))
 			.andExpect(status().is(422))
 			.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
 			.andExpect(jsonPath("$.errors").exists());
@@ -69,7 +69,7 @@ public class QrdaRestIntegrationTest {
 	void testInvalidAcceptHeader() throws Exception {
 		MockMultipartFile qrda3File = new MockMultipartFile("file", Files.newInputStream(Paths.get("../qrda-files/not-a-QDRA-III-file.xml")));
 		mockMvc.perform(MockMvcRequestBuilders
-				.fileUpload("/").file(qrda3File)
+				.multipart("/").file(qrda3File)
 				.accept("application/vnd.qpp.cms.gov.v2+json"))
 				.andExpect(status().is(406));
 	}
@@ -79,7 +79,7 @@ public class QrdaRestIntegrationTest {
 		String file = "../rest-api/src/test/resources/fail_validation.xml";
 		MockMultipartFile qrda3File = new MockMultipartFile("file", Files.newInputStream(Paths.get(file)));
 		mockMvc.perform(MockMvcRequestBuilders
-				.fileUpload("/").file(qrda3File))
+				.multipart("/").file(qrda3File))
 			.andExpect(status().is(422))
 			.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
 			.andExpect(jsonPath("$.errors[0].type").value("ValidationError"));
