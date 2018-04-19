3b38b699cf3b917e1a96838eba5e3ee5080dab15
Samuel Aquino <saquino@flexion.us>
2018-04-19 12:04:09 -0500

    Fix checkstyle issue

--- converter/src/main/java/gov/cms/qpp/conversion/validate/AggregateCountValidator.java
+++ converter/src/main/java/gov/cms/qpp/conversion/validate/AggregateCountValidator.java
@@ -27,7 +27,8 @@ public class AggregateCountValidator extends NodeValidator {
 	protected void internalValidateSingleNode(Node node) {
 		check(node)
 			.singleValue(ErrorCode.AGGREGATE_COUNT_VALUE_NOT_SINGULAR
-				.format(node.getParent().getType().name(), DuplicationCheckHelper.calculateDuplications(node, AggregateCountDecoder.AGGREGATE_COUNT)),
+				.format(node.getParent().getType().name(),
+					DuplicationCheckHelper.calculateDuplications(node, AggregateCountDecoder.AGGREGATE_COUNT)),
 				AggregateCountDecoder.AGGREGATE_COUNT)
 			.intValue(ErrorCode.AGGREGATE_COUNT_VALUE_NOT_INTEGER, AggregateCountDecoder.AGGREGATE_COUNT);
 
