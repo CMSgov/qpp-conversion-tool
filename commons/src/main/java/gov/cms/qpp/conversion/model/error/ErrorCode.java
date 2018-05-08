862e8e198f1f210b4e83fbbcd999a503af003277
Samuel Aquino <saquino@flexion.us>
2018-05-08 11:15:29 -0500

    Change UUID to GUID

--- commons/src/main/java/gov/cms/qpp/conversion/model/error/ErrorCode.java
+++ commons/src/main/java/gov/cms/qpp/conversion/model/error/ErrorCode.java
@@ -129,7 +129,7 @@ public enum ErrorCode implements LocalizedError {
 	INVALID_PERFORMANCE_PERIOD_FORMAT(69, "`(Performance period start or end date)` is an invalid date format. "
 		+ "Please use a standard ISO date format. "
 		+ "Example valid values are 2017-02-26, 2017/02/26T01:45:23, or 2017-02-26T01:45:23.123", true),
-	MISSING_OR_DUPLICATED_MEASURE_UUID(70, "The measure section measure reference and results has an incorrect number of "
+	MISSING_OR_DUPLICATED_MEASURE_GUID(70, "The measure section measure reference and results has an incorrect number of "
 		+ "measure GUID supplied. Please ensure that only one measure GUID is provided per measure."),
 	MEASURES_RNR_WITH_DUPLICATED_MEASURE_UUID(71, "Two or more different measure section measure reference and results have "
 		+ "the same measure UUID. Please ensure that each measure section measure reference and results do not have "
