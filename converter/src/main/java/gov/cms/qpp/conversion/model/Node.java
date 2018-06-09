b8ab8e7e0345cb134e8f5c5d37beadeba5713e74
Adam Harrison <ad@mharrison.us>
2018-06-09 06:49:39 +0100

    Remove unused constructor in XmlException

--- converter/src/main/java/gov/cms/qpp/conversion/model/Node.java
+++ converter/src/main/java/gov/cms/qpp/conversion/model/Node.java
@@ -8,7 +8,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import java.util.Optional;
 import java.util.Set;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
