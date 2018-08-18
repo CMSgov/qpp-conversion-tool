4c4edbf92a8c6f2dce97a5f42ea864503fbf2fc0
Samuel Aquino <saquino@flexion.us>
2018-08-18 15:18:56 -0500

    IA section validation quick change for testing performance period decoding and encoding

--- converter/src/main/java/gov/cms/qpp/conversion/validate/IaSectionValidator.java
+++ converter/src/main/java/gov/cms/qpp/conversion/validate/IaSectionValidator.java
@@ -3,7 +3,10 @@ package gov.cms.qpp.conversion.validate;
 import gov.cms.qpp.conversion.model.Node;
 import gov.cms.qpp.conversion.model.TemplateId;
 import gov.cms.qpp.conversion.model.Validator;
+import gov.cms.qpp.conversion.model.error.Detail;
 import gov.cms.qpp.conversion.model.error.ErrorCode;
+
+import java.util.List;
 
 /**
  * Validates Improvement Activity Section Node - expects at least one Improvement Activity Measure
@@ -22,5 +25,22 @@ public class IaSectionValidator extends NodeValidator {
 				.childMinimum(ErrorCode.IA_SECTION_MISSING_IA_MEASURE, 1, TemplateId.IA_MEASURE)
 				.childExact(ErrorCode.IA_SECTION_MISSING_REPORTING_PARAM, 1, TemplateId.REPORTING_PARAMETERS_ACT)
 				.onlyHasChildren(ErrorCode.IA_SECTION_WRONG_CHILD, TemplateId.IA_MEASURE, TemplateId.REPORTING_PARAMETERS_ACT);
+
+		if (node.getChildNodes(TemplateId.REPORTING_PARAMETERS_ACT).count() > 0) {
+			node.getChildNodes(TemplateId.IA_MEASURE).forEach(child -> {
+				if (child.findFirstNode(TemplateId.REPORTING_PARAMETERS_ACT) != null) {
+					//add error
+					addValidationError(Detail.forErrorAndNode(ErrorCode.IA_SECTION_MISSING_REPORTING_PARAM, node));
+				}
+			});
+		} else {
+			check(node).childExact(ErrorCode.IA_SECTION_MISSING_REPORTING_PARAM, 1, TemplateId.REPORTING_PARAMETERS_ACT);
+		}
 	}
+	// if has reporting param here, check children...
+	//   - if children also have reporting param fail.
+	//   - if children don't have reporting param pass
+	// else if reporting param non existent
+	//   - check if all measure have reporting param individually
+	//      - pass if so fail otherwise
 }
