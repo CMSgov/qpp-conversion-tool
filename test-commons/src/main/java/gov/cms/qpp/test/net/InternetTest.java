b1b04e71e0d14737a0cb4e99c0b3e117efe54cb1
Adam <ad@mharrison.us>
2018-09-18 14:39:10 -0400

    Windows test fixes

--- test-commons/src/main/java/gov/cms/qpp/test/net/InternetTest.java
+++ test-commons/src/main/java/gov/cms/qpp/test/net/InternetTest.java
@@ -1,17 +1,17 @@
 package gov.cms.qpp.test.net;
 
-import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
 import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.provider.ArgumentsSource;
 
-@ParameterizedTest
+@Test
 @ArgumentsSource(InternetArgumentSource.class)
 @Tag("internet")
 @Retention(RUNTIME)
