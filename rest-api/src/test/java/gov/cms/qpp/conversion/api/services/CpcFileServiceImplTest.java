8bc1c79d61dcc84125e17325a1bf01716808d9c5
Adam Harrison <ad@mharrison.us>
2018-11-13 08:00:39 -0500

    Fix tests

--- rest-api/src/test/java/gov/cms/qpp/conversion/api/services/CpcFileServiceImplTest.java
+++ rest-api/src/test/java/gov/cms/qpp/conversion/api/services/CpcFileServiceImplTest.java
@@ -91,12 +91,12 @@ class CpcFileServiceImplTest {
 		when(dbService.getMetadataById(key)).thenReturn(buildFakeMetadata(false, false));
 		when(storageService.getFileByLocationId(key)).thenReturn(new ByteArrayInputStream("1337".getBytes()));
 
-		NoFileInDatabaseException expectedException = assertThrows(NoFileInDatabaseException.class, ()
+		InvalidFileTypeException expectedException = assertThrows(InvalidFileTypeException.class, ()
 			-> objectUnderTest.getQppById(key));
 
 		verify(dbService, times(1)).getMetadataById(key);
 
-		assertThat(expectedException).hasMessageThat().isEqualTo(CpcFileServiceImpl.FILE_NOT_FOUND);
+		assertThat(expectedException).hasMessageThat().isEqualTo(CpcFileServiceImpl.INVALID_FILE);
 	}
 
 	@Test
@@ -131,12 +131,12 @@ class CpcFileServiceImplTest {
 		when(dbService.getMetadataById(anyString())).thenReturn(buildFakeMetadata(false, false));
 		when(storageService.getFileByLocationId("test")).thenReturn(new ByteArrayInputStream("1337".getBytes()));
 
-		NoFileInDatabaseException expectedException = assertThrows(NoFileInDatabaseException.class, ()
+		InvalidFileTypeException expectedException = assertThrows(InvalidFileTypeException.class, ()
 				-> objectUnderTest.getFileById("test"));
 
 		verify(dbService, times(1)).getMetadataById(anyString());
 
-		assertThat(expectedException).hasMessageThat().isEqualTo(CpcFileServiceImpl.FILE_NOT_FOUND);
+		assertThat(expectedException).hasMessageThat().isEqualTo(CpcFileServiceImpl.INVALID_FILE);
 	}
 
 	@Test
