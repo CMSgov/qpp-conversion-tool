ab707fed6ef3acc0995240c2214fb1cb36266a7c
Clyde Tedrick <iamclyde@gmail.com>
2018-12-31 00:47:07 -0500

    take 7

--- tools/docker/docker-artifacts/qppConverter.sh
+++ tools/docker/docker-artifacts/qppConverter.sh
@@ -2,5 +2,5 @@
 
 sed -i -e "s/NEWRELIC_API_KEY/$NEWRELIC_API_KEY/g" ./newrelic/newrelic.yml
 sed -i -e "s/APP_ENV/$APP_ENV/g" ./newrelic/newrelic.yml
-sleep 300
+sleep 420
 exec java $JAVA_OPTS -javaagent:./newrelic/newrelic.jar -jar ./rest-api.jar
