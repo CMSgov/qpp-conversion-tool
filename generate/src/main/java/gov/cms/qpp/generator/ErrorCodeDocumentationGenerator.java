839df70ef808735770cfc776538810696c0c5ff0
David Uselmann <duselmann@flexion.us>
2019-01-08 18:21:49 -0600

    Fix maven plugin file writing path.
    The generated ERROR_MESSAGES.md file was hard code writing to './' even when launching maven from the subproject dir. Devs run mvn from the subproject to save time during rapid iteration. This ensures the file is written to the same path regardless of the working dir.

--- generate/src/main/java/gov/cms/qpp/generator/ErrorCodeDocumentationGenerator.java
+++ generate/src/main/java/gov/cms/qpp/generator/ErrorCodeDocumentationGenerator.java
@@ -8,11 +8,14 @@ import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.project.MavenProject;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 @Mojo(name = "generateErrorCodeDoc")
 public class ErrorCodeDocumentationGenerator extends AbstractMojo {
@@ -21,7 +24,7 @@ public class ErrorCodeDocumentationGenerator extends AbstractMojo {
 		MustacheFactory mf = new DefaultMustacheFactory();
 		Mustache mdTemplate = mf.compile("error-code/error-code-tempate.md");
 
-		try (FileWriter fw = new FileWriter("./ERROR_MESSAGES.md")) {
+		try (FileWriter fw = new FileWriter(args[0] + "ERROR_MESSAGES.md")) {
 			List<ErrorCode> errorCodes = Arrays.asList(ErrorCode.values());
 			mdTemplate.execute(fw, errorCodes).flush();
 			fw.flush();
@@ -30,9 +33,33 @@ public class ErrorCodeDocumentationGenerator extends AbstractMojo {
 
 	@Override
 	public void execute() throws MojoExecutionException, MojoFailureException {
+		String parentDir = "";
+		
 		try {
 			getLog().info("Running Error Code documentation plugin");
-			ErrorCodeDocumentationGenerator.main();
+			
+			@SuppressWarnings("rawtypes")
+			Map context = getPluginContext();
+			MavenProject project = (MavenProject) context.get("project");
+			MavenProject parent = project.getParent();
+			
+			if (parent != null) {
+				String parentPath = parent.getBasedir().getAbsolutePath();
+				String workingDir = new File(".").getAbsolutePath();
+				workingDir = workingDir.substring(0, workingDir.length() - 2);
+				
+				if (parentPath.equals(workingDir)) {
+					// when the working dir is the parent project dir, use the working dir
+					parentDir = "./";
+				} else {
+					// when the working dir is the subproject dir, use the parent dir
+					// this ensure the error messages file is written to the parent project dir
+					parentDir = "../"; 
+				}
+			}
+			getLog().info("Parent project work directory offset " + parentDir);
+			
+			ErrorCodeDocumentationGenerator.main(parentDir.toString());
 		} catch (IOException e) {
 			throw new MojoExecutionException("Error code documentation problems", e);
 		}
