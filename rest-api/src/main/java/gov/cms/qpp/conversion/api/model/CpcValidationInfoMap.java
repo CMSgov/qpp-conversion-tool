2456e8f6bcc353f7f406aba919bbccaf36a0d009
Samuel Aquino <saquino@flexion.us>
2019-11-15 09:39:31 -0600

    start update for TIN/API/NPM validations

--- rest-api/src/main/java/gov/cms/qpp/conversion/api/model/CpcValidationInfoMap.java
+++ rest-api/src/main/java/gov/cms/qpp/conversion/api/model/CpcValidationInfoMap.java
@@ -17,10 +17,10 @@ import java.util.stream.Collectors;
 
 public class CpcValidationInfoMap {
 	private static final Logger DEV_LOG = LoggerFactory.getLogger(CpcValidationInfoMap.class);
-	private Map<String, CpcValidationInfo> apmToSpec;
+	private Map<String, List<CpcValidationInfo>> apmToSpec;
 
 	public CpcValidationInfoMap(InputStream cpcNpiToApmJson) {
-		apmToSpec = convertJsonToMap(cpcNpiToApmJson);
+		apmToSpec = convertJsonToMapOfLists(cpcNpiToApmJson);
 	}
 
 	private Map<String, CpcValidationInfo> convertJsonToMap(InputStream cpcNpiToApmJson) {
@@ -38,7 +38,25 @@ public class CpcValidationInfoMap {
 		}
 
 		return cpcValidationInfoList.stream()
-			.collect(Collectors.toMap(CpcValidationInfo::getNpi, Function.identity()));
+			.collect(Collectors.toMap(CpcValidationInfo::getTin, Function.identity()));
+	}
+
+	private Map<String, CpcValidationInfo> convertJsonToMapOfLists(InputStream cpcNpiToApmJson) {
+		if (cpcNpiToApmJson == null) {
+			return null;
+		}
+		List<CpcValidationInfo> cpcValidationInfoList = new ArrayList<>();
+		try {
+			ObjectMapper objectMapper = new ObjectMapper();
+			cpcValidationInfoList =
+				Arrays.asList(objectMapper.readValue(new InputStreamReader(cpcNpiToApmJson, StandardCharsets.UTF_8),
+					CpcValidationInfo[].class));
+		} catch (IOException exc) {
+			DEV_LOG.info("Failed to parse the cpc+ validation npi to apm list...");
+		}
+
+		return cpcValidationInfoList.stream()
+			.collect(Collectors.toMap(CpcValidationInfo::getTin, Function.identity()));
 	}
 
 	public Map<String, CpcValidationInfo> getApmToSpec() {
