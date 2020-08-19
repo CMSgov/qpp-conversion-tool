d663848243dbe50d6ed24e6bb2d5bfbba34dcb5f
KyleApfel <kyle.apfel@semanticbits.com>
2020-08-19 11:28:20 -0500

    Resolve CT Frontend Compilation Issues

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-import { FileSelectDirective, FileDropDirective, FileUploader } from '../../node_modules/ng2-file-upload/ng2-file-upload';
+import { FileSelectDirective, FileDropDirective, FileUploader } from 'ng2-file-upload';
 
 import { AppComponent } from './app.component';
 
