950bcb2d4d979ada287f4eadae114bd86483d8e6
Samuel Aquino <saquino@flexion.us>
2020-09-01 15:36:45 -0500

    Initial rti processing addition

--- rest-api/src/main/java/gov/cms/qpp/conversion/api/model/Constants.java
+++ rest-api/src/main/java/gov/cms/qpp/conversion/api/model/Constants.java
@@ -17,6 +17,7 @@ public class Constants {
 	public static final String CPC_DYNAMO_PARTITION_START = "CPC_";
 	public static final String DYNAMO_CPC_ATTRIBUTE = "Cpc";
 	public static final String DYNAMO_CPC_PROCESSED_CREATE_DATE_ATTRIBUTE = "CpcProcessed_CreateDate";
+	public static final String DYNAMO_RTI_PROCESSED_CREATE_DATE_ATTRIBUTE = "RtiProcessed_CreateDate";
 	public static final String DYNAMO_CREATE_DATE_ATTRIBUTE = "CreateDate";
 	public static final String CPC_PLUS_BUCKET_NAME_VARIABLE = "CPC_PLUS_BUCKET_NAME";
 	public static final String CPC_PLUS_FILENAME_VARIABLE = "CPC_PLUS_VALIDATION_FILE";
