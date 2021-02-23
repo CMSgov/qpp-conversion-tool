37cbd76a3f95446750fb5a8932d8d8864b4eeee6
Samuel Aquino <saquino@flexion.us>
2021-02-23 15:06:36 -0600

    Update virtual group submission sample and naming for entity id conversion

--- converter/src/main/java/gov/cms/qpp/conversion/decode/ClinicalDocumentDecoder.java
+++ converter/src/main/java/gov/cms/qpp/conversion/decode/ClinicalDocumentDecoder.java
@@ -72,7 +72,7 @@ public class ClinicalDocumentDecoder extends QrdaDecoder {
 				setNationalProviderIdOnNode(element, thisNode);
 			}
 			if (ENTITY_VIRTUAL_GROUP.equals(entityType)) {
-				setApmEntityIdOnNode(element, thisNode, ClinicalDocumentDecoder.ENTITY_ID);
+				setEntityIdOnNode(element, thisNode, ClinicalDocumentDecoder.ENTITY_ID);
 			}
 		}
 
@@ -92,7 +92,7 @@ public class ClinicalDocumentDecoder extends QrdaDecoder {
 				thisNode.putValue(PRACTICE_ID, id.getValue(), false);
 			setOnNode(element, getXpath(PRACTICE_ID), consumer, Filters.attribute(), false);
 		} else {
-			setApmEntityIdOnNode(element, thisNode, ClinicalDocumentDecoder.PRACTICE_ID);
+			setEntityIdOnNode(element, thisNode, ClinicalDocumentDecoder.PRACTICE_ID);
 		}
 	}
 
@@ -195,12 +195,9 @@ public class ClinicalDocumentDecoder extends QrdaDecoder {
 			consumer, Filters.attribute());
 	}
 
-	private void setApmEntityIdOnNode(Element element, Node thisNode, String entityLocationId) {
-		Consumer<? super Attribute> consumer = p ->
-			thisNode.putValue(ENTITY_ID,
-				p.getValue());
-		setOnNode(element, getXpath(entityLocationId),
-			consumer, Filters.attribute(), true);
+	private void setEntityIdOnNode(Element element, Node thisNode, String entityLocationId) {
+		Consumer<? super Attribute> consumer = p -> thisNode.putValue(ENTITY_ID, p.getValue());
+		setOnNode(element, getXpath(entityLocationId), consumer, Filters.attribute(), true);
 	}
 
 	/**
