44aeaf023094fff7b0aef6637f5ebe07f022fc13
Patrick Kasarski <patrick.kasarski@semanticbits.com>
2021-08-19 19:22:38 -0500

    QPPSF-8127: Y5 CPC+

--- commons/src/main/java/gov/cms/qpp/conversion/model/error/ProblemCode.java
+++ commons/src/main/java/gov/cms/qpp/conversion/model/error/ProblemCode.java
@@ -189,7 +189,8 @@ public enum ProblemCode implements LocalizedProblem {
 	CPC_PCF_PLUS_DENOMINATOR_COUNT_INVALID(101, "Denominator count must be equal to Initial Population count for CPC Plus measure population `(measure population id)`.", true),
 	PI_RESTRICTED_MEASURES(102, "A PI section cannot contain PI_HIE_5 with PI_HIE_1, PI_LVOTC_1, PI_HIE_4, or PI_LVITC_2", false),
 	PCF_TOO_FEW_QUALITY_MEASURE_CATEGORY(103, "PCF Submissions must have at least `(PCF Measure minimum)` "
-		+ "of the following measures: `(Listing of valid measure ids)`", true);
+		+ "of the following measures: `(Listing of valid measure ids)`", true),
+	CPC_PLUS_NO_PI(104, "The file contains Promoting Interoperability (PI) data; PI data must not be reported for CPC+");
 
 
 	private static final Map<Integer, ProblemCode> CODE_TO_VALUE = Arrays.stream(values())
