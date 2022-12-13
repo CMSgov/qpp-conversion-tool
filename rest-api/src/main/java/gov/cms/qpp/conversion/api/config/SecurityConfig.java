6728f464907746f4482f2022a8c764c6d1c33cb4
Samuel Aquino <saquino@flexion.us>
2022-12-13 08:35:50 -0600

    Update spring security config

--- rest-api/src/main/java/gov/cms/qpp/conversion/api/config/SecurityConfig.java
+++ rest-api/src/main/java/gov/cms/qpp/conversion/api/config/SecurityConfig.java
@@ -5,8 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 import gov.cms.qpp.conversion.api.security.JwtAuthorizationFilter;
+
+import java.util.Set;
 
 /**
  * Web Security Configuration
@@ -31,7 +34,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	 */
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.cors().and().csrf().disable();
+		http.requestMatchers().antMatchers(PCF_WILDCARD)
+			.and()
+			.authorizeRequests()
+			.anyRequest().authenticated()
+			.and()
+			.addFilter(new JwtAuthorizationFilter(authenticationManager(), Set.of(orgName, rtiOrgName)))
+			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+			.and().cors()
+			.and().csrf().disable();
 	}
-
 }
