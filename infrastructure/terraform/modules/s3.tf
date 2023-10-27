4ce07dc4160b2303eb6a7e95c20ca5e15748ffa0
timothy-ellis-ky <tim.ellis@semanticbits.com>
2023-10-27 13:24:10 -0400

    upgrades for new S3 resource attachments

--- infrastructure/terraform/modules/s3.tf
+++ infrastructure/terraform/modules/s3.tf
@@ -3,43 +3,67 @@
 ## Note this bucket is
 resource "aws_s3_bucket" "log_bucket" {
   bucket = "qppsf-conversion-tool-logs"
-  acl    = "log-delivery-write"
+  # QPPSE-1211 correct warning:
+  # Warning: Argument is deprecated
+  # ...
+  # Use resource aws_s3_bucket_acl instead
+  #
+  # acl    = "log-delivery-write"
 
-  versioning {
-    enabled = true
-  }
+  # QPPSE-1211 correct warning:
+  # Warning: Argument is deprecated
+  # ...
+  # Use resource aws_s3_bucket_versioning instead
+  #
+  # versioning {
+  #   enabled = true
+  # }
+  # QPPSE-1211 correct warning:
+  # Warning: Argument is deprecated
+  # ... 
+  # Use the aws_s3_bucket_lifecycle_configuration resource instead
+  # ...
+  # lifecycle_rule {
+  #   id      = "default-lifecycle"
+  #   enabled = true
 
-  lifecycle_rule {
-    id      = "default-lifecycle"
-    enabled = true
+  #   transition {
+  #     days          = 60
+  #     storage_class = "STANDARD_IA"
+  #   }
 
-    transition {
-      days          = 60
-      storage_class = "STANDARD_IA"
-    }
+  #   transition {
+  #     days          = 90
+  #     storage_class = "GLACIER"
+  #   }
 
-    transition {
-      days          = 90
-      storage_class = "GLACIER"
-    }
-
-    expiration {
-      days                         = 365
-      expired_object_delete_marker = true
-    }
-  }
+  #   expiration {
+  #     days                         = 365
+  #     expired_object_delete_marker = true
+  #   }
+  # }
 
   lifecycle {
     prevent_destroy = true
+    # QPPSE-1211 for resource additions below
+    ignore_changes = [ grant ]
   }
-  # Require encryption at rest
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
+  # ...
+  # QPPSE-1211 correct warning:
+  # Warning: Argument is deprecated
+  # ... 
+  # Use the aws_s3_bucket_server_side_encryption_configuration resource instead
+  # ...
+  # Require encryption at rest 
+  # server_side_encryption_configuration {
+  #   rule {
+  #     apply_server_side_encryption_by_default {
+  #       sse_algorithm = "AES256"
+  #     }
+  #   }
+  # }
+  #
+
   tags = {
     "Name"                = "${var.project_name}-s3-logs-bucket"
     "qpp:owner"           = var.owner
@@ -52,8 +76,120 @@ resource "aws_s3_bucket" "log_bucket" {
     "qpp:iac-repo-url"    = var.git-origin
   }
 
+
 }
 
+# QPPSE-1211
+data "aws_canonical_user_id" "current" {}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "log_bucket" {
+  bucket = aws_s3_bucket.log_bucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+resource "aws_s3_bucket_versioning" "log_bucket" {
+  bucket = aws_s3_bucket.log_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+resource "aws_s3_bucket_lifecycle_configuration" "log_bucket" {
+  bucket = aws_s3_bucket.log_bucket.id
+
+  rule {
+    id = "default-lifecycle"
+
+    expiration {
+      days                         = 365
+      expired_object_delete_marker = true
+    }
+
+    filter {}
+    #   and {
+    #     prefix = "/"
+
+    #     tags = {
+    #       rule      = "log"
+    #       autoclean = "true"
+    #     }
+    #   }
+    # }
+
+    status = "Enabled"
+
+    transition {
+      days          = 60
+      storage_class = "STANDARD_IA"
+    }
+
+    transition {
+      days          = 90
+      storage_class = "GLACIER"
+    }
+  }
+
+  # rule {
+  #   id = "prevent-destroy"
+  #   filter {}
+
+  # }
+  # rule {
+  #   id = "ignore-grant"
+  #   filter {}
+  #   ignore_changes = [
+  #     grant
+  #   ]
+  #   status = "Enabled"
+  # }
+}
+
+resource "aws_s3_bucket_ownership_controls" "log_bucket" {
+  bucket = aws_s3_bucket.log_bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "log_bucket" {
+  depends_on = [aws_s3_bucket_ownership_controls.log_bucket]
+
+  bucket = aws_s3_bucket.log_bucket.id
+
+  access_control_policy {
+    grant {
+      grantee {
+        id   = data.aws_canonical_user_id.current.id
+        type = "CanonicalUser"
+      }
+      permission = "FULL_CONTROL"
+    }
+
+    grant {
+      grantee {
+        type = "Group"
+        uri  = "http://acs.amazonaws.com/groups/s3/LogDelivery"
+      }
+      permission = "READ_ACP"
+    }
+
+    grant {
+      grantee {
+        type = "Group"
+        uri  = "http://acs.amazonaws.com/groups/s3/LogDelivery"
+      }
+      permission = "WRITE"
+    }
+
+    owner {
+      id = data.aws_canonical_user_id.current.id
+    }
+  }
+}
+#
 
 # No public access
 resource "aws_s3_bucket_public_access_block" "log_bucket-public-access-block" {
