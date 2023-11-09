72e30c84fc88765061f0d10fc68c3ca5fa0cb7d2
Samuel Aquino <saquino@flexion.us>
2023-11-09 15:42:41 -0600

    Test File location

--- upload-part-file.sh
+++ upload-part-file.sh
@@ -7,9 +7,11 @@ AWS_REGION=$4
 
 export AWS_REGION=${AWS_REGION}
 
+echo "${PART_FILE_BUCKET} ${PART_FILE} ${FORMATTED_FILE_NAME} ${AWS_REGION}"
+
 aws s3 cp s3://${PART_FILE_BUCKET}/${PART_FILE} .
 chmod +x ./tools/scripts/format-participation-file.py
-python3 ./tools/scripts/format-participation-file.py ${PART_FILE} ${FORMATTED_FILE_NAME}
+python ./tools/scripts/format-participation-file.py ${PART_FILE} ${FORMATTED_FILE_NAME}
 aws s3 mv ${FORMATTED_FILE_NAME} s3://${PART_FILE_BUCKET}/${FORMATTED_FILE_NAME}
 
 rm ${FORMATTED_FILE_NAME}
