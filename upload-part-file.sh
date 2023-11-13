c2c8c3912e54969f934a0fc94b5b416982a37bf1
Samuel Aquino <saquino@flexion.us>
2023-11-13 15:01:12 -0600

    Test: Add build spec and script with participation file update

---
+++ upload-part-file.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+PART_FILE_BUCKET=$1
+PART_FILE=$2
+FORMATTED_FILE_NAME=$3
+AWS_REGION=$4
+
+export AWS_REGION=${AWS_REGION}
+
+echo "${PART_FILE_BUCKET} ${PART_FILE} ${FORMATTED_FILE_NAME} ${AWS_REGION}"
+
+aws s3 cp s3://${PART_FILE_BUCKET}/${PART_FILE} .
+chmod +x ./tools/scripts/format-participation-file.py
+python ./tools/scripts/format-participation-file.py ${PART_FILE} ${FORMATTED_FILE_NAME}
+aws s3 mv ${FORMATTED_FILE_NAME} s3://${PART_FILE_BUCKET}/${FORMATTED_FILE_NAME}
+
+rm ${FORMATTED_FILE_NAME}
