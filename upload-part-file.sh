79b59b8fb9f546a8771993fede6ec0404dd5559a
Samuel Aquino <saquino@flexion.us>
2023-11-13 15:22:56 -0600

    Update script upload-part-file.sh to add messaging for successfully removing participation file locally

--- upload-part-file.sh
+++ upload-part-file.sh
@@ -15,4 +15,9 @@ chmod +x ./tools/scripts/format-participation-file.py
 python ./tools/scripts/format-participation-file.py ${PART_FILE} ${FORMATTED_FILE_NAME}
 aws s3 mv ${FORMATTED_FILE_NAME} s3://${PART_FILE_BUCKET}/${FORMATTED_FILE_NAME}
 
-rm ${FORMATTED_FILE_NAME}
+if [ -e ${FORMATTED_FILE_NAME} ]
+then
+  echo 'Formatted Participation file has been removed locally.'
+else 
+  echo 'Formatted Participation file has not been removed locally.' 
+fi
