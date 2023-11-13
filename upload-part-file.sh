dbfac1c70bda777233c090e4f124b0017b1b4d1e
Samuel Aquino <saquino@flexion.us>
2023-11-13 15:31:27 -0600

    File removal condition added

--- upload-part-file.sh
+++ upload-part-file.sh
@@ -15,9 +15,11 @@ chmod +x ./tools/scripts/format-participation-file.py
 python ./tools/scripts/format-participation-file.py ${PART_FILE} ${FORMATTED_FILE_NAME}
 aws s3 mv ${FORMATTED_FILE_NAME} s3://${PART_FILE_BUCKET}/${FORMATTED_FILE_NAME}
 
-if [ -e ${FORMATTED_FILE_NAME} ]
+if test -f "$FORMATTED_FILE_NAME"
 then
-  echo 'Formatted Participation file has been removed locally.'
+  echo 'Removing Formatted Participation file localy...'
+  rm ${FORMATTED_FILE_NAME}
+  echo 'Formatted Participation file has been removed locally.' 
 else 
-  echo 'Formatted Participation file has not been removed locally.' 
+  echo 'Formatted Participation file has been removed locally.'
 fi
