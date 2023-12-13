ce8714e72c57ef43ba2c186cab0623fc2c1467c6
Samuel Aquino <saquino@flexion.us>
2023-12-13 12:43:18 -0600

    Update automation for participaton file

--- upload-part-file.sh
+++ upload-part-file.sh
@@ -10,8 +10,6 @@ export AWS_REGION=${AWS_REGION}
 pip install openpyxl
 pip install simplejson
 
-#chmod +x ./tools/scripts/retrieve-fms-file.py
-#python ./tools/scripts/retrieve-fms-file.py -au ${AUTH_URL} -fu ${FMS_URL} -t ${FMS_TOKEN} -p ${FMS_PATH}
 aws s3 cp s3://${PART_FILE_BUCKET}/${PART_FILE} .
 chmod +x ./tools/scripts/format-participation-file.py
 python ./tools/scripts/format-participation-file.py ${PART_FILE} ${FORMATTED_FILE_NAME}
