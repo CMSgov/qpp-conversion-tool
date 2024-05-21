11f03684e62f29a5025eb775cfa33fda21ec8b8e
Sivakumar Srinivasulu <siva.srinivasulu@semanticbits.com>
2024-05-20 20:26:22 -0500

    updated and tested PY24 measures changes to command line module

--- commons/src/main/java/gov/cms/qpp/conversion/DocumentationReference.java
+++ commons/src/main/java/gov/cms/qpp/conversion/DocumentationReference.java
@@ -2,16 +2,19 @@ package gov.cms.qpp.conversion;
 
 public enum DocumentationReference {
 
-	PCF_SUBMISSIONS(14),
+	PCF_SUBMISSIONS(12),
 	IDENTIFIERS(15),
 	PERFORMANCE_PERIOD(17),
 	CLINICAL_DOCUMENT(19),
-	PRACTICE_SITE_ADDRESS(25),
+	PRACTICE_SITE_ADDRESS(22),
 	REPORTING_PARAMETERS_ACT(17),
 	MEASURE_IDS(43),
-	CEHRT(15);
+	CEHRT(22);
 
-	private static final String BASE_PATH = "https://ecqi.healthit.gov/sites/default/files/2023-CMS-QRDA-III-Eligible-Clinicians-IG-v1.1-508.pdf#page=";
+	private static final String BASE_PATH = "https://ecqi.healthit.gov/sites/default/files/2024-CMS-QRDA-III-EC-IG-v1.1-508.pdf#page=";
+	public static final String PERFORMANCE_START_DATE = "01/01/2024";
+	public static final String PERFORMANCE_END_DATE = "12/31/2024";
+	public static final String ROSTER_UPDATE_DATE = "December 1, 2024";
 	private final String path;
 
 	DocumentationReference(int page) {
