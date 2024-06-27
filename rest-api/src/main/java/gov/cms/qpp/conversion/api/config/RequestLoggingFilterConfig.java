4df0b37dce4d897c15e5fc21ec4354bf670fbebb
Sivakumar Srinivasulu <siva.srinivasulu@semanticbits.com>
2024-06-27 17:16:43 -0500

    added spring boot http logging

---
+++ rest-api/src/main/java/gov/cms/qpp/conversion/api/config/RequestLoggingFilterConfig.java
@@ -0,0 +1,16 @@
+package gov.cms.qpp.conversion.api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+@Configuration
+public class RequestLoggingFilterConfig {
+    @Bean
+    public CommonsRequestLoggingFilter requestLoggingFilter() {
+        CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
+        loggingFilter.setIncludeQueryString(true);
+        loggingFilter.setIncludeHeaders(true);
+        return loggingFilter;
+    }
+}
