72db298238f9b11949c29fb157ab4d1b24033d14
Sivakumar Srinivasulu <siva.srinivasulu@semanticbits.com>
2024-12-05 15:29:46 -0600

    updated some more dependencies to resolve server startup issues

--- rest-api/src/main/java/gov/cms/qpp/conversion/api/config/SecurityConfig.java
+++ rest-api/src/main/java/gov/cms/qpp/conversion/api/config/SecurityConfig.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
@@ -35,7 +34,8 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http.securityMatcher(PCF_WILDCARD)
+            .authorizeRequests()
 			.anyRequest().authenticated()
 			.and()
             .csrf(csrf -> csrf.disable())
@@ -48,10 +48,5 @@ public class SecurityConfig {
             );
 
         return http.build();
-    }
-
-    @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        return (web) -> web.ignoring().requestMatchers(PCF_WILDCARD);
     }
 }
