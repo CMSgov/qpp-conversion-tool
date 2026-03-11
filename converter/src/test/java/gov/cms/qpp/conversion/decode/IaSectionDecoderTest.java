a886310ee070c4d338bfb4b94983fb72ce9aa32c
David Berry <David.Berry@semanticbits.com>
2026-03-11 12:11:19 -0400

    feat: QPPA-11346 Java 21 update review complete

--- converter/src/test/java/gov/cms/qpp/conversion/decode/IaSectionDecoderTest.java
+++ converter/src/test/java/gov/cms/qpp/conversion/decode/IaSectionDecoderTest.java
@@ -34,13 +34,13 @@ class IaSectionDecoderTest {
 
 	@Test
 	void testDecodeIaSectionIgnoresGarbage() throws XmlException {
-		xmlFragment = xmlFragment.replaceAll("<statusCode ",
+		xmlFragment = xmlFragment.replace("<statusCode code=\"completed\"/>",
 				"""
-				
-				<Stuff arbitrary="123"><newnode>Some extra stuff</newnode></Stuff>\
+
+				<Stuff arbitrary=\"123\"><newnode>Some extra stuff</newnode></Stuff>
 				Unexpected stuff appears here
-				
-				<statusCode """);
+
+				<statusCode code=\"completed\"/>""");
 
 		Node root = executeDecoderWithoutDefaults();
 		Node iaSectionNode = root.findFirstNode(TemplateId.IA_SECTION_V3);
